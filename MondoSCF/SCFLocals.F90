f56ed7d2880d96ceb8d9b8acaaa4dc148dd7a012
mchalla <mchalla>
2000-12-12 19:17:43 +0000

    Added looser matrix thresholds.

--- MondoSCF/SCFLocals.F90
+++ MondoSCF/SCFLocals.F90
@@ -43,7 +43,7 @@ MODULE SCFLocals
 !  Thresholds (Loose,Good,Tight,VeryTight):
 
    REAL(DOUBLE),DIMENSION(4) :: CubeNeglect=(/1.D-4, 1.D-5, 1.D-6,  1.D-8 /)
-   REAL(DOUBLE),DIMENSION(4) :: TrixNeglect=(/1.D-4, 1.D-5, 5.D-6,  1.D-6 /)
+   REAL(DOUBLE),DIMENSION(4) :: TrixNeglect=(/1.D-2, 1.D-3, 1.D-4,  1.D-6 /)
    REAL(DOUBLE),DIMENSION(4) :: TwoENeglect=(/1.D-5, 1.D-6, 1.D-7,  1.D-9 /)
    REAL(DOUBLE),DIMENSION(4) :: DistNeglect=(/1.D-6, 1.D-8, 1.D-10, 1.D-12/)
 !
