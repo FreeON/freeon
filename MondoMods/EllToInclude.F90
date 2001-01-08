696b92f4dcb335605a3527b4a398a2f18b2f1071
mchalla <mchalla>
2001-01-08 23:07:56 +0000

    Writes file to be included in MMA codes that generate explicit source.

---
+++ MondoMods/EllToInclude.F90
@@ -0,0 +1,15 @@
+PROGRAM EllToInclude
+   USE DerivedTypes
+   USE GlobalScalars
+#ifdef NAG
+   USE F90_UNIX
+#endif
+   CHARACTER(LEN=DEFAULT_CHR_LEN) :: IncDir
+   CALL GetEnv('MONDO_INC',IncDir)
+   OPEN(UNIT=90,FILE=TRIM(IncDir)//'/Ell.m',STATUS='REPLACE')
+   WRITE(*,*)TRIM(IncDir)//'Ell.m'
+   WRITE(90,*)'HGEll=',HGEll,';'             
+   WRITE(90,*)'SPEll=',SPEll,';'             
+   CLOSE(UNIT=90,STATUS='KEEP')
+END PROGRAM EllToInclude
+
