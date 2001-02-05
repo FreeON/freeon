25c22167215accc2864bcb491392a9a98eb74497
salazarm <salazarm>
2001-02-05 20:30:18 +0000

    2/5/01 Working gradient of T

--- OneE/GradT.F90
+++ OneE/GradT.F90
@@ -2,11 +2,11 @@
 !--  This source code is part of the MondoSCF suite of 
 !--  linear scaling electronic structure codes.  
 !
-!--  Matt Challacombe and  C. J. Tymczak
+!--  Matt Challacombe and  Michael R. Salazar
 !--  Los Alamos National Laboratory
 !--  Copyright 2000, The University of California
 !
-!    COMPUTE THE Kintic Energy Matrix
+!    COMPUTE THE GRADIENT
 !
 PROGRAM GradT
   USE DerivedTypes
@@ -19,64 +19,46 @@ PROGRAM GradT
   USE Macros
   USE LinAlg
   USE AtomPairs
+  USE BraKetBloks
   USE GradTBlock
 #ifdef PARALLEL
   USE MondoMPI
-#endif
-  IMPLICIT NONE
-!-------------------------------------------------------- 
-!     FUNCTION TBlok(BS,MD,Pair)
-!        USE DerivedTypes
-!        USE GlobalScalars
-!        IMPLICIT NONE
-!        TYPE(BSET),                INTENT(IN)    :: BS
-!        TYPE(DBL_RNK4),            INTENT(IN)    :: MD
-!        TYPE(Pair),                INTENT(IN)    :: Pair
-!        REAL(DOUBLE),DIMENSION(Pair%NA*Pair%NB)  :: TVck 
-!     END SUBROUTINE TBlok
-!--------------------------------------------------------
-! 
-#ifdef PARALLEL
-  TYPE(DBCSR)         :: P
+  TYPE(DBCSR)         :: T1,P
 #else
-  TYPE(BCSR)          :: P
+  TYPE(BCSR)          :: T1,P
 #endif
 #ifdef PERIODIC 
-  LOGICAL             :: NotMakeBlock,InitBlock
-  INTEGER             :: NC,NCells,NX,NY,NZ,I
+  LOGICAL             :: NotMakeBlock
+  INTEGER             :: NC,NLay,MMLow,MMHig,MaxL
   REAL(DOUBLE)        :: Bx,By,Bz
 #endif
   TYPE(AtomPair)      :: Pair
-!
   TYPE(BSET)          :: BS
   TYPE(CRDS)          :: GM
   TYPE(DBL_RNK4)      :: MD
-  TYPE(DBL_RNK2)      :: TrPT
-!
   TYPE(ARGMT)         :: Args
-  INTEGER             :: PP,R,AtA,AtB,NN,IStrtP,IStopP,LP,JP,MB,MA
-                         
+  INTEGER             :: PP,R,AtA,AtB,NN,iSwitch,IStrtP,IStopP,LP,JP,MB,MA
+  REAL(DOUBLE)        :: E_Nuc_Tot,E_Nuc_TotX,E_Nuc_TotY,E_Nuc_TotZ,VBlokX,VBlokY,VBlokZ
+  TYPE(HGRho)         :: Rho
+  TYPE(DBL_RNK2)      :: TrPdT
   CHARACTER(LEN=5),PARAMETER :: Prog='GradT'
-!--------------------------------------- 
+!---------------------------------------------- 
 ! Start up macro
 !
   CALL StartUp(Args,Prog)
 !----------------------------------------------
 ! Get basis set and geometry
 !
-  CALL Get(BS)
-  CALL Get(GM)
+  CALL Get(BS,Tag_O=CurBase)
+  CALL Get(GM,Tag_O=CurGeom)
 !---------------------------------------------- 
 ! Allocations 
 !
   CALL New(MD,(/3,BS%NASym+4,BS%NASym+4,2*BS%NASym+4/),(/1,-2,-2,0/))
   CALL New(P)
   CALL Get(P,TrixFile('D',Args,1))
-  CALL New(TrPT,(/3,NAtoms/))
-!-----------------------------------------------
-! Set the Distance Overlap Threshhold
-!-----------------------------------------------
-  CALL SetOverLapDist(Thresholds%Dist)
+  CALL New(TrPdT,(/3,NAtoms/))
+  CALL NewGradBraKetBlok(BS)
 !-----------------------------------------------
 ! Initialize the matrix and associated indecies
 !
@@ -84,13 +66,22 @@ PROGRAM GradT
 #ifdef PERIODIC
 !-----------------------------------------------
 ! Calculate the Number of Cells
-!
+!-----------------------------------------------
   CALL SetCellNumber(GM)
+!-----------------------------------------------
+! Set Up the Multipoles  
+!-----------------------------------------------
+  MaxL = 32
+  CALL MMSetup(MaxL,GM,Rho)
+!-----------------------------------------------
+! Calculate the Multipoles for Rho
+!-----------------------------------------------
+  CALL CalMMRho(Rho)
 #endif
 !-----------------------------------------------
-! Main loops
+!  Main loops over P
 !
-  TrPT%D=Zero
+  TrPdT%D=Zero
 #ifdef PARALLEL
   DO AtA=Beg%I(MyId),End%I(MyId)
 #else
@@ -104,18 +95,60 @@ PROGRAM GradT
         PP=P%BlkPt%I(JP)
         MB=BSiz%I(AtB)
         IF(SetAtomPair(GM,BS,AtB,AtA,Pair)) THEN
-           TrPT%D(:,AtA)=TrPT%D(:,AtA) + &
+#ifdef PERIODIC
+           Bx = Pair%B(1)
+           By = Pair%B(2)           
+           Bz = Pair%B(3)
+           NotMakeBlock = .TRUE.
+           DO NC = 1,CS%NCells
+              Pair%B(1) = Bx+CS%CellCarts%D(1,NC)
+              Pair%B(2) = By+CS%CellCarts%D(2,NC)
+              Pair%B(3) = Bz+CS%CellCarts%D(3,NC)
+              Pair%AB2  = (Pair%A(1)-Pair%B(1))**2 &
+                        + (Pair%A(2)-Pair%B(2))**2 &
+                        + (Pair%A(3)-Pair%B(3))**2
+              IF(TestAtomPair(Pair)) THEN
+                 TrPdT%D(:,AtA)=TrPdT%D(:,AtA) + &
+                      GradTBlok(BS,MD,AtB,AtA,AtA,P%MTrix%D(PP:PP+MA*MB-1),Pair)
+                 IF (AtA.NE.AtB) THEN
+                    TrPdT%D(:,AtB)=TrPdT%D(:,AtB) + &
+                         GradTBlok(BS,MD,AtB,AtA,AtB,P%MTrix%D(PP:PP+MA*MB-1),Pair)
+                 ENDIF
+              ENDIF
+           ENDDO
+#else
+           TrPdT%D(:,AtA)=TrPdT%D(:,AtA) + &
                 GradTBlok(BS,MD,AtB,AtA,AtA,P%MTrix%D(PP:PP+MA*MB-1),Pair)
            IF (AtA.NE.AtB) THEN
-              TrPT%D(:,AtB)=TrPT%D(:,AtB) + &
+              TrPdT%D(:,AtB)=TrPdT%D(:,AtB) + &
                    GradTBlok(BS,MD,AtB,AtA,AtB,P%MTrix%D(PP:PP+MA*MB-1),Pair)
            ENDIF
         ENDIF
+#endif
      ENDDO
   ENDDO
+  DO AtA=1,NAtoms
+     WRITE(*,*) TrPdT%D(1,AtA),TrPdT%D(2,AtA),TrPdT%D(3,AtA)
+  ENDDO
 !------------------------------------------------------------
+! Put P to disk
+!  
+  CALL Filter(T1,P)
+!  CALL Put(T1,TrixFile('W',Args),BlksName_O='nsi',Non0Name_O='nsm')
+!-----------------------------------------------------------
+! Printing
+!
+  CALL PChkSum(T1,'P',Prog)
+  CALL PPrint( T1,'P')
+  CALL Plot(   T1,'P')
+
+!---------------------------------------------------
 ! Tidy up
 ! 
+  CALL Delete(T1)
+  CALL Delete(P)
+  CALL DeleteGradBraKetBlok()
+  CALL Delete_HGRho(Rho)
   CALL Delete(BS)
   CALL Delete(GM)
   CALL Delete(MD)
