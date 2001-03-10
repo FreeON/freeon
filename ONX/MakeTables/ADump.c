b9f1740580a04fc98cd44dedca92b5bf6532ab52
schwegle <schwegle>
2001-03-10 18:34:47 +0000

    New binary gamma table format for NONX

--- ONX/MakeTables/ADump.c
+++ ONX/MakeTables/ADump.c
@@ -2,40 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-void GDump(int *L, int *Length, int *Table)
+void ADump(int *Ltot, int *GammaA)
 {
     char* GammaHome;
-    char SL[3];
     char FileName[100];
     char* Mode = "wb";
     FILE* InFile;
 
     if ((GammaHome = getenv("MONDO_HOME")) == NULL) {
-       printf(" Error in GDump.\n");
+       printf(" Error in ADump.\n");
        printf(" The MONDO_HOME environment variable is not set.\n");
        exit( EXIT_FAILURE );
     }
     
-    sprintf(SL,"%d",*L);
-    sprintf(FileName,"%s/MondoMods/MMA/Functions/Gamma_%s.bin",GammaHome,SL);
+    sprintf(FileName,"%s/MondoMods/MMA/Functions/Gamma_A.bin",GammaHome);
 
     if((InFile = fopen(FileName,Mode)) == NULL) {
        printf(" %s\n",FileName);
-       printf(" Error trying to create a binary Gamma table.\n");
+       printf(" Error trying to create a binary Gamma asymptotics table.\n");
        exit( EXIT_FAILURE );
     }
 
-     fwrite(Table,sizeof(double),(*Length),InFile);
+     fwrite(Ltot,sizeof(int),1,InFile);
+     fwrite(GammaA,sizeof(double),(*Ltot)+1,InFile);
      fclose(InFile);
 }
 
-void gdump_(int *L, int *Length, int *Table)
+void adump_(int *Ltot, int *GammaA)
 {
-    GDump(L, Length, Table);
+    ADump(Ltot,GammaA);
 }
 
-void gdump(int *L, int *Length, int *Table)
+void adump(int *Ltot, int *GammaA)
 {
-    GDump(L, Length, Table);
+    ADump(Ltot,GammaA);
 }
 
