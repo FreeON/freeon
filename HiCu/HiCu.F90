058b6f8cb29ee7c7c1347db3459add67e016849f
mchalla <mchalla>
2001-04-16 21:16:22 +0000

    Added global CubeRoot.

--- HiCu/HiCu.F90
+++ HiCu/HiCu.F90
@@ -1,13 +1,34 @@
+!------------------------------------------------------------------------------
+!--  This code is part of the MondoSCF suite of programs for linear scaling 
+!    electronic structure theory and ab initio molecular dynamics.
 !
-!--  This source code is part of the MondoSCF suite of 
-!--  linear scaling electronic structure codes.  
-!
-!--  Matt Challacombe
-!--  Los Alamos National Laboratory
-!--  Copyright 2000, The University of California
-!
-!    COMPUTE THE EXCHANGE CORRELATION MATRIX $K_{xc}$ USING HIERARCHICAL CUBATURE
-!
+!--  Copyright (c) 2001, the Regents of the University of California.  
+!    This SOFTWARE has been authored by an employee or employees of the 
+!    University of California, operator of the Los Alamos National Laboratory 
+!    under Contract No. W-7405-ENG-36 with the U.S. Department of Energy.  
+!    The U.S. Government has rights to use, reproduce, and distribute this 
+!    SOFTWARE.  The public may copy, distribute, prepare derivative works 
+!    and publicly display this SOFTWARE without charge, provided that this 
+!    Notice and any statement of authorship are reproduced on all copies.  
+!    Neither the Government nor the University makes any warranty, express 
+!    or implied, or assumes any liability or responsibility for the use of 
+!    this SOFTWARE.  If SOFTWARE is modified to produce derivative works, 
+!    such modified SOFTWARE should be clearly marked, so as not to confuse 
+!    it with the version available from LANL.  The return of derivative works
+!    to the primary author for integration and general release is encouraged. 
+!    The first publication realized with the use of MondoSCF shall be
+!    considered a joint work.  Publication of the results will appear
+!    under the joint authorship of the researchers nominated by their
+!    respective institutions. In future publications of work performed
+!    with MondoSCF, the use of the software shall be properly acknowledged,
+!    e.g. in the form "These calculations have been performed using MondoSCF, 
+!    a suite of programs for linear scaling electronic structure theory and
+!    ab initio molecular dynamics", and given appropriate citation.  
+!------------------------------------------------------------------------------
+!    Author: Matt Challacombe
+!    COMPUTE THE EXCHANGE CORRELATION MATRIX $K_{xc}$ IN O(N)
+!    USING HIERARCHICAL CUBATURE INVOLVING K-D BINARY TREES
+!------------------------------------------------------------------------------
 PROGRAM HaiKu
   USE DerivedTypes
   USE GlobalScalars
@@ -26,7 +47,6 @@ PROGRAM HaiKu
   USE KxcGen
   IMPLICIT NONE
   TYPE(ARGMT)                 :: Args
-  TYPE(CubeNode), POINTER     :: CubeRoot
   TYPE(BCSR)                  :: Kxc,T1
   TYPE(TIME)                  :: TimeRhoToTree,TimeGridGen, &
                                  TimeExcWalk,TimeKxcGen
@@ -43,7 +63,6 @@ PROGRAM HaiKu
 ! Get basis set, geometry, thresholds and model type
   CALL Get(BS,CurBase)
   CALL Get(GM,CurGeom)
-  CALL NewBraKetBlok(BS)
   CALL Get(ModelChem,'ModelChemistry',CurBase)
   NEl=GM%NElec
 #ifdef PERIODIC
@@ -53,11 +72,14 @@ PROGRAM HaiKu
 ! Convert density to a 5-D BinTree
   CALL RhoToTree(Args)
 ! Generate the grid as a 3-D BinTree 
-  CALL GridGen(CubeRoot)
+  CALL GridGen()
 ! Delete the density
   CALL DeleteRhoTree(RhoRoot)
 ! Compute the exchange correlation matirix Kxc
+  CALL New(Kxc)
+  CALL NewBraBlok(BS)
   CALL MakeKxc(Kxc,CubeRoot)
+  CALL DeleteBraBlok()
 ! Put Kxc to disk
   CALL Filter(T1,Kxc)
   CALL Put(T1,TrixFile('Kxc',Args,0))
