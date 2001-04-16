80a16c586af5e118d3f22ad2b28aa500dc90dbe0
mchalla <mchalla>
2001-04-16 21:17:11 +0000

    Uses TreeWalk now.

--- HiCu/KxcGen.F90
+++ HiCu/KxcGen.F90
@@ -8,25 +8,10 @@ MODULE KxcGen
   USE InOut
   USE Thresholding
   USE AtomPairs
-  USE BraKetBloks
+  USE BraBloks
   USE CubeTree 
+  USE TreeWalk
   IMPLICIT NONE
-  LOGICAL PrintFlag
-!--------------------------------------------------------------------------
-! Primative Atom Pair Type
-!
-  TYPE PrimPair
-     INTEGER                        :: Ell  
-     REAL(DOUBLE)                   :: Z
-     REAL(DOUBLE),DIMENSION(3)      :: P
-     REAL(DOUBLE),DIMENSION(HGLen)  :: Ket
-     REAL(DOUBLE)                   :: Extent
-     TYPE(BBox)                     :: Box
-  ENDTYPE PrimPair
-!---------------------------------------
-! Global primitive 
-!
-  TYPE(PrimPair)                 :: Prim
 !----------!
   CONTAINS !
 !=============================================================================================
@@ -45,9 +30,6 @@ MODULE KxcGen
       LOGICAL                :: NotMakeBlock
 #endif     
 !---------------------------------------------- 
-!     Allocations 
-      CALL New(MD,(/3,BS%NASym,BS%NASym,2*BS%NASym/),(/1,0,0,0/))
-      CALL New(Kxc)
 !     Initialize the matrix and associated indecies
       P=1; 
       R=1; 
@@ -125,7 +107,6 @@ MODULE KxcGen
             ENDIF
          ENDDO
       ENDDO
-!
     END SUBROUTINE MakeKxc
 !=============================================================================================
 !
@@ -165,7 +146,13 @@ MODULE KxcGen
        KBlk=Zero
        KA=Pair%KA
        KB=Pair%KB
-!
+!----------------------------------
+       Prim%A=Pair%A
+       Prim%B=Pair%B
+       Prim%AB2=Pair%AB2
+       Prim%KA=Pair%KA
+       Prim%KB=Pair%KB
+!----------------------------------
        IndexA=0                  
        DO CFA=1,BS%NCFnc%I(KA)    
        DO CFB=1,BS%NCFnc%I(KB) 
@@ -177,59 +164,34 @@ MODULE KxcGen
           StopLB  = BS%LStop%I(CFB,KB)
           MaxLA   = BS%ASymm%I(2,CFA,KA)
           MaxLB   = BS%ASymm%I(2,CFB,KB)
+!----------------------------------
+          Prim%CFA=CFA
+          Prim%CFB=CFB
+          Prim%Ell=MaxLA+MaxLB
+!----------------------------------
           DO PFA=1,BS%NPFnc%I(CFA,KA)          
           DO PFB=1,BS%NPFnc%I(CFB,KB)
-             ZetaA=BS%Expnt%D(PFA,CFA,KA)
-             ZetaB=BS%Expnt%D(PFB,CFB,KB)
-             EtaAB=ZetaA+ZetaB 
-             EtaIn=One/EtaAB
-             XiAB =ZetaA*ZetaB*EtaIn
-!            Primitive thresholding                      
-             IF(TestPrimPair(XiAB,Pair%AB2))THEN
+!----------------------------------
+             Prim%ZA=BS%Expnt%D(PFA,CFA,KA)
+             Prim%ZB=BS%Expnt%D(PFB,CFB,KB)
+             Prim%Zeta=Prim%ZA+Prim%ZB
+             Prim%Xi=Prim%ZA*Prim%ZB/Prim%Zeta
+             IF(TestPrimPair(Prim%Xi,Prim%AB2))THEN
+                Prim%PFA=PFA 
+                Prim%PFB=PFB
 !               Set primitive values
-                ExpAB=EXP(-XiAB*Pair%AB2)
-                Prim%P(1)=(ZetaA*Pair%A(1)+ZetaB*Pair%B(1))*EtaIn
-                Prim%P(2)=(ZetaA*Pair%A(2)+ZetaB*Pair%B(2))*EtaIn
-                Prim%P(3)=(ZetaA*Pair%A(3)+ZetaB*Pair%B(3))*EtaIn
-                Prim%Ell=MaxLA+MaxLB
-                Prim%Z=EtaAB
-                PAx=Prim%P(1)-Pair%A(1)
-                PAy=Prim%P(2)-Pair%A(2)
-                PAz=Prim%P(3)-Pair%A(3)
-                PBx=Prim%P(1)-Pair%B(1)
-                PBy=Prim%P(2)-Pair%B(2)
-                PBz=Prim%P(3)-Pair%B(3)
-!               McMurchie Davidson prefactors for HG Primitives
-                CALL MD2TRR(BS%NASym,0,MaxLA,MaxLB,EtaAB,MD%D,PAx,PBx,PAy,PBy,PAz,PBz) 
-!               Primitive coefficients in a HG representationj
-                CALL SetBraBlok(CFA,PFA,KA,CFB,PFB,KB,ExpAB,BS,MD)
-!               Find the max amplitude for this primitive distribution
-                MaxAmp=Zero
-                IA = IndexA
-                DO LMNA=StartLA,StopLA
-                   IA=IA+1
-                   IB=IndexB
-                   EllA=BS%LxDex%I(LMNA)+BS%LyDex%I(LMNA)+BS%LzDex%I(LMNA)                         
-                   DO LMNB=StartLB,StopLB
-                      IB=IB+1
-                      Amp2=Zero
-                      EllB=BS%LxDex%I(LMNB)+BS%LyDex%I(LMNB)+BS%LzDex%I(LMNB)                         
-                      DO LMN=1,LHGTF(EllA+EllB)
-                         Amp2=Amp2+HGBra%D(LMN,IA,IB)**2
-                      ENDDO
-                      MaxAmp=MAX(MaxAmp,SQRT(Amp2))
-                   ENDDO
-                ENDDO
+                MaxAmp=SetBraBlok(Prim,BS)
+!---------------------------------------------------------------------------
                 MaxAmp=LOG(MaxAmp)
 !               Evaluate this primitives Ket contribution to Kxc_ab
-                Prim%Ket=Zero
-                Prim%Box%BndBox(1,:)=Prim%P(1)
-                Prim%Box%BndBox(2,:)=Prim%P(2)
-                Prim%Box%BndBox(3,:)=Prim%P(3)
-                Prim%Extent=GaussianExtent(Prim%Z,MaxAmp)
-                Prim%Box=ExpandBox(Prim%Box,Prim%Extent)
-!               Walk the CubeTree
-                CALL PrimKxcWalk(CubeRoot)!,Prim)
+                Ket=Zero
+                PBox%BndBox(1,:)=Prim%P(1)
+                PBox%BndBox(2,:)=Prim%P(2)
+                PBox%BndBox(3,:)=Prim%P(3)
+                PExtent=GaussianExtent(Prim%Zeta,MaxAmp)
+                PBox=ExpandBox(PBox,PExtent)
+!               Walk the walk
+                CALL KxcWalk(CubeRoot)
 !               Contract <Bra|Ket> bloks to compute matrix elements of Kxc
                 IA = IndexA
                 DO LMNA=StartLA,StopLA
@@ -240,11 +202,12 @@ MODULE KxcGen
                       IB=IB+1
                       EllB=BS%LxDex%I(LMNB)+BS%LyDex%I(LMNB)+BS%LzDex%I(LMNB)                         
                       DO LMN=1,LHGTF(EllA+EllB)
-                         Kblk(IA,IB)=Kblk(IA,IB)+HGBra%D(LMN,IA,IB)*Prim%Ket(LMN)
+                         Kblk(IA,IB)=Kblk(IA,IB)+HGBra%D(LMN,IA,IB)*Ket(LMN)
                       ENDDO
-                  ENDDO
-                  ENDDO
-             ENDIF !End primitive thresholding
+                   ENDDO
+                ENDDO
+!---------------------------------------------------------------------------
+             ENDIF 
           ENDDO 
           ENDDO
        ENDDO
@@ -254,98 +217,4 @@ MODULE KxcGen
 !====================================================================================================
 !
 !====================================================================================================
-     RECURSIVE SUBROUTINE PrimKxcWalk(Cube)!,Prim)
-       TYPE(CubeNode), POINTER   :: Cube
-!       TYPE(PrimPair)            :: Prim
-!
-       REAL(DOUBLE)              :: T
-!-------------------------------------------------------------------------------------
-!        Is the BBox containing this primitive distribution outside this nodes BBox? 
-!
-       T=ABS(Prim%Box%Center(1)-Cube%Box%Center(1))
-       IF(T>Prim%Box%Half(1)+Cube%Box%Half(1))RETURN
-       T=ABS(Prim%Box%Center(2)-Cube%Box%Center(2))
-       IF(T>Prim%Box%Half(2)+Cube%Box%Half(2))RETURN
-       T=ABS(Prim%Box%Center(3)-Cube%Box%Center(3))
-       IF(T>Prim%Box%Half(3)+Cube%Box%Half(3))RETURN
-
-!       IF(BoxInBox(Prim%Box,Cube%Box))THEN
-          IF(Cube%Leaf)THEN
-             CALL KxcCube(Cube)!,Prim)
-          ELSE
-             CALL PrimKxcWalk(Cube%Descend)!,Prim)
-             CALL PrimKxcWalk(Cube%Descend%Travrse)!,Prim)
-          ENDIF
-!       ENDIF
-     END SUBROUTINE PrimKxcWalk
-!====================================================================================================
-!
-!====================================================================================================
-     SUBROUTINE KxcCube(Cube)!,Prim)
-       TYPE(CubeNode), POINTER               :: Cube
-!       TYPE(PrimPair)                        :: Prim
-! 
-       REAL(DOUBLE), DIMENSION(0:HGEll+1)    :: LambdaX,LambdaY,LambdaZ
-       REAL(DOUBLE)                          :: Z,X,RPx,RPy,RPz,RP2,Xpt,TwoZ,  &
-                                                Co,RL1,Dist, Wght,dEdRho,    &
-                                                dEdAbsGradRho2,GradBraRhoDot,&
-                                                GradRhoX,GradRhoY,GradRhoZ,  &
-                                                PrimDist,GradPrimDistX,      &
-                                                GradPrimDistY,GradPrimDistZ 
-       INTEGER                               :: IC,IQ,JC,JQ,KC,KQ,NQ,Ell,    &
-                                                I,J,L,M,N,L1,L2,LMN,LMNLen
-
-       REAL(DOUBLE), DIMENSION(50) :: Gx,Gy,Gz
-       REAL(DOUBLE)                :: GradX,GradY,GradZ
-       INTEGER                     :: IA,IB,EllA,EllB,LMNA,LMNB
-
-!
-#ifdef EXPLICIT_SOURCE
-       INCLUDE 'ExplicitBraElements.Inc'
-#else
-       DO I=1,NGrid
-          RPx=Cube%Grid(I,1)-Prim%P(1)
-          RPy=Cube%Grid(I,2)-Prim%P(2)
-          RPz=Cube%Grid(I,3)-Prim%P(3)
-          RP2=RPx**2+RPy**2+RPz**2
-          Dist=Z*RP2
-          Xpt=EXP(-Prim%Z*RP2)
-          IF(Xpt>1.D-14)THEN
-          TwoZ=Two*Prim%Z
-          LambdaX(0)=One
-          LambdaY(0)=One
-          LambdaZ(0)=One
-          LambdaX(1)=TwoZ*RPx 
-          Lambday(1)=TwoZ*RPy
-          LambdaZ(1)=TwoZ*RPz
-          DO L=2,Prim%Ell+1
-             L1=L-1
-             L2=L-2
-             RL1=DBLE(L1)
-             LambdaX(L)=TwoZ*(RPx*LambdaX(L1)-RL1*LambdaX(L2))
-             LambdaY(L)=TwoZ*(RPy*LambdaY(L1)-RL1*LambdaY(L2))
-             LambdaZ(L)=TwoZ*(RPz*LambdaZ(L1)-RL1*LambdaZ(L2))
-          ENDDO
-          DO L=0,Prim%Ell
-             DO M=0,Prim%Ell-L
-                DO N=0,Prim%Ell-M-L
-                   LMN=LMNDex(L,M,N)
-                   PrimDist     = LambdaX(L  )*LambdaY(M  )*LambdaZ(N  )*Xpt
-                   GradPrimDistX=-LambdaX(L+1)*LambdaY(M  )*LambdaZ(N  )*Xpt
-                   GradPrimDistY=-LambdaX(L  )*LambdaY(M+1)*LambdaZ(N  )*Xpt
-                   GradPrimDistZ=-LambdaX(L  )*LambdaY(M  )*LambdaZ(N+1)*Xpt
-!                  Prim_Kxc(LMN)=Prim_Kxc(LMN)+w_i*(dE/dRho-2dE/d(GradRho)^2 Phi_a*Phi_B GradRho.Grad(Phi_a*Phi_B)
-                   Prim%Ket(LMN)=Prim%Ket(LMN)+Cube%Wght(I)        &
-                                   *(Cube%Vals(I,1)*PrimDist       & 
-                                    +Cube%Vals(I,2)*(              &
-                                     Cube%Vals(I,3)*GradPrimDistX  &
-                                    +Cube%Vals(I,4)*GradPrimDistY  &
-                                    +Cube%Vals(I,5)*GradPrimDistZ))
-                ENDDO
-             ENDDO
-          ENDDO
-          ENDIF
-       ENDDO
-#endif
-     END SUBROUTINE KxcCube
 END MODULE KxcGen
