9894309b4626158a1fd55b39e85071683880f8a7
mchalla <mchalla>
2001-04-17 18:07:08 +0000

    Added * as a Special character

--- MondoMods/ParsingConstants.F90
+++ MondoMods/ParsingConstants.F90
@@ -13,8 +13,8 @@ MODULE ParsingConstants
    CHARACTER(LEN=26), PARAMETER   :: Upper='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
    CHARACTER(LEN=26), PARAMETER   :: Lower='abcdefghijklmnopqrstuvwxyz'
    CHARACTER(LEN=23), PARAMETER   :: Numbers='0123456789-+.0123456789'
-   CHARACTER(LEN=4),  PARAMETER   :: Special='$/_#'
-   CHARACTER(LEN=53), PARAMETER   :: Characters=Special//Lower//Numbers
+   CHARACTER(LEN=5),  PARAMETER   :: Special='$/_#*'
+   CHARACTER(LEN=54), PARAMETER   :: Characters=Special//Lower//Numbers
    CHARACTER(LEN=9),  PARAMETER   :: Delimeters='[(|")],= '
    CHARACTER(LEN=4) , PARAMETER   :: Stars='****'
    CHARACTER(LEN=1) , PARAMETER   :: Space=' '
