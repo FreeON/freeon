d3e3e59359c9e737cc8a584400782adf411002ec
mchalla <mchalla>
2001-04-17 20:55:03 +0000

    Module for block trace of S force.

---
+++ OneE/BlokTrPdT.F90
@@ -0,0 +1,238 @@
+!------------------------------------------------------------------------------
+!--  This code is part of the MondoSCF suite of programs for linear scaling 
+!    electronic structure theory and ab initio molecular dynamics.
+!
+!--  Copyright (c) 2001, the Regents of the University of California.  
+!    This SOFTWARE has been authored by an employee or employees of the 
+!    University of California, operator of the Los Alamos National Laboratory 
+!    under Contract No. W-7405-ENG-36 with the U.S. Department of Energy.  
+!    The U.S. Government has rights to use, reproduce, and distribute this 
+!    SOFTWARE.  The public may copy, distribute, prepare derivative works 
+!    and publicly display this SOFTWARE without charge, provided that this 
+!    Notice and any statement of authorship are reproduced on all copies.  
+!    Neither the Government nor the University makes any warranty, express 
+!    or implied, or assumes any liability or responsibility for the use of 
+!    this SOFTWARE.  If SOFTWARE is modified to produce derivative works, 
+!    such modified SOFTWARE should be clearly marked, so as not to confuse 
+!    it with the version available from LANL.  The return of derivative works
+!    to the primary author for integration and general release is encouraged. 
+!    The first publication realized with the use of MondoSCF shall be
+!    considered a joint work.  Publication of the results will appear
+!    under the joint authorship of the researchers nominated by their
+!    respective institutions. In future publications of work performed
+!    with MondoSCF, the use of the software shall be properly acknowledged,
+!    e.g. in the form "These calculations have been performed using MondoSCF, 
+!    a suite of programs for linear scaling electronic structure theory and
+!    ab initio molecular dynamics", and given appropriate citation.  
+!------------------------------------------------------------------------------
+!    Author: Matt Challacombe
+!    BLOKWISE ACCUMULATION OF Tr{P_(A,B).dT^T_(A,B)/dA}
+!------------------------------------------------------------------------------
+MODULE BlokTrPdT
+  USE DerivedTypes
+  USE GlobalScalars
+  USE PrettyPrint
+  USE McMurchie
+  USE AtomPairs
+  IMPLICIT NONE
+
+  CONTAINS
+     FUNCTION TrPdT(BS,Pair,P,AtA,AtB) RESULT(Vck)
+     TYPE(BSET)                                      :: BS
+     TYPE(AtomPair)                                  :: Pair
+     REAL(DOUBLE),DIMENSION(3)                       :: Vck
+     REAL(DOUBLE),DIMENSION(Pair%NA,Pair%NB)         :: P
+     REAL(DOUBLE),DIMENSION(Pair%NA,Pair%NB,3)       :: dTBlk
+     REAL(DOUBLE),DIMENSION(3,0:BS%NASym,0:BS%NASym) :: T,dT
+     REAL(DOUBLE),DIMENSION(3,-1:BS%NASym+1, &
+                              -1:BS%NASym+1)         :: dE
+     REAL(DOUBLE),DIMENSION(3,-1:BS%NASym+2, &
+                              -1:BS%NASym+2, &
+                              -1:2*BS%NAsym+4)       :: E
+
+!
+     INTEGER                                   :: NBFA,NBFB,KA,KB,KK,AtA,AtB
+     REAL(DOUBLE)                              :: Ax,Ay,Az,Bx,By,Bz,AB2 
+     REAL(DOUBLE)                              :: Px,Py,Pz,PAx,PAy,PAz,PBx,PBy,PBz,ZetaA,ZetaB,  &
+                                                  EtaAB,EtaIn,ZAB,XiAB,ExpAB,Ov,PiE32,CA,CB,DLA,DLB
+     REAL(DOUBLE)                              :: Txx,Txy,Txz,Tyx,Tyy,Tyz,Tzx,Tzy,Tzz
+     INTEGER                                   :: CFA,CFB,PFA,PFB,IndexA,IndexB,StartLA,StartLB, &
+                                                 StopLA,StopLB,MaxLA,MaxLB,IA,IB,LMNA,LMNB,     &
+                                                 LA,LB,MA,MB,NA,NB,K
+     REAL(DOUBLE), EXTERNAL                    :: BlkTrace_2 
+!-------------------------------------------------------------------------------------------------
+     KA   = Pair%KA
+     KB   = Pair%KB
+     NBFA = Pair%NA
+     NBFB = Pair%NB
+     Ax = Pair%A(1)
+     Ay = Pair%A(2)
+     Az = Pair%A(3)
+     Bx = Pair%B(1)
+     By = Pair%B(2)
+     Bz = Pair%B(3)
+     AB2=Pair%AB2
+     dTBlk=Zero
+!------------------------------------------------------------------------------------------
+     DO CFA=1,BS%NCFnc%I(KA) 
+     IndexA  = CFBlokDex(BS,CFA,KA)
+     StartLA = BS%LStrt%I(CFA,KA)        
+     StopLA  = BS%LStop%I(CFA,KA)
+     MaxLA   = BS%ASymm%I(2,CFA,KA)
+     DO CFB=1,BS%NCFnc%I(KB) 
+        IndexB  = CFBlokDex(BS,CFB,KB)
+        StartLB = BS%LStrt%I(CFB,KB)
+        StopLB  = BS%LStop%I(CFB,KB)
+        MaxLB   = BS%ASymm%I(2,CFB,KB)   
+        DO PFA=1,BS%NPFnc%I(CFA,KA)
+        DO PFB=1,BS%NPFnc%I(CFB,KB)
+           ZetaA=BS%Expnt%D(PFA,CFA,KA)
+           ZetaB=BS%Expnt%D(PFB,CFB,KB)
+           EtaAB=ZetaA+ZetaB 
+           EtaIn=One/EtaAB
+           ZAB  =ZetaA*ZetaB
+           XiAB =ZAB*EtaIn
+           ExpAB=EXP(-XiAB*AB2)
+           PiE32=(Pi/EtaAB)**(1.5D0)
+           Ov=ExpAB*PiE32
+           Px=(ZetaA*Ax+ZetaB*Bx)*EtaIn
+           Py=(ZetaA*Ay+ZetaB*By)*EtaIn
+           Pz=(ZetaA*Az+ZetaB*Bz)*EtaIn
+           PAx=Px-Ax
+           PAy=Py-Ay
+           PAz=Pz-Az
+           PBx=Px-Bx
+           PBy=Py-By
+           PBz=Pz-Bz               
+           CALL MD2TRR(BS%NASym+2,-1,MaxLA+2,MaxLB+2,EtaAB,E,PAx,PBx,PAy,PBy,PAz,PBz) 
+!------------------------------------------------------------------------------------              
+!          Naive derivatives of the McMurchie Davidson E and T coefficients WRT A
+!          See Helgaker and Taylor, TCA v.83, p177 (1992)
+           dE=Zero
+           IF(.NOT.Pair%SameAtom)THEN
+              DO LA=0,MaxLA+1
+              DO LB=0,MaxLB+1
+                 DO K=1,3
+                    dE(K,LA,LB)=Two*ZetaA*E(K,LA+1,LB,0)-DBLE(LA)*E(K,LA-1,LB,0)
+                 ENDDO
+              ENDDO
+              ENDDO
+           ENDIF
+!------------------------------------------------------------------------------------              
+!          Build McMurchie Davidson style T coefficients and their derivatives.
+!          Note that dT_Ak/dAi=0 if k/=i
+           DO LA=0,MaxLA
+           DO LB=0,MaxLB
+              DLA=DBLE(LA) 
+              DLB=DBLE(LB)
+              DO K=1,3
+                 T(K,LA,LB)=        DLA*DLB*E(K,LA-1,LB-1,0) &
+                           +       Four*ZAB*E(K,LA+1,LB+1,0) &
+                           -  ZetaA*Two*DLB*E(K,LA+1,LB-1,0) &
+                           -  ZetaB*Two*DLA*E(K,LA-1,LB+1,0) 
+
+                 dT(K,LA,LB)=      DLA*DLB*dE(K,LA-1,LB-1) &
+                            +     Four*ZAB*dE(K,LA+1,LB+1) &
+                            -ZetaA*Two*DLB*dE(K,LA+1,LB-1) &
+                            -ZetaB*Two*DLA*dE(K,LA-1,LB+1) 
+              ENDDO
+           ENDDO
+           ENDDO
+!------------------------------------------------------------------------------------
+           IB=IndexB
+           DO LMNB=StartLB,StopLB
+              LB=BS%LxDex%I(LMNB)
+              MB=BS%LyDex%I(LMNB)
+              NB=BS%LzDex%I(LMNB)
+              CB=BS%CCoef%D(LMNB,PFB,CFB,KB)
+              IB=IB+1
+              IA=IndexA
+              DO LMNA=StartLA,StopLA
+                 IA=IA+1
+                 LA=BS%LxDex%I(LMNA)
+                 MA=BS%LyDex%I(LMNA)
+                 NA=BS%LzDex%I(LMNA)
+                 CA=BS%CCoef%D(LMNA,PFA,CFA,KA)
+!IF(AtA==1.AND.AtB==2.AND.CFA==1.AND.CFB==1)THEN
+!WRITE(*,*)' '
+!WRITE(*,*)' '
+!WRITE(*,*)' '
+!           WRITE(*,*)' CFA = ',CFA,' PFA = ',PFA,' CFB = ',CFB,' PFB = ',PFB
+!
+!WRITE(*,*)LA,MA,NA,LB,MB,NB
+! WRITE(77,22)AtA,AtB,LA,MA,NA,LB,MB,NB,  &
+!            BS%CCoef%D(LMNA,PFA,CFA,KA),BS%CCoef%D(LMNB,PFB,CFB,KB), &
+!            zetaa,zetab,Pair%A,Pair%B
+! WRITE(*,22)AtA,AtB,LA,MA,NA,LB,MB,NB,  &
+!            BS%CCoef%D(LMNA,PFA,CFA,KA),BS%CCoef%D(LMNB,PFB,CFB,KB), &
+!            zetaa,zetab,Pair%A,Pair%B
+!22 FORMAT('test[',I3,',',I3,']={ la->',I2,',ma->',I2,',na->',I2,           &
+!                               ',lb->',I2,',mb->',I2,',nb->',I2,           &
+!                               ',ca->',F10.7,',cb->',F10.7,               &
+!                               ',za->',F10.7,',zb->',F10.7,               &
+!                               ',ax->',F10.7,',ay->',F10.7,',az->',F10.7, &
+!                               ',bx->',F10.7,',by->',F10.7,',bz->',F10.7,'}; \n')
+
+!                dT/dAx
+                 Txx=dT(1,LA,LB)*   E(2,MA,MB,0)* E(3,NA,NB,0)
+                 Tyx=dE(1,LA,LB)*   T(2,MA,MB)*   E(3,NA,NB,0)
+                 Tzx=dE(1,LA,LB)*   E(2,MA,MB,0)* T(3,NA,NB)
+                 dTBlk(IA,IB,1)=dTBlk(IA,IB,1)+Half*CA*CB*Ov*(Txx+Tyx+Tzx)
+!                dT/dAy
+                 Txy= T(1,LA,LB)*  dE(2,MA,MB)*   E(3,NA,NB,0)   
+                 Tyy= E(1,LA,LB,0)*dT(2,MA,MB)*   E(3,NA,NB,0)
+                 Tzy= E(1,LA,LB,0)*dE(2,MA,MB)*   T(3,NA,NB)
+                 dTBlk(IA,IB,2)=dTBlk(IA,IB,2)+Half*CA*CB*Ov*(Txy+Tyy+Tzy)
+!                dT/dAz
+                 Txz= T(1,LA,LB)*   E(2,MA,MB,0)*dE(3,NA,NB)
+                 Tyz= E(1,LA,LB,0)* T(2,MA,MB)*  dE(3,NA,NB)
+                 Tzz= E(1,LA,LB,0)* E(2,MA,MB,0)*dT(3,NA,NB)
+
+!WRITE(*,*)' ty = ',T(2,MA,MB)
+!WRITE(*,*)' f3 = ',dE(3,NA,NB)
+!WRITE(*,*)' dTxz = ',Txz
+!WRITE(*,*)' dTyz = ',Tyz
+!WRITE(*,*)' dTzz = ',Tzz
+
+                 dTBlk(IA,IB,3)=dTBlk(IA,IB,3)+Half*CA*CB*Ov*(Txz+Tyz+Tzz)
+
+!ENDIF
+              ENDDO
+           ENDDO
+!           CALL  Print_DBL_Rank2A(dTBlk(:,:,3),'dTz',Unit_O=6)
+
+
+
+        ENDDO
+        ENDDO
+     ENDDO
+     ENDDO
+!     WRITE(*,*)'=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+!     PrintFlags%Fmt=DEBUG_DBLSTYLE
+!     CALL  Print_DBL_Rank2A(dTBlk(:,:,3),'dTz',Unit_O=6)
+!     CALL  Print_DBL_Rank2A(P,'P',Unit_O=6)
+!     CALL  Print_DBL_Rank2A(MATMUL(P,TRANSPOSE(dS(:,:,3))),'P.dT^T',Unit_O=6)
+     DO K=1,3
+        Vck(K)=BlkTrace_2(Pair%NA,Pair%NB,P,TRANSPOSE(dTBlk(:,:,K)))
+     ENDDO
+  END FUNCTION TrPdT
+END MODULE BlokTrPdT
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
