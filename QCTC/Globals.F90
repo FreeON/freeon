3a069e3991fd280251534eb466646fb6f63f98cc
tymczak <tymczak>
2001-04-19 17:50:32 +0000

    Resolved conflicts with Matt's version.

--- QCTC/Globals.F90
+++ QCTC/Globals.F90
@@ -2,6 +2,10 @@ MODULE Globals
   USE DerivedTypes
   USE GlobalScalars   
   USE GlobalObjects
+!
   TYPE(BSET)      :: BS
   TYPE(CRDS)      :: GM
+  TYPE(DBL_RNK4)  :: MD
+  TYPE(ARGMT)     :: Args
+  TYPE(HGRho)     :: Rho
 END MODULE Globals
