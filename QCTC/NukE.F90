c324454c8aff822249949b654aad4c3e3cbc7aea
tymczak <tymczak>
2001-05-29 17:08:37 +0000

    Reorganized and removed bugs

--- QCTC/NukE.F90
+++ QCTC/NukE.F90
@@ -19,35 +19,70 @@ MODULE NuklarE
   USE BraBloks
   USE PoleTree 
   USE TreeWalk
+#ifdef PERIODIC
+  USE PBCFarField
+#endif
   IMPLICIT NONE
 !----------!
   CONTAINS !
 !=============================================================================================
 !
 !=============================================================================================
-    FUNCTION NukE() 
-       REAL(DOUBLE) :: NukE,Tau,NukeCo,NukePole
+    FUNCTION NukE()
+       REAL(DOUBLE)                   :: NukE,Tau,NukeCo,NukePole
+       REAL(DOUBLE),DIMENSION(1:1)    :: HGBra
+       REAL(DOUBLE),DIMENSION(0:0)    :: SPBraC,SPBraS
+#ifdef PERIODIC
+       INTEGER                        :: NC
+       REAL(DOUBLE),DIMENSION(3)      :: QC
+#endif
 !---------------------------------------------------------------------------------------------
-!      Set MAC and PAC parameters
-       NukE=Zero
+       NukE=Zero 
        DO At=1,NAtoms
-          NukeCo=-GM%AtNum%I(At)*(NuclearExpnt/Pi)**(ThreeHalves)
-          NukePole=-GM%AtNum%I(At)
-          Tau=Thresholds%TwoE
-          DP2=(GM%AtNum%I(At)/Tau)**(Two/DBLE(SPEll+1))
-          PoleSwitch=MAX(1.D-12,-LOG(Two*Tau/GM%AtNum%I(At)))
+          NukeCo    = -GM%AtNum%I(At)*(NuclearExpnt/Pi)**(ThreeHalves)
+          NukePole  = -GM%AtNum%I(At)
+!         Set MAC and PAC parameters
+          Tau       = Thresholds%TwoE
+          DP2       = (ABS(NukePole)/Tau)**(Two/DBLE(SPEll+1))
+          PoleSwitch= MAX(1.D-12,-LOG(Two*Tau/ABS(NukePole)))
 !         Set atomic "primitive"
+          Prim%Ell  = 0
+          Prim%Zeta = NuclearExpnt
+!         Zero Accumulators
+          HGBra(1)  = NukeCo
+          SPBraC(0) = NukePole
+          SPBraS(0) = Zero
+          HGKet(1)  = Zero
+          SPKetC(0) = Zero
+          SPKetS(0) = Zero
+#ifdef PERIODIC
+          QC(:)=GM%Carts%D(:,At)
+          DO NC=1,CSMM1%NCells
+!            Set Atomic Coordinates
+             Prim%P(:)=QC(:)+CSMM1%CellCarts%D(:,NC)
+!            Walk the walk
+             CALL VWalk(PoleRoot)
+          ENDDO
+!         Reset the Atomic Coordinates
+          Prim%P(:) = QC(:)
+!         Accumulate the atomic contribution
+          NukE=NukE+HGBra(1)*HGKet(1)+SPBraC(0)*SPKetC(0)
+!         Add in the Far Field, Dipole and Quadripole  Correction
+          IF(Dimen > 0) THEN
+             NukE = NukE + CTraxFF(Prim,HGBra) &
+                         + CTraxQ(Prim,HGBra)
+          ENDIF
+#else
+!         Set Atomic Coordinates
           Prim%P=GM%Carts%D(:,At) 
-          Prim%Zeta=NuclearExpnt
-          HGKet(1)=Zero
-          SPKetC(0)=Zero
 !         Walk the walk
           CALL VWalk(PoleRoot)
 !         Accumulate the atomic contribution
-          NukE=NukE+NukeCo*HGKet(1)+NukePole*SPKetC(0)
+          NukE=NukE+HGBra(1)*HGKet(1)+SPBraC(0)*SPKetC(0)
+#endif
        ENDDO
-     END FUNCTION NukE
-!=============================================================================================
 !
-!=============================================================================================
+       WRITE(*,*)' NukeE = ',NukE
+     END FUNCTION NukE
+!
 END MODULE
