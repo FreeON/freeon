a8eb58175bae3c947ba44017934bb22a25d9c92d
mchalla <mchalla>
2001-06-13 21:55:39 +0000

    Thresholds for controling MAC and PAC errors, controled by Thresholds%TwoE

---
+++ QCTC/Thresholds.F90
@@ -0,0 +1,52 @@
+!------------------------------------------------------------------------------
+!--  This code is part of the MondoSCF suite of programs for linear scaling 
+!    electronic structure theory and ab initio molecular dynamics.
+!
+!--  Copyright (c) 2001, the Regents of the University of California.  
+!    This SOFTWARE has been authored by an employee or employees of the 
+!    University of California, operator of the Los Alamos National Laboratory 
+!    under Contract No. W-7405-ENG-36 with the U.S. Department of Energy.  
+!    The U.S. Government has rights to use, reproduce, and distribute this 
+!    SOFTWARE.  The public may copy, distribute, prepare derivative works 
+!    and publicly display this SOFTWARE without charge, provided that this 
+!    Notice and any statement of authorship are reproduced on all copies.  
+!    Neither the Government nor the University makes any warranty, express 
+!    or implied, or assumes any liability or responsibility for the use of 
+!    this SOFTWARE.  If SOFTWARE is modified to produce derivative works, 
+!    such modified SOFTWARE should be clearly marked, so as not to confuse 
+!    it with the version available from LANL.  The return of derivative works
+!    to the primary author for integration and general release is encouraged. 
+!    The first publication realized with the use of MondoSCF shall be
+!    considered a joint work.  Publication of the results will appear
+!    under the joint authorship of the researchers nominated by their
+!    respective institutions. In future publications of work performed
+!    with MondoSCF, the use of the software shall be properly acknowledged,
+!    e.g. in the form "These calculations have been performed using MondoSCF, 
+!    a suite of programs for linear scaling electronic structure theory and
+!    ab initio molecular dynamics", and given appropriate citation.  
+!------------------------------------------------------------------------------
+!    
+!------------------------------------------------------------------------------
+MODULE QCTCThresholds
+   USE Derivedtypes
+   USE GlobalScalars   
+   USE GlobalObjects
+   USE ProcessControl
+   USE Indexing
+   USE Parse
+   USE InOut
+   USE Macros
+   USE Thresholding
+   USE BoundingBox
+   IMPLICIT NONE
+   REAL(DOUBLE) :: TauPAC
+   REAL(DOUBLE) :: TauMAC
+   CONTAINS
+      SUBROUTINE SetLocalThresholds(Tau)
+         REAL(DOUBLE) :: Tau
+!        Local penetration thresholds 
+!        NEEDS TUNING...
+         TauPAC=Tau*1.D-2
+         TauMAC=Tau*1.D-6
+      END SUBROUTINE SetLocalThresholds
+END MODULE
