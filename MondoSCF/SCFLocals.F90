500add7dfea7278baa2b79ae1cb403ba8de2a102
mchalla <mchalla>
2001-09-11 20:51:33 +0000

    changed thresholds yet again.

--- MondoSCF/SCFLocals.F90
+++ MondoSCF/SCFLocals.F90
@@ -74,13 +74,12 @@ MODULE SCFLocals
 !------------------------------------------------------------------------------------------------  
 !  Thresholds (Loose,Good,Tight,VeryTight):
 
-   REAL(DOUBLE),DIMENSION(4) :: TrixNeglect=(/1.D-3, 1.D-4, 1.D-5,  1.D-6 /)
-!   REAL(DOUBLE),DIMENSION(4) :: TrixNeglect=(/1.D-3, 1.D-4, 1.D-5,  1.D-6 /)
-   REAL(DOUBLE),DIMENSION(4) :: CubeNeglect=(/1.D-3, 1.D-5, 1.D-7,  1.D-9 /)
+   REAL(DOUBLE),DIMENSION(4) :: TrixNeglect=(/1.D-4, 1.D-5, 1.D-6,  1.D-7 /)
+   REAL(DOUBLE),DIMENSION(4) :: CubeNeglect=(/1.D-3, 1.D-5, 1.D-7,  1.D-9/)
    REAL(DOUBLE),DIMENSION(4) :: TwoENeglect=(/1.D-6, 1.D-8, 1.D-10, 1.D-12/)
    REAL(DOUBLE),DIMENSION(4) :: DistNeglect=(/1.D-8, 1.D-10,1.D-12, 1.D-14/)
    REAL(DOUBLE),DIMENSION(4) :: ETol       =(/1.D-5, 1.D-7, 1.D-9,  1.D-11/)
-   REAL(DOUBLE),DIMENSION(4) :: DTol       =(/1.D-1, 1.D-3, 1.D-6,  1.D-9 /)
+   REAL(DOUBLE),DIMENSION(4) :: DTol       =(/1.D-2, 1.D-3, 1.D-4,  1.D-5 /)
 !-----------------------------------------------------------------------------------------------
 !  Asymptotic dimensioning parameters for memory limits (needs work)
    REAL(DOUBLE),DIMENSION(4) :: BandWidth=(/7.D2 ,7.D2 ,7.D2 ,7.D2 /)
