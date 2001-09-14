9521bf034dce00e0bf31a363dfc0bf88a8663ff8
mchalla <mchalla>
2001-09-14 20:25:56 +0000

    Again changed thresholds

--- MondoSCF/SCFLocals.F90
+++ MondoSCF/SCFLocals.F90
@@ -72,9 +72,9 @@ MODULE SCFLocals
 !
    END TYPE
 !------------------------------------------------------------------------------------------------  
-!  Thresholds (Loose,Good,Tight,VeryTight):
+!  Thresholds (Loose ~4 digits, Good ~6 digits, Tight ~8 digits, VeryTight ~10 digits):
 
-   REAL(DOUBLE),DIMENSION(4) :: TrixNeglect=(/1.D-4, 1.D-5, 1.D-6,  1.D-7 /)
+   REAL(DOUBLE),DIMENSION(4) :: TrixNeglect=(/1.D-3, 1.D-4, 1.D-5,  1.D-6 /)
    REAL(DOUBLE),DIMENSION(4) :: CubeNeglect=(/1.D-3, 1.D-5, 1.D-7,  1.D-9/)
    REAL(DOUBLE),DIMENSION(4) :: TwoENeglect=(/1.D-6, 1.D-8, 1.D-10, 1.D-12/)
    REAL(DOUBLE),DIMENSION(4) :: DistNeglect=(/1.D-8, 1.D-10,1.D-12, 1.D-14/)
