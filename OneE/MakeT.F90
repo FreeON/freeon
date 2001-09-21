ee7913827c499c7899c7a1a06bca0c1f5f51f049
mchalla <mchalla>
2001-09-21 12:09:19 +0000

    Postpone filtration

--- OneE/MakeT.F90
+++ OneE/MakeT.F90
@@ -18,9 +18,9 @@ PROGRAM MakeT
 #endif
   IMPLICIT NONE
 #ifdef PARALLEL
-  TYPE(DBCSR)         :: TK,T2
+  TYPE(DBCSR)         :: T,T2
 #else
-  TYPE(BCSR)          :: TK,T2
+  TYPE(BCSR)          :: T,T2
 #endif
 #ifdef PERIODIC 
   LOGICAL             :: NotMakeBlock
@@ -49,7 +49,7 @@ PROGRAM MakeT
 ! Allocations 
 !
   CALL New(MD,(/3,BS%NASym+1,BS%NASym+1,2*BS%NASym+2/),(/1,-1,-1,-1/))
-  CALL New(TK)
+  CALL New(T)
 #ifdef PERIODIC
 !-----------------------------------------------
 ! Calculate the Number of Cells
@@ -59,17 +59,17 @@ PROGRAM MakeT
 !-----------------------------------------------
 ! Initialize the matrix and associated indecies
 !
-  P=1; R=1; TK%RowPt%I(1)=1
-  CALL SetEq(TK%MTrix,Zero)
+  P=1; R=1; T%RowPt%I(1)=1
+  CALL SetEq(T%MTrix,Zero)
 !-----------------------------------------------
 ! Main loops
 !
 #ifdef PARALLEL
-  TK%NAtms=0
+  T%NAtms=0
   DO AtA=Beg%I(MyId),End%I(MyId)
-     TK%NAtms=TK%NAtms+1  
+     T%NAtms=T%NAtms+1  
 #else
-  TK%NAtms=NAtoms
+  T%NAtms=NAtoms
   DO AtA=1,NAtoms
 #endif
      DO AtB=1,NAtoms
@@ -88,26 +88,26 @@ PROGRAM MakeT
                         + (Pair%A(2)-Pair%B(2))**2 &
                         + (Pair%A(3)-Pair%B(3))**2
               IF(TestAtomPair(Pair)) THEN
-                 TK%MTrix%D(R:R+NN-1)=TK%MTrix%D(R:R+NN-1)+TBlok(BS,MD,Pair)
+                 T%MTrix%D(R:R+NN-1)=T%MTrix%D(R:R+NN-1)+TBlok(BS,MD,Pair)
                  NotMakeBlock = .FALSE.
               ENDIF
            ENDDO
 #else
-           TK%MTrix%D(R:R+NN-1)=TBlok(BS,MD,Pair)
+           T%MTrix%D(R:R+NN-1)=TBlok(BS,MD,Pair)
 #endif
-           TK%ColPt%I(P)=AtB
-           TK%BlkPt%I(P)=R
+           T%ColPt%I(P)=AtB
+           T%BlkPt%I(P)=R
            R=R+NN 
            P=P+1 
 #ifdef PARALLEL           
-           TK%RowPt%I(TK%NAtms+1)=P
+           T%RowPt%I(T%NAtms+1)=P
            IF(R>MaxNon0Node.OR.P>MaxBlksNode)THEN
               WRITE(*,*)' MyId = ',MyId,MaxBlksNode,MaxNon0Node
               WRITE(*,*)' MyId = ',MyId,' P = ',P,' R = ',R
               CALL Halt(' DBCSR dimensions blown in MakeT ')
            ENDIF
 #else  
-           TK%RowPt%I(AtA+1)=P        
+           T%RowPt%I(AtA+1)=P        
            IF(R>MaxNon0.OR.P>MaxBlks) &
                 CALL Halt(' BCSR dimensions blown in MakeT ')
 #endif
@@ -118,24 +118,24 @@ PROGRAM MakeT
         ENDIF
      ENDDO
   ENDDO
-  TK%NBlks=P-1
-  TK%NNon0=R-1
+  T%NBlks=P-1
+  T%NNon0=R-1
 !------------------------------------------------------------
 ! Put T to disk
 !  
-  CALL Filter(T2,TK)
-  CALL Put(T2,TrixFile('T',Args))
+! now skiping filtration until after orthogonal Fock is xformed
+!  CALL Filter(T2,T)
+  CALL Put(T,TrixFile('T',Args))
 !-----------------------------------------------------------
 ! Printing
 !
-  CALL PChkSum(T2,'T',Prog)
-  CALL PPrint( T2,'T')
-  CALL Plot(   T2,'T')
+  CALL PChkSum(T,'T',Prog)
+  CALL PPrint( T,'T')
+  CALL Plot(   T,'T')
 !------------------------------------------------------------
 ! Tidy up
 ! 
-  CALL Delete(TK)
-  CALL Delete(T2)
+  CALL Delete(T)
   CALL Delete(BS)
   CALL Delete(GM)
   CALL Delete(MD)
