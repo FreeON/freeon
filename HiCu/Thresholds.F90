7afbbac6103b39a743ee29668e7b815ff7e65230
mchalla <mchalla>
2001-09-25 19:18:04 +0000

    Dialed HiCu thresholds yet again

--- HiCu/Thresholds.F90
+++ HiCu/Thresholds.F90
@@ -16,6 +16,6 @@ MODULE HiCuThresholds
       SUBROUTINE SetLocalThresholds(Tau)
          REAL(DOUBLE) :: Tau
          TauRel=Tau*1.D-0  ! Determines integration error
-         TauRho=Tau*1.D-3  ! Deterines error of density on the grid
+         TauRho=Tau*1.D-1  ! Deterines error of density on the grid
       END SUBROUTINE SetLocalThresholds
 END MODULE
