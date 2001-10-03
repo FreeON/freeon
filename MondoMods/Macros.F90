41ee9272cb152b578f7e339ad0acd3d3c15cd0b9
ckgan <ckgan>
2001-10-03 21:09:33 +0000

    minor changes.

--- MondoMods/Macros.F90
+++ MondoMods/Macros.F90
@@ -16,10 +16,9 @@ MODULE Macros
    IMPLICIT NONE
    INTERFACE Init
       MODULE PROCEDURE Init_TIME, Init_DEBG, Init_MEMS
-   ENDINTERFACE
-!--------------------------------------------------
+   END INTERFACE
+!-------------------------------------------------------------------------------
 !  Global status variables
-!
    INTEGER,DIMENSION(3) :: Current
    INTEGER,DIMENSION(3) :: Previous
    CHARACTER(LEN=3)     :: SCFCycl
@@ -31,35 +30,38 @@ MODULE Macros
    CHARACTER(LEN=3)     :: CurGeom
    CHARACTER(LEN=3)     :: PrvGeom
    CHARACTER(LEN=3)     :: NxtGeom
-!--------------------------------------------------
+!-------------------------------------------------------------------------------
    CONTAINS
       SUBROUTINE StartUp(Args,Prog,Serial_O)
          TYPE(ARGMT),INTENT(OUT)              :: Args
          CHARACTER(LEN=*),INTENT(IN)          :: Prog
-         CHARACTER(LEN=DEFAULT_CHR_LEN)       :: Sub
          LOGICAL,OPTIONAL                     :: Serial_O
          INTEGER                              :: I
 #ifdef PARALLEL
          LOGICAL                              :: Serial
          INTEGER                              :: ChkNPrc
-!------------------------------------------------------------------
+!-------------------------------------------------------------------------------
          IF(PRESENT(Serial_O))THEN
             Serial=Serial_O
          ELSE
             Serial=.FALSE.
          ENDIF
 !        Fire up MPI
-         IF(.NOT.Serial) &
-            CALL InitMPI()
+         IF(.NOT.Serial)CALL InitMPI()
+#else
 #endif
+
 !        Get arguments and open InfFile 
          CALL Get(Args)
          SCFName=TRIM(Args%C%C(1))
+ 
+
+
          IF(Args%NC>=2) &
             SCFActn=TRIM(Args%C%C(2))
          Current=Args%I%I(1:3)
          Previous=Args%I%I(4:6)
-!
+
          SCFCycl=TRIM(IntToChar(Args%I%I(1)))
          IF(Args%I%I(3)-1<1)THEN
             PrvCycl=TRIM(IntToChar(Previous(1)))        
@@ -67,7 +69,7 @@ MODULE Macros
             PrvCycl=TRIM(IntToChar(Args%I%I(1)-1))
          ENDIF
          NxtCycl=TRIM(IntToChar(Args%I%I(1)+1))
-!
+
          CurGeom=TRIM(IntToChar(Args%I%I(3)))
          IF(Args%I%I(3)-1<1)THEN
             PrvGeom=TRIM(IntToChar(Previous(3)))        
@@ -75,14 +77,14 @@ MODULE Macros
             PrvGeom=TRIM(IntToChar(Args%I%I(3)-1))
          ENDIF
          NxtGeom=TRIM(IntToChar(Args%I%I(3)+1))
-!
+
          CurBase=TRIM(IntToChar(Args%I%I(2)))
          PrvBase=TRIM(IntToChar(Previous(2)))
-!
+
          InfFile=TRIM(SCFName)//TRIM(InfF)
          CALL OpenHDF(TRIM(InfFile))
          CALL MarkFailure(Prog)
-         Sub=TRIM(Prog)//'$StartUp'
+!-----------------------------------------------------------------------------
 !        Load global characters
          CALL Get(logFile,'logfile')
          CALL Get(OutFile,'outputfile')
@@ -115,13 +117,14 @@ MODULE Macros
 !        Load global thresholding values
          CALL SetThresholds(CurBase)
 #ifdef PARALLEL
-         IF(InParallel)THEN
+         IF(InParallel)THEN        
             CALL New(OffSt,NPrc-1,0)
             CALL Get(OffSt,'dbcsroffsets',Tag_O=CurBase)
             CALL Get(ChkNPrc,'chknprc')
-            IF(NPrc/=ChkNPrc)CALL Halt(' Inconsisency: NPrc = '             &
-                                     //TRIM(IntToChar(NPrc))//' ChkNPrc = ' &
-                                     //TRIM(IntToChar(ChkNPrc)))
+            IF(NPrc/=ChkNPrc) &
+              CALL Halt(' In StartUp() --- Inconsistency: NPrc = '  &
+              //TRIM(IntToChar(NPrc))//' ChkNPrc = ' &
+              //TRIM(IntToChar(ChkNPrc)))
             CALL New(Beg,NPrc-1,0)
             CALL New(End,NPrc-1,0)
             CALL Get(Beg,'beg',Tag_O=CurBase)
@@ -129,6 +132,7 @@ MODULE Macros
          ENDIF
 #endif
 !        Print time stamp
+
          IF(PrintFlags%Key>DEBUG_MEDIUM)THEN
 #ifdef PARALLEL
             IF(MyId==ROOT) &
@@ -138,12 +142,11 @@ MODULE Macros
 #endif 
          ENDIF
       END SUBROUTINE StartUp
-!----------------------------------------------------------------------------------------
-!
-!----------------------------------------------------------------------------------------
+
+!-------------------------------------------------------------------------------
       SUBROUTINE ShutDown(Prog)
          CHARACTER(LEN=*),INTENT(IN) :: Prog
-!----------------------------------------------------------
+!-------------------------------------------------------------------------------
          CALL Delete(BSiz)
          CALL Delete(OffS)
 #ifdef PARALLEL
@@ -158,7 +161,6 @@ MODULE Macros
          ENDIF
          IF(PrintFlags%Key==DEBUG_MAXIMUM) &
             CALL PPrint(MemStats,Prog)
-!
          CALL MarkSuccess()
          CALL CloseHDF()
 !        Print time stamp
@@ -179,13 +181,13 @@ MODULE Macros
          CALL Put(.TRUE.,'ProgramFailed')
          CALL Put(Prog  ,'FailedProgram')
       END SUBROUTINE         
-!
+ 
       SUBROUTINE MarkSuccess()
          CALL Put(.FALSE.,'ProgramFailed')
       END SUBROUTINE         
-!-----------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 !
-!-----------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
       SUBROUTINE Init_MEMS(A)
          TYPE(MEMS), INTENT(OUT) :: A
          A%Allocs=0
@@ -194,9 +196,9 @@ MODULE Macros
          A%MaxMem=0
          A%MaxAlloc=0
       END SUBROUTINE Init_MEMS
-!-----------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 !
-!-----------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
       SUBROUTINE Init_TIME(A)
          TYPE(TIME),INTENT(OUT) :: A
 #ifdef PARALLEL
@@ -207,9 +209,9 @@ MODULE Macros
          A%CPUS=CPUSec()
          A%Wall=WallSec()
      END SUBROUTINE Init_TIME
-!-----------------------------------------------------------------------------------
-!
-!-----------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
+ 
+!-------------------------------------------------------------------------------
      SUBROUTINE Init_DEBG(A,Prog)
         TYPE(DEBG), INTENT(OUT)      :: A
         CHARACTER(LEN=*), INTENT(IN) :: Prog
@@ -255,7 +257,4 @@ MODULE Macros
 #endif
         CLOSE(Inp)
     END SUBROUTINE Init_DEBG
-END MODULE
-
-
-
+END MODULE Macros
