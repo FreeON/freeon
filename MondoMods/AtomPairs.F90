706bf1072873ee8a0067d40ac189312f565fb577
ckgan <ckgan>
2001-10-03 20:26:52 +0000

    Changed comment lines so we don't get wraparound...

--- MondoMods/AtomPairs.F90
+++ MondoMods/AtomPairs.F90
@@ -1,13 +1,12 @@
-!
 !--  This source code is part of the MondoSCF suite of 
 !--  linear scaling electronic structure codes.  
-!
+
 !--  Matt Challacombe and  C. J. Tymczak
 !--  Los Alamos National Laboratory
 !--  Copyright 2000, The University of California
-!
+
 !    Compute the Atom Pair and the Cells for PBC
-!
+
 MODULE AtomPairs
   USE DerivedTypes
   USE GlobalScalars
@@ -19,16 +18,16 @@ MODULE AtomPairs
 #endif
   USE Thresholding
   IMPLICIT NONE
-!---------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 ! Global variables
-!---------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 #ifdef PERIODIC
   TYPE(CellSet)                :: CS,CSMM1,CSMM2
 #endif
 CONTAINS
-!--------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 ! Test to see if the atomic overlap is above tolerance and create pair
-!--------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
   FUNCTION SetAtomPair(GM,BS,I,J,Pair)
     LOGICAL                   :: SetAtomPair
     INTEGER                   :: I,J,K,N1,N2,N3,M1,M2,M3
@@ -37,12 +36,12 @@ CONTAINS
     TYPE(BSet)                :: BS
 #ifdef PERIODIC
     REAL(DOUBLE),DIMENSION(3) :: VecF,VecA,NRgn
-!
+
     NRgn = 0
     DO K=1,3
        IF(GM%AutoW(K)) NRgn(K) = 1
     ENDDO
-!
+
     Pair%AB2 = 1.D16
     DO N1 = -NRgn(1),NRgn(1)
        DO N2 = -NRgn(2),NRgn(2)
@@ -73,9 +72,9 @@ CONTAINS
        Pair%SameAtom = .FALSE.
     ENDIF
   END FUNCTION SetAtomPair
-!--------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 ! Convert a Block Matrix to a Vector
-!--------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
   FUNCTION BlockToVect(M,N,A) RESULT (B)
     INTEGER                      :: M,N,I,J,K
     REAL(DOUBLE), DIMENSION(M,N) :: A
@@ -88,9 +87,9 @@ CONTAINS
        ENDDO
     ENDDO
   END FUNCTION BlockToVect
-!--------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 ! Convert a Block Matrix to a Vector
-!--------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
   FUNCTION VectToBlock(M,N,A) RESULT (B)
     INTEGER                      :: M,N,I,J,K
     REAL(DOUBLE), DIMENSION(M*N) :: A
@@ -104,15 +103,15 @@ CONTAINS
     ENDDO
   END FUNCTION VectToBlock
 #ifdef PERIODIC
-!--------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 ! Set up the lattice vecters to sum over 
 ! Also, Added a Factor that Takes into Account the Box Size and Shape
-!--------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
   SUBROUTINE SetCellNumber(GM)
     TYPE(CRDS)                     :: GM
     INTEGER                        :: I
     REAL(DOUBLE)                   :: Radius,Radd,A0,B0,C0
-!
+
     A0 = Zero
     B0 = Zero
     C0 = Zero
@@ -123,44 +122,44 @@ CONTAINS
     ENDDO
     Radd = MAX(SQRT(A0),SQRT(B0))
     Radd = MAX(Radd,SQRT(C0))
-!
+
     Radius = Radd+SQRT(AtomPairDistanceThreshold)
     CALL New_CellSet_Sphere(CS,GM%AutoW,GM%BoxShape%D,Radius)
-!
+
     WRITE(*,*) 'THE NUMBER OF CELLS  in CS = ',CS%NCells 
   END SUBROUTINE SetCellNumber
-!----------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 ! Convert from Atomic Coordinates  to Fractional Coordinates
-!----------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
   FUNCTION AtomToFrac(GM,VecA) RESULT(VecF)
     TYPE(CRDS)                 :: GM
     REAL(DOUBLE),DIMENSION(3)  :: VecA,VecF
-!
+
     VecF(1) = VecA(1)*GM%InvBoxSh%D(1,1) + VecA(2)*GM%InvBoxSh%D(1,2) + VecA(3)*GM%InvBoxSh%D(1,3)
     VecF(2) = VecA(1)*GM%InvBoxSh%D(2,1) + VecA(2)*GM%InvBoxSh%D(2,2) + VecA(3)*GM%InvBoxSh%D(2,3)
     VecF(3) = VecA(1)*GM%InvBoxSh%D(3,1) + VecA(2)*GM%InvBoxSh%D(3,2) + VecA(3)*GM%InvBoxSh%D(3,3)
-!
+
   END FUNCTION AtomToFrac
-!-----------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 ! Convert from Fractional Coordinates to Atomic Coordinates
-!-----------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
   FUNCTION FracToAtom(GM,VecF) RESULT(VecA)
     TYPE(CRDS)                 :: GM
     REAL(DOUBLE),DIMENSION(3)  :: VecA,VecF
-!
+
     VecA(1) = VecF(1)*GM%BoxShape%D(1,1) + VecF(2)*GM%BoxShape%D(1,2) + VecF(3)*GM%BoxShape%D(1,3)
     VecA(2) = VecF(1)*GM%BoxShape%D(2,1) + VecF(2)*GM%BoxShape%D(2,2) + VecF(3)*GM%BoxShape%D(2,3)
     VecA(3) = VecF(1)*GM%BoxShape%D(3,1) + VecF(2)*GM%BoxShape%D(3,2) + VecF(3)*GM%BoxShape%D(3,3)
-!
+
   END FUNCTION FracToAtom
-!-----------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 ! In Perodic Systems, Cyclically put positions back in the Fracional Box (in Frac Coord)
-!-----------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
   SUBROUTINE FracCyclic(GM,VecF)
     TYPE(CRDS)                 :: GM        
     REAL(DOUBLE),DIMENSION(3)  :: VecF
     INTEGER                    :: I,N      
-!
+
     DO I=1,3
        IF(GM%AutoW(I)) THEN
           IF(VecF(I) > one) THEN
@@ -174,27 +173,27 @@ CONTAINS
        ENDIF
     ENDDO
   END SUBROUTINE FracCyclic
-!------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 ! In Perodic Systems, Cyclically put positions back in the Atomic Box (in Atom Coord)
-!------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
   SUBROUTINE AtomCyclic(GM,VecA)
     TYPE(CRDS)                 :: GM        
     REAL(DOUBLE),DIMENSION(3)  :: VecA,VecF     
-!
+
     VecF = AtomToFrac(GM,VecA)
     CALL FracCyclic(GM,VecF)
     VecA = FracToAtom(GM,VecF)
-!
+
   END SUBROUTINE AtomCyclic
-!------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 ! Test to See if in the Box (Fractional Coordinates)
-!------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
   FUNCTION InFracBox(GM,VecF)
     TYPE(CRDS)                 :: GM     
     LOGICAL                    :: InFracBox
     INTEGER                    :: I
     REAL(DOUBLE),DIMENSION(3)  :: VecF
-!
+
     InFracBox = .TRUE.
     DO I=1,3
        IF(GM%AutoW(I)) THEN
@@ -204,20 +203,20 @@ CONTAINS
           ENDIF
        ENDIF
     ENDDO
-!
+
   END FUNCTION InFracBox
-!------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 ! Test to See if in the Box (Atomic Coordinates)
-!------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
   FUNCTION InAtomBox(GM,VecA)
     TYPE(CRDS)                 :: GM   
     LOGICAL                    :: InAtomBox
     REAL(DOUBLE),DIMENSION(3)  :: VecA,VecF     
-!
+
     VecF = AtomToFrac(GM,VecA)
     InAtomBox = InFracBox(GM,VecF)
-!
+
   END FUNCTION InAtomBox
 #endif
-!
+
 END MODULE AtomPairs
