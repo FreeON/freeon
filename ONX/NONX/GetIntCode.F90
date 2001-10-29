f2175b90d149c486ee9297ad009e5df8fb931046
schwegle <schwegle>
2001-10-29 16:12:45 +0000

    Should be a stable version for matrices.

--- ONX/NONX/GetIntCode.F90
+++ ONX/NONX/GetIntCode.F90
@@ -14,11 +14,15 @@
     TK=TKet
     IF(TBra.eq.0301) TB=0201
     IF(TBra.eq.0303) TB=0202
+    IF(TBra.eq.0302) TB=0202
     IF(TKet.eq.0301) TK=0201
     IF(TKet.eq.0303) TK=0202
+    IF(TKet.eq.0302) TK=0202
     IntCodeC = TBra*10000+TKet
     IntCodeV = TB*10000+TK
     
+    IF (IntCodeC.eq.02010301) Explicit=.FALSE.
+    IF (IntCodeC.eq.03010201) Explicit=.FALSE.
     IF (IntCodeC.eq.03010301) Explicit=.FALSE.
     IF (IntCodeC.eq.01010601) Explicit=.FALSE.
     IF (IntCodeC.eq.06010101) Explicit=.FALSE.
