bc611241a1da65380fffbfa7c3cbf2828c3a4b3a
schwegle <schwegle>
2001-11-01 16:58:25 +0000

    Anouther fix to the integral code

--- ONX/NONX/GetIntCode.F90
+++ ONX/NONX/GetIntCode.F90
@@ -13,12 +13,15 @@
     ENDIF
     TB=TBra
     TK=TKet
+
     IF(TBra.eq.0301) TB=0201
     IF(TBra.eq.0303) TB=0202
     IF(TBra.eq.0302) TB=0202
+    IF(TBra.eq.0603) TB=0602
     IF(TKet.eq.0301) TK=0201
     IF(TKet.eq.0303) TK=0202
     IF(TKet.eq.0302) TK=0202
+    IF(TKet.eq.0603) TK=0602
     IntCodeC = TBra*10000+TKet
     IntCodeV = TB*10000+TK
 !
@@ -39,18 +42,46 @@
 !
     IF (IntCodeC.eq.02020201) Explicit=.FALSE.
     IF (IntCodeC.eq.02010202) Explicit=.FALSE.
+    IF (IntCodeC.eq.06020101) Explicit=.FALSE.
+    IF (IntCodeC.eq.01010602) Explicit=.FALSE.
+    IF (IntCodeC.eq.06010201) Explicit=.FALSE.
+    IF (IntCodeC.eq.02010601) Explicit=.FALSE.
+    IF (IntCodeC.eq.03020301) Explicit=.FALSE.
+    IF (IntCodeC.eq.03030301) Explicit=.FALSE.
+    IF (IntCodeC.eq.03020201) Explicit=.FALSE.
+    IF (IntCodeC.eq.03030201) Explicit=.FALSE.
+    IF (IntCodeC.eq.02020301) Explicit=.FALSE.
+    IF (IntCodeC.eq.03010302) Explicit=.FALSE.
+    IF (IntCodeC.eq.02010303) Explicit=.FALSE.
+    IF (IntCodeC.eq.03010303) Explicit=.FALSE.
+    IF (IntCodeC.eq.03010202) Explicit=.FALSE.
+    IF (IntCodeC.eq.02010302) Explicit=.FALSE.
+    IF (IntCodeC.eq.06030101) Explicit=.FALSE.
+    IF (IntCodeC.eq.01010603) Explicit=.FALSE.
+    IF (IntCodeC.eq.06010301) Explicit=.FALSE.
+    IF (IntCodeC.eq.03010601) Explicit=.FALSE.
 !
 ! L=4 exceptions
 !
-    IF (IntCodeC.eq.02020202) Explicit=.FALSE.
     IF (IntCodeC.eq.03020202) Explicit=.FALSE.
-    IF (IntCodeC.eq.02020302) Explicit=.FALSE.
-    IF (IntCodeC.eq.03020302) Explicit=.FALSE.
+    IF (IntCodeC.eq.02020301) Explicit=.FALSE.
     IF (IntCodeC.eq.03030202) Explicit=.FALSE.
     IF (IntCodeC.eq.02020303) Explicit=.FALSE.
-    IF (IntCodeC.eq.03030302) Explicit=.FALSE.
-    IF (IntCodeC.eq.03020303) Explicit=.FALSE.
-    IF (IntCodeC.eq.03030303) Explicit=.FALSE.
+    IF (IntCodeC.eq.03020302) Explicit=.FALSE.
+    IF (IntCodeC.eq.02020202) Explicit=.FALSE.
+    IF (IntCodeC.eq.06060101) Explicit=.FALSE.
+    IF (IntCodeC.eq.01010606) Explicit=.FALSE.
+    IF (IntCodeC.eq.06010601) Explicit=.FALSE.
+    IF (IntCodeC.eq.06010202) Explicit=.FALSE.
+    IF (IntCodeC.eq.06010302) Explicit=.FALSE.
+    IF (IntCodeC.eq.06010303) Explicit=.FALSE.
+    IF (IntCodeC.eq.02020601) Explicit=.FALSE.
+    IF (IntCodeC.eq.03020601) Explicit=.FALSE.
+    IF (IntCodeC.eq.03030601) Explicit=.FALSE.
+    IF (IntCodeC.eq.06020101) Explicit=.FALSE.
+    IF (IntCodeC.eq.06030101) Explicit=.FALSE.
+    IF (IntCodeC.eq.01010602) Explicit=.FALSE.
+    IF (IntCodeC.eq.01010603) Explicit=.FALSE.
 
   END SUBROUTINE GetIntCode
   
--- ONX/NONX/MemInit.F90
+++ ONX/NONX/MemInit.F90
@@ -12,6 +12,8 @@ SUBROUTINE MemInit(DB,IB,SB,Drv,BSc,BSp)
   TYPE(BSet),INTENT(IN)    :: BSc,BSp
   INTEGER                  :: LR
 
+  write(*,*) "In MemInit: ",ErrorCode
+
   IF (ErrorCode==eInit) THEN
     DB%MAXDis  = 1000
     DB%MAXPrm  = 3000
