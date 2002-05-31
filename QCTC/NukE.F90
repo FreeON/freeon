fed1356fcb2eb8754b9abd8f9979b14c94054980
nemeth <nemeth>
2002-05-31 20:57:49 +0000

    Modified such that GM/GM_MM must be passed in as GM_Loc, also type of AtNum changed.

--- QCTC/NukE.F90
+++ QCTC/NukE.F90
@@ -22,7 +22,8 @@ MODULE NuklarE
 !=============================================================================================
 !
 !=============================================================================================
-    FUNCTION NukE()
+    FUNCTION NukE(GM_Loc)
+       TYPE(CRDS),  INTENT(IN)  :: GM_Loc
        REAL(DOUBLE)                    :: NukE,NukeCo,NukePole,PExtent
        REAL(DOUBLE),DIMENSION(1:1)     :: HGBra
        REAL(DOUBLE),DIMENSION(0:0)     :: SPBraC,SPBraS
@@ -32,22 +33,21 @@ MODULE NuklarE
 #endif
 !---------------------------------------------------------------------------------------------
        NukE=Zero 
-       DO At=1,NAtoms
+       DO At=1,GM_Loc%Natms
 !         Initialize |BRA>
-          HGBra(1) =-GM%AtNum%I(At)*(NuclearExpnt/Pi)**(ThreeHalves)
-          SPBraC(0)=-GM%AtNum%I(At)
+          HGBra(1) =-GM_Loc%AtNum%D(At)*(NuclearExpnt/Pi)**(ThreeHalves)
+          SPBraC(0)=-GM_Loc%AtNum%D(At)
           Prim%Ell=0
-          Prim%P=GM%Carts%D(:,At)
+          Prim%P=GM_Loc%Carts%D(:,At)
           Prim%Zeta=NuclearExpnt
 !         Set the MAC
-          DP2=(GM%AtNum%I(At)/TauMAC)**(Two/DBLE(SPEll+2))
+          DP2=(GM_Loc%AtNum%D(At)/TauMAC)**(Two/DBLE(SPEll+2))
 !         Set the PAC
           PExtent=Extent(0,NuclearExpnt,HGBra,TauPAC)
 !         Initialize <KET|
-
           CALL SetKet(Prim,PExtent)
 #ifdef PERIODIC
-          PTmp=GM%Carts%D(:,At)
+          PTmp=GM_Loc%Carts%D(:,At)
           DO NC=1,CS_IN%NCells
 !            Set Atomic Coordinates
              Prim%P=PTmp+CS_IN%CellCarts%D(:,NC)
@@ -60,7 +60,7 @@ MODULE NuklarE
 !         Accumulate the atomic contribution
           NukE=NukE+HGBra(1)*HGKet(1)+SPBraC(0)*SPKetC(0)
 !         Add in the Far Field, Dipole and Quadripole  Correction
-          IF(GM%PBC%Dimen > 0) THEN
+          IF(GM_Loc%PBC%Dimen > 0) THEN
              NukE = NukE + CTraxFF(Prim,HGBra)
           ENDIF
 #else
@@ -70,7 +70,6 @@ MODULE NuklarE
           NukE=NukE+HGBra(1)*HGKet(1)+SPBraC(0)*SPKetC(0) 
 #endif
        ENDDO
-!
      END FUNCTION NukE
 !
 END MODULE
