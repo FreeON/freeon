9f84958d624fcadd264ad37da39f0a8a9716d3ca
nemeth <nemeth>
2002-06-14 16:06:28 +0000

    New, compact routine calculates Coulomb and LJ exclusion energies and gradients, LJ full energy calculation modified, LJ gradients tested, seem to be OK.

--- Dynamo/dynamo.F90
+++ Dynamo/dynamo.F90
@@ -39,410 +39,184 @@ CONTAINS
 !-------------------------------------------------------------- 
 !
 #ifdef MMech
-   SUBROUTINE COULOMB_EXCL(Natoms,E_C_EXCL,InfFile,Cur)
-!
-! Calculate Coulomb exclusion for MM and QMMM calculations
-! MM_Natoms and MM geometry must be used
-!
-     IMPLICIT NONE
-     CHARACTER(LEN=DEFAULT_CHR_LEN) :: InfFile
-     REAL(DOUBLE) :: E_C_EXCL12,E_C_EXCL13,E_C_EXCL14,E_C_EXCL
-     TYPE(INT_VECT) :: ATMMARK
-     TYPE(CRDS) :: GM_MM 
-     INTEGER :: I,J,K,L,M,N,Natoms,NMAX12,NMAX13,NMAX14
-     TYPE(DBL_VECT)    :: CHARGE14
-     TYPE(INT_RNK2)    :: TOP12,TOP13,TOP14
-     CHARACTER(LEN=3)  :: Cur
-     REAL(DOUBLE) :: XI,YI,ZI,XJ,YJ,ZJ,DIJ,QI,QJ,CONVF,QI14,QJ14
-!
-! If QMMM distinguish QM and MM atoms
-!   
-     CALL OpenHDF(InfFile)
-!
-     GM_MM%NAtms=Natoms
-     CALL New(GM_MM)
-     CALL New(ATMMARK,Natoms)
-     CALL New(CHARGE14,Natoms)
-!
-     CALL Get(NMAX12,'NMAX12')
-     CALL Get(NMAX13,'NMAX13')
-     CALL Get(NMAX14,'NMAX14')
-     CALL New(TOP12,(/Natoms,NMAX12/))
-     CALL New(TOP13,(/Natoms,NMAX13/))
-     CALL New(TOP14,(/Natoms,NMAX14/))
-!
-     IF(HasQM()) THEN
-       CALL Get(ATMMARK,'ATMMARK')
-     ELSE 
-       ATMMARK%I(:)=0 !!! all MM atoms
-     ENDIF
-!
-! Get MM geometry and charges
-! also get 14-charges
-! also get topology matrices for 1-2, 1-3 and 1-4 neighbour atoms
-!
-!    CALL Get(GM_MM,'GM_MM'//Cur)
-     CALL Get(GM_MM%Carts,'cartesians',Tag_O='GM_MM'//Cur)
-     CALL Get(GM_MM%AtNum,'atomicnumbers',Tag_O='GM_MM'//Cur)
-     CALL Get(CHARGE14,'CHARGE14')
-     CALL Get(TOP12,'TOP12')
-     CALL Get(TOP13,'TOP13')
-     CALL Get(TOP14,'TOP14')
-!
-     CALL CloseHDF() 
-!!
-!!test data got from HDF
-!      write(*,*) 'coordinates test'
-!     do i=1,Natoms
-!      write(*,200) I,GM_MM%Carts%D(1:3,I)/AngstromsToAU 
-!200   format(I5,3F12.6)
-!     enddo
-!      write(*,*) 'charges, ch14 eps14'
-!     do i=1,Natoms
-!      write(*,200) I,GM_MM%AtNum%D(I),CHARGE14%D(I)
-!     enddo
-!      write(*,*) 'top12 '
-!     do i=1,Natoms
-!      write(*,300) I,TOP12%I(I,1:NMAX12) 
-!     enddo
-!      write(*,*) 'top13 '
-!     do i=1,Natoms
-!      write(*,300) I,TOP13%I(I,1:NMAX13) 
-!     enddo
-!      write(*,*) 'top14 '
-!     do i=1,Natoms
-!      write(*,300) I,TOP14%I(I,1:NMAX14) 
-!     enddo
-!300  format(I5,20I4)
-!
-! Now calculate Coulomb exclusion energy
-! Please note that all MM charges on the QM positions
-! and on the link atoms, as well as on the atoms of the 
-! broken covalent bonds should be set to zero at this point.
-!
-     E_C_EXCL12=Zero
-     E_C_EXCL13=Zero
-     E_C_EXCL14=Zero
-     CONVF=e2PerAngstroemToKJPerMol*AngstromsToAU
-!
-! bond terms
-!
-     DO I=1,Natoms
-       IF(ATMMARK%I(I)==0) THEN
-       XI=GM_MM%Carts%D(1,I)
-       YI=GM_MM%Carts%D(2,I)
-       ZI=GM_MM%Carts%D(3,I)
-       QI=GM_MM%AtNum%D(I)
-     DO M=1,TOP12%I(I,1)
-       J=TOP12%I(I,M+1)
-       IF(ATMMARK%I(J)==0) THEN
-       XJ=GM_MM%Carts%D(1,J)
-       YJ=GM_MM%Carts%D(2,J)
-       ZJ=GM_MM%Carts%D(3,J)
-       QJ=GM_MM%AtNum%D(J)
-       DIJ=SQRT((XI-XJ)**2+(YI-YJ)**2+(ZI-ZJ)**2)
-         IF(DIJ>0.001D0) THEN
-           E_C_EXCL12=E_C_EXCL12+QI*QJ/DIJ
-         ELSE
-       write(*,*) 'bond dij= ',i,j,dij
-  Call MondoHalt(QMMM_ERROR,'Atoms are too close to each other in MM set')
-         ENDIF
-       ENDIF
-     ENDDO
-       ENDIF
-     ENDDO
-!
-! angle terms
-!
-     DO I=1,Natoms
-       IF(ATMMARK%I(I)==0) THEN
-       XI=GM_MM%Carts%D(1,I)
-       YI=GM_MM%Carts%D(2,I)
-       ZI=GM_MM%Carts%D(3,I)
-       QI=GM_MM%AtNum%D(I)
-     DO M=1,TOP13%I(I,1)
-       J=TOP13%I(I,M+1)
-       IF(ATMMARK%I(J)==0) THEN
-       XJ=GM_MM%Carts%D(1,J)
-       YJ=GM_MM%Carts%D(2,J)
-       ZJ=GM_MM%Carts%D(3,J)
-       QJ=GM_MM%AtNum%D(J)
-       DIJ=SQRT((XI-XJ)**2+(YI-YJ)**2+(ZI-ZJ)**2)
-         IF(DIJ>0.001D0) THEN
-           E_C_EXCL13=E_C_EXCL13+QI*QJ/DIJ
-         ELSE
-       write(*,*) 'angle dij= ',i,j,dij
-  Call MondoHalt(QMMM_ERROR,'Atoms are too close to each other in MM set')
-         ENDIF
-       ENDIF
-     ENDDO
-       ENDIF
-     ENDDO
-!
-! dihedral terms
-!
-     DO I=1,Natoms
-       IF(ATMMARK%I(I)==0) THEN
-       XI=GM_MM%Carts%D(1,I)
-       YI=GM_MM%Carts%D(2,I)
-       ZI=GM_MM%Carts%D(3,I)
-       QI=GM_MM%AtNum%D(I)
-       QI14=CHARGE14%D(I)
-     DO M=1,TOP14%I(I,1)
-       J=TOP14%I(I,M+1)
-       IF(ATMMARK%I(J)==0) THEN
-       XJ=GM_MM%Carts%D(1,J)
-       YJ=GM_MM%Carts%D(2,J)
-       ZJ=GM_MM%Carts%D(3,J)
-       QJ=GM_MM%AtNum%D(J)
-       QJ14=CHARGE14%D(J)
-       DIJ=SQRT((XI-XJ)**2+(YI-YJ)**2+(ZI-ZJ)**2)
-         IF(DIJ>0.001D0) THEN
-           E_C_EXCL14=E_C_EXCL14+(QI*QJ-QI14*QJ14)/DIJ
-         ELSE
-       write(*,*) 'angle dij= ',i,j,dij
-  Call MondoHalt(QMMM_ERROR,'Atoms are too close to each other in MM set')
-         ENDIF
-       ENDIF
-     ENDDO
-       ENDIF
-     ENDDO
-!
-     E_C_EXCL12=E_C_EXCL12*CONVF/Two
-     E_C_EXCL13=E_C_EXCL13*CONVF/Two
-     E_C_EXCL14=E_C_EXCL14*CONVF/Two
-     E_C_EXCL=E_C_EXCL12+E_C_EXCL13+E_C_EXCL14
-!
-     CALL DELETE(GM_MM)
-     CALL DELETE(ATMMARK)
-     CALL DELETE(CHARGE14)
-     CALL DELETE(TOP12)
-     CALL DELETE(TOP13)
-     CALL DELETE(TOP14)
-     write(*,*) 'E_C_EXCL12= ',E_C_EXCL12
-     write(*,*) 'E_C_EXCL13= ',E_C_EXCL13
-     write(*,*) 'E_C_EXCL14= ',E_C_EXCL14
-     write(*,*) 'E_C_EXCL= ',E_C_EXCL
-!
-     CALL OpenHDF(InfFile)
-       CALL Put(E_C_EXCL12,'E_C_EXCL12')
-       CALL Put(E_C_EXCL13,'E_C_EXCL13')
-       CALL Put(E_C_EXCL14,'E_C_EXCL14')
-       CALL Put(E_C_EXCL,'E_C_EXCL')
-     CALL CloseHDF()
-!
-   END SUBROUTINE COULOMB_EXCL
 !
 !--------------------------------------------------------
 !
-   SUBROUTINE LJ_EXCL(Natoms,E_LJ_EXCL,InfFile,Cur)
+   SUBROUTINE EXCL(Natoms,Cur,InfFile,E_LJ_EXCL,E_C_EXCL,Grad_Loc)
 !
-! Calculate the Lennard-Jones exclusion for MM and QMMM calculations
+! Calculate the Coulomb and Lennard-Jones exclusion 
+! energies and gradients for MM and QMMM calculations
 ! MM_Natoms and MM geometry must be used
+! Presently, energies are given in KJ/mol, 
+! gradients in KJ/mol/angstroem
 !
      IMPLICIT NONE
      CHARACTER(LEN=DEFAULT_CHR_LEN) :: InfFile
-     REAL(DOUBLE) :: E_LJ_EXCL12,E_LJ_EXCL13,E_LJ_EXCL14,E_LJ_EXCL
-     TYPE(INT_VECT) :: ATMMARK
-     TYPE(CRDS) :: GM_MM 
+     REAL(DOUBLE),OPTIONAL :: E_LJ_EXCL,E_C_EXCL
+     TYPE(INT_VECT) :: AtmMark
+     TYPE(CRDS) :: GM_Loc 
      INTEGER :: I,J,K,L,M,N,Natoms,NMAX12,NMAX13,NMAX14
-     TYPE(DBL_VECT)    :: LJEPS14,LJEPS,LJRAD
-     TYPE(INT_RNK2)    :: TOP12,TOP13,TOP14
+     TYPE(DBL_VECT)    :: LJEps14,LJEps,LJRad,Charge14
+     TYPE(INT_RNK2)    :: Top_Loc
      CHARACTER(LEN=3)  :: Cur
-     REAL(DOUBLE) :: XI,YI,ZI,XJ,YJ,ZJ,DIJ,QI,QJ,CONVF,QI14,QJ14
-     REAL(DOUBLE) :: RI,RJ,R6
+     REAL(DOUBLE) :: XI,YI,ZI,XJ,YJ,ZJ,QI,QJ,CONVF,CONVF2,QI14,QJ14
+     REAL(DOUBLE) :: RI,RJ,R6,DIJ,DIJ2,DIJAu,DIJ2Au,Pref1,Pref2
+     REAL(DOUBLE) :: QQI,QQI14,QQJ,QQJ14
+     TYPE(DBL_RNK2),OPTIONAL :: Grad_Loc
+     TYPE(DBL_VECT) :: DVect     
+     INTEGER :: ITop
 !
-! If QMMM distinguish QM and MM atoms
+! If QMMM, distinguish QM and MM atoms
 !   
      CALL OpenHDF(InfFile)
 !
-     GM_MM%NAtms=Natoms
-     CALL New(GM_MM)
-     CALL New(ATMMARK,Natoms)
-     CALL New(LJEPS,Natoms)
-     CALL New(LJEPS14,Natoms)
-     CALL New(LJRAD,Natoms)
+     GM_Loc%NAtms=Natoms
+     CALL New(GM_Loc)
+     CALL New(DVect,3)
+     CALL New(AtmMark,Natoms)
 !
      CALL Get(NMAX12,'NMAX12')
      CALL Get(NMAX13,'NMAX13')
      CALL Get(NMAX14,'NMAX14')
-     CALL New(TOP12,(/Natoms,NMAX12/))
-     CALL New(TOP13,(/Natoms,NMAX13/))
-     CALL New(TOP14,(/Natoms,NMAX14/))
 !
      IF(HasQM()) THEN
-       CALL Get(ATMMARK,'ATMMARK')
+       CALL Get(AtmMark,'AtmMark')
      ELSE 
-       ATMMARK%I(:)=0 !!! all MM atoms
+       AtmMark%I(:)=0 !!! all MM atoms
      ENDIF
 !
-! Get MM geometry and charges
+! Get MM geometry and LJ data
 ! also get 14-charges
-! also get topology matrices for 1-2, 1-3 and 1-4 neighbour atoms
+! also get Topology matrices for 1-2, 1-3 and 1-4 neighbour atoms
 !
-     CALL Get(GM_MM%Carts,'cartesians',Tag_O='GM_MM'//Cur)
-     CALL Get(LJRAD,'LJRAD')
-     CALL Get(LJEPS,'LJEPS')
-     CALL Get(LJEPS14,'LJEPS14')
-     CALL Get(TOP12,'TOP12')
-     CALL Get(TOP13,'TOP13')
-     CALL Get(TOP14,'TOP14')
+     CALL Get(GM_Loc%Carts,'cartesians',Tag_O='GM_MM'//Cur)
+     IF(PRESENT(E_LJ_EXCL)) THEN
+       CALL New(LJEps,Natoms)
+       CALL New(LJEps14,Natoms)
+       CALL New(LJRad,Natoms)
+       CALL Get(LJRad,'LJRad')
+       CALL Get(LJEps,'LJEps')
+       CALL Get(LJEps14,'LJEps14')
+       E_LJ_EXCL=Zero
+     ENDIF
+     IF(PRESENT(E_C_EXCL)) THEN
+       CALL Get(GM_Loc%AtNum,'atomicnumbers',Tag_O='GM_MM'//Cur)
+       CALL New(CHARGE14,Natoms)
+       CALL Get(Charge14,'Charge14')
+       E_C_EXCL=Zero
+     ENDIF
 !
-     CALL CloseHDF() 
-!!
-!!test data got from HDF
-!      write(*,*) 'coordinates test'
-!     do i=1,Natoms
-!      write(*,200) I,GM_MM%Carts%D(1:3,I)/AngstromsToAU 
-!200   format(I5,3F12.6)
-!     enddo
-!      write(*,*) 'charges, ch14 eps14'
-!     do i=1,Natoms
-!      write(*,200) I,LJEPS%D(I),LJEPS14%D(I)
-!     enddo
-!      write(*,*) 'top12 '
-!     do i=1,Natoms
-!      write(*,300) I,TOP12%I(I,1:NMAX12) 
-!     enddo
-!      write(*,*) 'top13 '
-!     do i=1,Natoms
-!      write(*,300) I,TOP13%I(I,1:NMAX13) 
-!     enddo
-!      write(*,*) 'top14 '
-!     do i=1,Natoms
-!      write(*,300) I,TOP14%I(I,1:NMAX14) 
-!     enddo
-!300  format(I5,20I4)
-!
-! Now calculate Lennard-Jones exclusion energy
+! Now calculate Lennard-Jones and Coulomb exclusion energies
 ! Please note that all MM charges on the QM positions
 ! and on the link atoms, as well as on the atoms of the 
 ! broken covalent bonds should be set to zero at this point.
 !
-     E_LJ_EXCL12=Zero
-     E_LJ_EXCL13=Zero
-     E_LJ_EXCL14=Zero
-     CONVF=e2PerAngstroemToKJPerMol*AngstromsToAU
+     CONVF=e2PerAngstroemToKJPerMol
 !
-! bond terms
+! bond, angle and dihedral terms
 !
-     DO I=1,Natoms
-       IF(ATMMARK%I(I)==0) THEN
-       XI=GM_MM%Carts%D(1,I)
-       YI=GM_MM%Carts%D(2,I)
-       ZI=GM_MM%Carts%D(3,I)
-       QI=LJEPS%D(I)
-       RI=LJRAD%D(I)
-     DO M=1,TOP12%I(I,1)
-       J=TOP12%I(I,M+1)
-!      IF(ATMMARK%I(J)==0) THEN !!! the second atom may also be QM
-       XJ=GM_MM%Carts%D(1,J)
-       YJ=GM_MM%Carts%D(2,J)
-       ZJ=GM_MM%Carts%D(3,J)
-       QJ=LJEPS%D(J)
-       RJ=LJRAD%D(J)
-       DIJ=SQRT((XI-XJ)**2+(YI-YJ)**2+(ZI-ZJ)**2)/AngstromsToAU
-         IF(DIJ>0.001D0) THEN
-           R6=(RI*RJ/DIJ)**6
-           E_LJ_EXCL12=E_LJ_EXCL12+QI*QJ*R6*(R6-One)
-         ELSE
-       write(*,*) 'bond dij= ',i,j,dij
-  Call MondoHalt(QMMM_ERROR,'Atoms are too close to each other in MM set')
-         ENDIF
-!      ENDIF !!!ATMMARK(J)
-     ENDDO
-       ENDIF
-     ENDDO
+     DO ITop=1,3
 !
-! angle terms
+     IF(ITop==1) THEN
+       CALL New(Top_Loc,(/Natoms,NMAX12/)) 
+       CALL Get(Top_Loc,'Top12')
+     ENDIF
+     IF(ITop==2) THEN
+       CALL New(Top_Loc,(/Natoms,NMAX13/)) 
+       CALL Get(Top_Loc,'Top13')
+     ENDIF
+     IF(ITop==3) THEN
+       CALL New(Top_Loc,(/Natoms,NMAX14/)) 
+       CALL Get(Top_Loc,'Top14')
+     ENDIF
 !
      DO I=1,Natoms
-       IF(ATMMARK%I(I)==0) THEN
-       XI=GM_MM%Carts%D(1,I)
-       YI=GM_MM%Carts%D(2,I)
-       ZI=GM_MM%Carts%D(3,I)
-       QI=LJEPS%D(I)
-       RI=LJRAD%D(I)
-     DO M=1,TOP13%I(I,1)
-       J=TOP13%I(I,M+1)
-!      IF(ATMMARK%I(J)==0) THEN !!! the second atom may also be QM
-       XJ=GM_MM%Carts%D(1,J)
-       YJ=GM_MM%Carts%D(2,J)
-       ZJ=GM_MM%Carts%D(3,J)
-       QJ=LJEPS%D(J)
-       RJ=LJRAD%D(J)
-       DIJ=SQRT((XI-XJ)**2+(YI-YJ)**2+(ZI-ZJ)**2)/AngstromsToAU
+       IF(AtmMark%I(I)/=0) CYCLE !!!interactions of MM atoms only
+       IF(PRESENT(E_LJ_EXCL)) THEN
+         QI=LJEps%D(I)
+         QI14=LJEps14%D(I)
+         RI=LJRad%D(I)
+       ENDIF
+       IF(PRESENT(E_LJ_EXCL)) THEN
+         QQI=GM_Loc%AtNum%D(I)
+         QQI14=CHARGE14%D(I)
+       ENDIF
+     DO M=1,Top_Loc%I(I,1)
+       J=Top_Loc%I(I,M+1)
+     IF(AtmMark%I(J)==0.AND.J<I) CYCLE !!!avoid double counting
+       DVect%D(:)=GM_Loc%Carts%D(:,I)-GM_Loc%Carts%D(:,J)
+       DVect%D(:)=DVect%D(:)/AngstromsToAU !!!work in angstroems
+       DIJ2=DOT_PRODUCT(DVect%D,DVect%D)
+       DIJ=SQRT(DIJ2)
+       DIJAu=DIJ*AngstromsToAU
+       DIJ2Au=DIJAu*DIJAu
+       IF(PRESENT(E_LJ_EXCL)) THEN
+         QJ=LJEps%D(J)
+         QJ14=LJEps14%D(J)
+         RJ=LJRad%D(J)
+       ENDIF
+       IF(PRESENT(E_LJ_EXCL)) THEN
+         QQJ=GM_Loc%AtNum%D(J)
+         QQJ14=CHARGE14%D(J)
+       ENDIF
          IF(DIJ>0.001D0) THEN
-           R6=(RI*RJ/DIJ)**6
-           E_LJ_EXCL13=E_LJ_EXCL13+QI*QJ*R6*(R6-One)
+           IF(PRESENT(E_LJ_EXCL)) THEN
+             R6=(RI*RJ/DIJ)**6
+             IF(ITop==3) THEN
+               Pref1=(QI*QJ-QI14*QJ14)*R6
+             ELSE
+               Pref1=QI*QJ*R6
+             ENDIF
+               E_LJ_EXCL=E_LJ_EXCL+Pref1*(R6-One)
+               IF(PRESENT(Grad_Loc)) THEN
+             Pref2=Six*Pref1*(One-Two*R6)/DIJ2 
+!since it is about exclusion gradients, they get a negative sign
+             Grad_Loc%D(:,I)=Grad_Loc%D(:,I)+(-Pref2*DVect%D(:))
+             Grad_Loc%D(:,J)=Grad_Loc%D(:,J)-(-Pref2*DVect%D(:))
+               ENDIF
+           ENDIF
+           IF(PRESENT(E_C_EXCL)) THEN
+             IF(ITop==3) THEN
+               Pref1=(QQI*QQJ-QQI14*QQJ14)/DIJ*CONVF
+             ELSE
+               Pref1=QQI*QQJ/DIJ*CONVF
+             ENDIF
+               E_C_EXCL=E_C_EXCL+Pref1
+               IF(PRESENT(Grad_Loc)) THEN
+             Pref2=-Pref1/DIJ2 
+!since it is about exclusion gradients, they get a negative sign
+             Grad_Loc%D(:,I)=Grad_Loc%D(:,I)+(-Pref2*DVect%D(:))
+             Grad_Loc%D(:,J)=Grad_Loc%D(:,J)-(-Pref2*DVect%D(:))
+               ENDIF
+           ENDIF
          ELSE
-       write(*,*) 'angle dij= ',i,j,dij
+       write(*,*) 'd dij= ',i,j,dij
   Call MondoHalt(QMMM_ERROR,'Atoms are too close to each other in MM set')
          ENDIF
-!      ENDIF !!!ATMMARK(J)
      ENDDO
-       ENDIF
      ENDDO
 !
-! dihedral terms
+     CALL Delete(Top_Loc)
+     ENDDO ! ITop
 !
-     DO I=1,Natoms
-       IF(ATMMARK%I(I)==0) THEN
-       XI=GM_MM%Carts%D(1,I)
-       YI=GM_MM%Carts%D(2,I)
-       ZI=GM_MM%Carts%D(3,I)
-       QI=LJEPS%D(I)
-       QI14=LJEPS14%D(I)
-       RI=LJRAD%D(I)
-     DO M=1,TOP14%I(I,1)
-       J=TOP14%I(I,M+1)
-!      IF(ATMMARK%I(J)==0) THEN !!! the second atom may also be QM
-       XJ=GM_MM%Carts%D(1,J)
-       YJ=GM_MM%Carts%D(2,J)
-       ZJ=GM_MM%Carts%D(3,J)
-       QJ=LJEPS%D(J)
-       QJ14=LJEPS14%D(J)
-       RJ=LJRAD%D(J)
-       DIJ=SQRT((XI-XJ)**2+(YI-YJ)**2+(ZI-ZJ)**2)/AngstromsToAU
-         IF(DIJ>0.001D0) THEN
-           R6=(RI*RJ/DIJ)**6
-!          E_LJ_EXCL14=E_LJ_EXCL14+(QI14*QJ14)*R6*(R6-One)
-           E_LJ_EXCL14=E_LJ_EXCL14+(QI*QJ-QI14*QJ14)*R6*(R6-One)
-         ELSE
-       write(*,*) 'dihedral dij= ',i,j,dij
-  Call MondoHalt(QMMM_ERROR,'Atoms are too close to each other in MM set')
-         ENDIF
-!      ENDIF !!!ATMMARK(J)
-     ENDDO
-       ENDIF
-     ENDDO
-!
-     E_LJ_EXCL12=E_LJ_EXCL12/Two
-     E_LJ_EXCL13=E_LJ_EXCL13/Two
-     E_LJ_EXCL14=E_LJ_EXCL14/Two
-     E_LJ_EXCL=E_LJ_EXCL12+E_LJ_EXCL13+E_LJ_EXCL14
-!
-     CALL DELETE(GM_MM)
-     CALL DELETE(ATMMARK)
-     CALL DELETE(LJEPS)
-     CALL DELETE(LJEPS14)
-     CALL DELETE(LJRAD)
-     CALL DELETE(TOP12)
-     CALL DELETE(TOP13)
-     CALL DELETE(TOP14)
-     write(*,*) 'E_LJ_EXCL12= ',E_LJ_EXCL12
-     write(*,*) 'E_LJ_EXCL13= ',E_LJ_EXCL13
-     write(*,*) 'E_LJ_EXCL14= ',E_LJ_EXCL14
-     write(*,*) 'E_LJ_EXCL= ',E_LJ_EXCL
-!
-     CALL OpenHDF(InfFile)
-       CALL Put(E_LJ_EXCL12,'E_LJ_EXCL12')
-       CALL Put(E_LJ_EXCL13,'E_LJ_EXCL13')
-       CALL Put(E_LJ_EXCL14,'E_LJ_EXCL14')
+     CALL Delete(GM_Loc)
+     CALL Delete(DVect)
+     CALL Delete(AtmMark)
+     IF(PRESENT(E_LJ_EXCL)) THEN
+       CALL Delete(LJEps)
+       CALL Delete(LJEps14)
+       CALL Delete(LJRad)
+       write(*,*) 'E_LJ_EXCL= ',E_LJ_EXCL
        CALL Put(E_LJ_EXCL,'E_LJ_EXCL')
+     ENDIF
+     IF(PRESENT(E_C_EXCL)) THEN
+       CALL Delete(Charge14)
+       write(*,*) 'E_C_EXCL= ',E_C_EXCL
+       CALL Put(E_C_EXCL,'E_C_EXCL')
+     ENDIF
      CALL CloseHDF()
 !
-   END SUBROUTINE LJ_EXCL
+   END SUBROUTINE EXCL
 !-------------------------------------------------------------- 
 !
    SUBROUTINE ENERGY_LENNARD_JONES(ELJ,ISet,BoxSize,Grad_Loc)
@@ -453,9 +227,9 @@ CONTAINS
    INTEGER :: IX,IY,IZ,IOrd,IOrdD
    INTEGER :: I1,I2,JJ1,JJ2,IXD,IYD,IZD
    REAL(DOUBLE) :: X1,Y1,Z1,X2,Y2,Z2,R1,R2,R12,R6,Q1,Q2
-   TYPE(INT_VECT) :: BoxI,BoxJ,ATMMARK
+   TYPE(INT_VECT) :: BoxI,BoxJ,AtmMark
    TYPE(CRDS) :: GM_Loc
-   TYPE(DBL_VECT) :: LJEPS,LJRAD,DVect
+   TYPE(DBL_VECT) :: LJEps,LJRad,DVect
    TYPE(DBL_RNK2),OPTIONAL :: Grad_Loc
    REAL(DOUBLE) :: ELJ,r12_2,Pref1,Pref2
 !
@@ -464,9 +238,9 @@ CONTAINS
 ! BoxSize is in angstroems
 !
 ! get MM or QMMM atom coordinates as stored in GM_Loc
-! get ATMMARK to distinguish between QM and MM atoms
-! get LJEPS%D : LJ epsilon
-! get LJRAD%D : LJ radii of atoms
+! get AtmMark to distinguish between QM and MM atoms
+! get LJEps%D : LJ epsilon
+! get LJRad%D : LJ radii of atoms
 ! calculate and get BoxI and BoxJ for a certain Box size, then
 ! sum up LJ contributions within Boxes and between 
 ! neighbouring Boxes
@@ -474,18 +248,18 @@ CONTAINS
    CALL Get(GM_Loc,'GM_MM'//TRIM(IntToChar(ISet)))
    Natoms=GM_Loc%Natms
 !
-     CALL New(ATMMARK,Natoms)
+     CALL New(AtmMark,Natoms)
    IF(HasQM()) THEN
-     CALL Get(ATMMARK,'ATMMARK')
+     CALL Get(AtmMark,'AtmMark')
    ELSE 
-     ATMMARK%I(:)=0 !!! all MM atoms
+     AtmMark%I(:)=0 !!! all MM atoms
    ENDIF
 !
    CALL New(DVect,3)
-   CALL New(LJEPS,Natoms)
-   CALL New(LJRAD,Natoms)
-   CALL Get(LJEPS,'LJEPS')
-   CALL Get(LJRAD,'LJRAD') 
+   CALL New(LJEps,Natoms)
+   CALL New(LJRad,Natoms)
+   CALL Get(LJEps,'LJEps')
+   CALL Get(LJRad,'LJRad') 
 !
    GM_Loc%Carts%D(:,:)=GM_Loc%Carts%D(:,:)/AngstromsToAU !!! work with angstroems here
 !
@@ -513,8 +287,9 @@ CONTAINS
 !
      DO I1=BoxI%I(IOrd),BoxI%I(IOrd+1)-1
        JJ1=BoxJ%I(I1)
-        R1=LJRAD%D(JJ1)
-        Q1=LJEPS%D(JJ1)
+       IF(AtmMark%I(JJ1)/=0) CYCLE !!!interactions of MM atoms only
+        R1=LJRad%D(JJ1)
+        Q1=LJEps%D(JJ1)
 ! second atom may come from central or neigbouring Boxes 
 ! and must be an MM atom, LJ is not calculated for QM-QM pairs
      DO IZD=-1,1
@@ -527,10 +302,10 @@ CONTAINS
        IOrdD=NX*NY*(IZ-1+IZD)+NY*(IX-1+IXD)+IY+IYD
          DO I2=BoxI%I(IOrdD),BoxI%I(IOrdD+1)-1
            JJ2=BoxJ%I(I2)
-       IF(JJ1>JJ2) CYCLE !!!! do not count twice
-         IF(ATMMARK%I(JJ2)==0) THEN
-           R2=LJRAD%D(JJ2)
-           Q2=LJEPS%D(JJ2)
+       IF(AtmMark%I(JJ2)==0.AND.JJ2<JJ1) CYCLE !!!avoid double counting
+         IF(AtmMark%I(JJ2)==0) THEN
+           R2=LJRad%D(JJ2)
+           Q2=LJEps%D(JJ2)
            DVect%D(:)=GM_Loc%Carts%D(:,JJ1)-GM_Loc%Carts%D(:,JJ2)
            r12_2=DOT_PRODUCT(DVect%D,DVect%D)                
            R12=SQRT(r12_2)
@@ -569,10 +344,10 @@ CONTAINS
    CALL CloseHDF()
 !
    CALL Delete(GM_Loc)
-   CALL Delete(ATMMARK)
+   CALL Delete(AtmMark)
    CALL Delete(DVect)
-   CALL Delete(LJEPS)
-   CALL Delete(LJRAD)
+   CALL Delete(LJEps)
+   CALL Delete(LJRad)
    CALL Delete(BoxI)
    CALL Delete(BoxJ)
 !
