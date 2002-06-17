82bceccde6b5535bf780fb2d8dc4eb458e5290cb
mchalla <mchalla>
2002-06-17 21:33:49 +0000

    Added ifdef MMech to remove all QM/MM parts.  Redid error analysis to be IMHO more correct, and simplified the printing.  Needs to be checked for QM/MM still.

--- OneE/MakeRho.F90
+++ OneE/MakeRho.F90
@@ -16,15 +16,8 @@ PROGRAM MakeRho
   USE BraBloks
   USE RhoBlok
   USE RhoTools
-!#ifdef PARALLEL
-!  USE MondoMPI
-!#endif
   IMPLICIT NONE
-!#ifdef PARALLEL
-!  TYPE(DBCSR)              :: Dmat,D1,D2
-!#else
   TYPE(BCSR)                :: Dmat,D1,D2
-!#endif
 #ifdef PERIODIC 
   INTEGER                   :: NC
   REAL(DOUBLE),DIMENSION(3) :: B
@@ -39,20 +32,17 @@ PROGRAM MakeRho
   TYPE(INT_VECT)            :: Stat
   INTEGER                   :: P,R,AtA,AtB,NN,iSwitch,IC1,IC2
   INTEGER                   :: NExpt,NDist,NCoef,I,J,Iq,Ir,Pbeg,Pend
-  INTEGER                   :: N1,N2,QMOffSetQ,QMOffSetR
+  INTEGER                   :: N1,N2,QMOffSetQ,QMOffSetR,PcntDist
   LOGICAL                   :: First
-  REAL(DOUBLE)              :: DistThresh,RSumE,RSumN,RSumMM,RelRhoErr
+  REAL(DOUBLE)              :: DistThresh,RSumE,RSumN,RSumMM,RelRhoErr, &
+                               QMCharge,dQMCharge,MMCharge,dMMCharge,PcntCharge
   CHARACTER(LEN=DEFAULT_CHR_LEN) :: Mssg1,Mssg2,RestartHDF
   CHARACTER(LEN=7),PARAMETER :: Prog='MakeRho'
   REAL(DOUBLE),DIMENSION(10,10) :: DD
   REAL(DOUBLE),DIMENSION(3) :: Center
-!----------------------------------------------
-! Start up macro
-!----------------------------------------------
-!
-     CALL StartUp(Args,Prog)
-!----------------------------------------------
-! Calculate density of point charges
+!---------------------------------------------------------------------------------------
+  ! Start up macro
+  CALL StartUp(Args,Prog)
 #ifdef MMech
   IF(HasMM())THEN
      CALL Get(GM_MM,Tag_O='GM_MM'//CurGeom)
@@ -60,7 +50,7 @@ PROGRAM MakeRho
   IF(HasQM())THEN
 #endif
      IF(SCFActn=='BasisSetSwitch')THEN
-        !    Get the previous information
+        !  Get the previous information
         CALL Get(BS,PrvBase)
         CALL Get(GM,CurGeom)
         CALL SetThresholds(PrvBase)
@@ -73,7 +63,7 @@ PROGRAM MakeRho
         CALL Get(NBasF,'nbasf',PrvBase)
         CALL Get(Dmat,TrixFile('D',Args,-1))
      ELSEIF(SCFActn=='Restart')THEN
-        !    Get the old information
+        ! Get the old information
         CALL Get(RestartHDF,'OldInfo')
         CALL CloseHDF()
         CALL OpenHDF(RestartHDF)
@@ -91,10 +81,8 @@ PROGRAM MakeRho
         CALL CloseHDF()
         CALL OpenHDF(InfFile)     
         CALL Get(Dmat,TrixFile('D',Args,0))
-        !     WRITE(*,*)TRIM(TrixFile('D',Args,0))
-        !     CALL PChkSum(DMat,'RestartDM',Prog,Unit_O=6)
      ELSE
-        !    Get the current information
+        ! Get the current information
         CALL Get(BS,CurBase)
         CALL Get(GM,CurGeom)
         CALL Get(NExpt,'nexpt',CurBase)
@@ -112,7 +100,6 @@ PROGRAM MakeRho
            ENDIF
            CALL Delete(D1)
            CALL Delete(D2)
-           !        CALL PPrint(DMat,'D',Unit_O=6)
         ELSEIF(SCFActn=='ForceEvaluation')THEN
            CALL Get(Dmat,TrixFile('D',Args,1))
         ELSEIF(SCFActn/='Core')THEN
@@ -127,10 +114,10 @@ PROGRAM MakeRho
      CALL SetCellNumber(GM)
      CALL PPrint(CS_OUT,'outer sum',Prog)
 #endif
-!--------------------------------------------------------------
+     !--------------------------------------------------------------
      ! Main loops: First pass calculates the size.
      !             Second pass calculates the density
-!-------------------------------------------------------------
+     !-------------------------------------------------------------
      ! Initailize  NQ
      Rho%NQ%I = 0
      IF(SCFActn/='InkFok')Rho%NQ%I(Rho%NExpt)=NAtoms
@@ -169,15 +156,14 @@ PROGRAM MakeRho
   ELSE
      CALL Halt(' Logic problem in MakeRho ')
   ENDIF !!!! Mechanics
-#endif
-!
   IF(HasMM()) THEN
-    ! In case of MM, append MM atoms to nuclear-exponent list 
-    ! and update counters and offsets
-    QMOffSetQ=CalNDist(Rho)
-    QMOffSetR=CalNCoef(Rho)
-    Rho%NQ%I(Rho%NExpt)=QMOffSetQ+GM_MM%Natms
+     ! In case of MM, append MM atoms to nuclear-exponent list 
+     ! and update counters and offsets
+     QMOffSetQ=CalNDist(Rho)
+     QMOffSetR=CalNCoef(Rho)
+     Rho%NQ%I(Rho%NExpt)=QMOffSetQ+GM_MM%Natms
   ENDIF
+#endif
   ! Calculate NDist and NCoef from NQ and Lndx
   NDist = CalNDist(Rho)
   NCoef = CalNCoef(Rho)
@@ -187,10 +173,7 @@ PROGRAM MakeRho
   ! Initailize  RhoCo and First
   First = .TRUE.
   Rho%Co%D=zero
-!-----------------------------------------------------
-! Loop over atoms and calculate the electronic 
-! density
-!
+  ! Loop over atoms and calculate the electronic density
 #ifdef MMech
   IF(HasQM())THEN
 #endif
@@ -220,7 +203,6 @@ PROGRAM MakeRho
            ENDIF
         ENDDO
      ENDDO
-!--------------------------------------------------------
      ! Add in the density for the nuclear centers
      IF(SCFActn/='InkFok') CALL AddNukes(GM,Rho)
 #ifdef MMech
@@ -231,113 +213,122 @@ PROGRAM MakeRho
      CALL AddNukes(GM_MM,Rho,(/QMOffSetQ,QMOffSetR/))
   ENDIF
 #endif
-!--------------------------------------------------------
 #ifdef PERIODIC 
-!-----------------------------------------------------------
-!  Fold the Distributions back into the Cell
-!
+  !  Fold the Distributions back into the Cell
   CALL Fold_Rho(GM,Rho)
 #endif
-!
-! Prune negligible distributions from the electronic density
+  ! Prune negligible distributions from the electronic density
   CALL Prune_Rho(Thresholds%Dist,Rho,Rho2) 
-!
-! Compute integrated electron and nuclear densities
+  ! Compute integrated electron and nuclear densities
+#ifdef MMech
   N1=0
   N2=0
   IF(HasMM())N1=GM_MM%Natms
   IF(HasQM())N2=GM%Natms
   CALL Integrate_HGRho(Rho2,RSumE,RSumN,RSumMM,N1,N2)
-    CALL Get(OutFile,'outputfile')
-    CALL OpenASCII(OutFile,Out)
-    write(out,*) 'Charge integrals: ',RSumE,RSumN,RSumMM
-    CLOSE(UNIT=Out,STATUS='KEEP') 
-! Calculate dipole and quadrupole moments
-  CALL New(MP)
-#ifdef PERIODIC
-  IF(HasMM()) THEN
-!   CALL Halt(' PBCs and QM/MM not yet hacked together...')
-    Center(:) = GM_MM%PBC%CellCenter(:)
-  ELSE
-    Center(:) = GM%PBC%CellCenter(:)
-  ENDIF
 #else
+  CALL Integrate_HGRho(Rho2,RSumE,RSumN,RSumMM,0,GM%Natms)
+#endif
+  ! Calculate dipole and quadrupole moments
+  CALL New(MP)
+  ! Find center of system to expand multipole moments about
+  ! This is arbitrary for non-periodic systems...
+#ifdef PERIODIC
+#ifdef MMech
   IF(HasMM()) THEN
-    Center(:) = Half*(GM_MM%BndBox%D(:,2)+GM_MM%BndBox%D(:,1))
+     CALL Warn(' PBCs and QM/MM not yet hacked together...')
+     Center(:) = GM_MM%PBC%CellCenter(:)
   ELSE
-    Center(:) = Half*(GM%BndBox%D(:,2)+GM%BndBox%D(:,1))
+#endif
+     Center(:) = GM%PBC%CellCenter(:)
+#ifdef MMech
   ENDIF
 #endif
+#else
+#ifdef MMech
+  IF(HasMM()) THEN
+     Center(:) = Half*(GM_MM%BndBox%D(:,2)+GM_MM%BndBox%D(:,1))
+  ELSE
+#endif
+     Center(:) = Half*(GM%BndBox%D(:,2)+GM%BndBox%D(:,1))
+#ifdef MMech
+  ENDIF
+#endif
+#endif
   CALL CalRhoPoles(MP,Center,Rho2)
-! Format output for pruning and multipole stats
+  ! Format output for pruning and multipole stats
+#ifdef MMech
   IF(HasQM()) THEN
-  IF(SCFActn=='InkFok')THEN
-     Mssg1=ProcessName(Prog,'InkFok')
-     Mssg2=Mssg1
-  ELSE
-     Mssg1=ProcessName(Prog,'Pruned Rho')
-     Mssg2=ProcessName(Prog,'Moments')
-  ENDIF
-  ENDIF
-!
-! Total system charge may be nonzero
-!
-  IF(MMOnly())THEN
-     RelRhoErr=(ABS(Two*RSumMM+GM_MM%TotCh))/DBLE(GM_MM%Natms)
-  ELSEIF(HasQM().AND.HasMM())THEN
-     RelRhoErr=ABS(Two*(RSumE+RSumN)+GM%TotCh+Two*RSumMM+GM_MM%TotCh)/DBLE(GM%Natms+NEl+GM_MM%Natms)
-  ELSE
-     RelRhoErr=ABS(Two*(RSumE+RSumN)+GM%TotCh)/DBLE(GM%Natms+NEl)
-  ENDIF
-!
-  IF(HasQM())THEN
-     Mssg1=TRIM(Mssg1)//'dNel = '                       &
-          //TRIM(DblToShrtChar(Two*ABS(RSumE+RSumN)))   &
-          //', '//TRIM(IntToChar(FLOOR(1.D2*DBLE(Rho2%NDist)/DBLE(Rho%NDist)))) &
-          //'% of distributions retained'
-     Mssg2=TRIM(Mssg2)                                     &
-          //'<r> = ('//TRIM(DblToShrtChar(MP%DPole%D(1))) &
-          //', '//TRIM(DblToShrtChar(MP%DPole%D(2)))      &
-          //', '//TRIM(DblToShrtChar(MP%DPole%D(3)))      &
-          //'), <r^2> = '//TRIM(DblToShrtChar(            &
-          MP%QPole%D(1)+MP%QPole%D(2)+MP%QPole%D(3)))
-     ! Output pruning and multipole stats
-     IF(PrintFlags%Key==DEBUG_MAXIMUM)THEN
-        I=OpenPU()
-        WRITE(*,*)TRIM(Mssg1)
-        WRITE(*,*)TRIM(Mssg2)
-        WRITE(I,*)TRIM(Mssg1)
-        WRITE(I,*)TRIM(Mssg2)
-        CALL ClosePU(I)
-     ELSEIF(PrintFlags%Key==DEBUG_MEDIUM)THEN
-        I=OpenPU()
-        WRITE(I,*)TRIM(Mssg1)
-        WRITE(I,*)TRIM(Mssg2)
-        CALL ClosePU(I)
+#endif
+     IF(SCFActn=='InkFok')THEN
+        Mssg1=ProcessName(Prog,'InkFok')
+        Mssg2=Mssg1
+     ELSE
+        Mssg1=ProcessName(Prog,'Pruned Rho')
+        Mssg2=ProcessName(Prog,'Moments')
      ENDIF
-
+#ifdef MMech
   ENDIF
-
-!! Check for error
-!
-! write(*,*) 'threshold= ',Thresholds%Dist,' QM TotCh= ',GM%TotCh,GM_MM%TotCh
+#endif
+  QMCharge=Zero
+  dQMCharge=Zero
+  MMCharge=Zero
+  dMMCharge=Zero  
+#ifdef MMech
+  IF(HasQM())THEN
+#endif
+     QMCharge=Two*(RSumE-RSumN)
+     dQMCharge=Two*RSumE-NEl
+     PcntDist=FLOOR(1.D2*DBLE(Rho2%NDist)/DBLE(Rho%NDist))
+#ifdef MMech
+  ENDIF
+  IF(HasMM())THEN
+     MMCharge=Two*RSumMM
+     dMMCharge=MMCharge+GM_MM%TotCh
+  ENDIF
   IF(MMOnly())THEN
-    IF(RelRhoErr>0.000005.AND.SCFActn/='NumForceEvaluation') & !!!!!temporary!
-!   IF(RelRhoErr>Thresholds%Dist*1.D3.AND.SCFActn/='NumForceEvaluation') &
-       CALL Halt('In MakeRho, missing '//TRIM(DblToShrtChar(Two*RSumMM+GM_MM%TotCh))   &
-       //' charges after pruning.')
-  ELSEIF(HasQM().AND.HasMM())THEN
-    IF(RelRhoErr>Thresholds%Dist*1.D3.AND.SCFActn/='NumForceEvaluation') &
-       CALL Halt('In MakeRho, missing '//TRIM(DblToShrtChar(Two*(RSumE+RSumN)+GM%TotCh+Two*RSumMM+GM_MM%TotCh))   &
-       //' charges after pruning.')
+     Mssg1=TRIM(Mssg1)//' dMMChg = ' //TRIM(DblToShrtChar(dMMCharge))
+     RelRhoErr=ABS(MMCharge)/DBLE(GM_MM%Natms)
+  ELSEIF(QMOnly())THEN
+#endif
+     Mssg1=TRIM(Mssg1)//' dNel = '//TRIM(DblToShrtChar(dQMCharge))//', kept '  &
+          //TRIM(IntToChar(PcntDist))//'% of distributions.'
+     RelRhoErr=ABS(dQMCharge)/DBLE(NEl)
+#ifdef MMech
   ELSE
-    IF(RelRhoErr>Thresholds%Dist*1.D3.AND.SCFActn/='NumForceEvaluation') &
-       CALL Halt('In MakeRho, missing '//TRIM(DblToShrtChar(Two*(RSumE+RSumN)+GM%TotCh))   &
-       //' charges after pruning.')
+     Mssg1=TRIM(Mssg1)//' QMChg = '//TRIM(DblToShrtChar(QMCharge))//', '       &
+          //'MMChg = '//TRIM(DblToShrtChar(MMCharge))//', '       &
+          //'dNel = '//TRIM(DblToShrtChar(dQMCharge))//', kept '  &
+          //TRIM(IntToChar(PcntDist))//'% of distributions.'
+     RelRhoErr=ABS(QMCharge+MMCharge)/DBLE(NEl) 
   ENDIF
-!
-
-! Put Rho and MPs to disk
+#endif
+  Mssg2=TRIM(Mssg2)                                      &
+        //' <r> = ('//TRIM(DblToShrtChar(MP%DPole%D(1))) &
+        //', '//TRIM(DblToShrtChar(MP%DPole%D(2)))       &
+        //', '//TRIM(DblToShrtChar(MP%DPole%D(3)))       &
+        //'), <r^2> = '//TRIM(DblToShrtChar(             &
+           MP%QPole%D(1)+MP%QPole%D(2)+MP%QPole%D(3)))
+  ! Output pruning and multipole stats
+  IF(PrintFlags%Key==DEBUG_MAXIMUM)THEN
+     I=OpenPU()
+     WRITE(*,*)TRIM(Mssg1)
+     WRITE(*,*)TRIM(Mssg2)
+     WRITE(I,*)TRIM(Mssg1)
+     WRITE(I,*)TRIM(Mssg2)
+     CALL ClosePU(I)
+  ELSEIF(PrintFlags%Key==DEBUG_MEDIUM)THEN
+     I=OpenPU()
+     WRITE(I,*)TRIM(Mssg1)
+     WRITE(I,*)TRIM(Mssg2)
+     CALL ClosePU(I)
+  ENDIF
+  ! Check error
+  IF(RelRhoErr>Thresholds%Dist*5.D3.AND.SCFActn/='NumForceEvaluation') &
+       CALL Halt('In MakeRho, relative error in density = '//TRIM(DblToShrtChar(RelRhoErr)) &
+       //'. Distribution threshold = '//TRIM(DblToShrtChar(Thresholds%Dist))      &
+       //'. Total charge lost = '//TRIM(DblToShrtChar(dQMCharge+dMMCharge)))
+  ! Put Rho and MPs to disk
   IF(SCFActn=='ForceEvaluation')THEN
      CALL Put_HGRho(Rho2,'Rho',Args,1) 
      CALL Put(MP,NxtCycl)
@@ -349,19 +340,23 @@ PROGRAM MakeRho
      CALL Put(MP,IntToChar(Current(1))) 
   ENDIF
   CALL PChkSum(Rho2,'Rho',Prog)
-! Tidy up
+  ! Tidy up
+#ifdef MMech
   IF(HasMM()) THEN
-    CALL Delete(GM_MM)
+     CALL Delete(GM_MM)
   ENDIF
   IF(HasQM()) THEN
-    CALL Delete(GM)
-    CALL Delete(Dmat)
-    CALL Delete(BS)
-    CALL Delete(MD)
-    CALL DeleteBraBlok()
+#endif
+     CALL Delete(GM)
+     CALL Delete(Dmat)
+     CALL Delete(BS)
+     CALL Delete(MD)
+     CALL DeleteBraBlok()
+#ifdef MMech
   ENDIF
-    CALL Delete_HGRho(Rho)
-    CALL Delete_HGRho(Rho2)
-    CALL ShutDown(Prog)
+#endif
+  CALL Delete_HGRho(Rho)
+  CALL Delete_HGRho(Rho2)
+  CALL ShutDown(Prog)
 END PROGRAM MakeRho
 
