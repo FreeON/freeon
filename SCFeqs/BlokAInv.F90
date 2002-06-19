58f13a812daa7c1b144788aa3057e1a400f327a2
mchalla <mchalla>
2002-06-19 22:34:59 +0000

    Moved routines for orthogonalization transformations here, since most of their functionality has been subsumed into MondoMods anyway.

---
+++ SCFeqs/BlokAInv.F90
@@ -0,0 +1,136 @@
+  !    COMPUTE THE INCOMPLETE INVERSE CHOLESKY FACTOR 
+  !    OF THE OVERLAP MATRIX Z=S^(-L) 
+  !    Author: Matt Challacombe
+  !-----------------------------------------------------------------
+  PROGRAM BlokAInv
+    USE DerivedTypes
+    USE GlobalScalars
+    USE GlobalCharacters
+    USE InOut
+    USE PrettyPrint
+    USE MemMan
+    USE Parse
+    USE Macros
+    USE LinAlg
+    USE AInv
+#ifdef NAG
+    USE F90_UNIX_ENV
+#endif
+    IMPLICIT NONE
+    TYPE(BCSR)          :: A,Z,Zt,DiagD
+    !#ifdef SPATIAL_THRESHOLDING
+    TYPE(BCSR)          :: T1,T2
+    !#endif 
+#ifdef FIND_CONDA
+    TYPE(DBL_RNK2)      :: B,C
+#endif
+    TYPE(BSET)          :: BS
+    TYPE(CRDS)          :: GM
+    TYPE(INT_VECT)      :: AiFlg,ZiFlg,ColPt,BlkPt
+    TYPE(ARGMT)         :: Args
+    INTEGER             :: I,J,Q,R,IDex,JDex,ZDex,ZBlk,NIJ, &
+         n,ni,msiz,strtai,stopai,strtaj,stopaj, &
+         strtzi,stopzi,nj,strtzj,stopzj,jcol,k,kdex, &
+         aiblk,ajblk,zjblk,m,ziblk,icol,zrowpt,zcolpt, & 
+         zblkpt,NewBloks,EndBloks,IRow,JRow,ZBlksPreFilter,ZBlksPostFilter
+    TYPE(DBL_VECT)      :: Blk1,Blk2
+    TYPE(DBL_RNK2)      :: P,DA
+    REAL(DOUBLE)        :: Op,Mx0,B2Norm
+
+    REAL(DOUBLE)        :: AInvDistanceThresh,IRowX,IRowY,IRowZ
+    TYPE(AtomPair)      :: Pair
+    REAL(DOUBLE), &
+         EXTERNAL :: DDOT
+
+    LOGICAL             :: TEST_AINV
+    CHARACTER(LEN=DEFAULT_CHR_LEN) :: Mssg
+    CHARACTER(LEN=8),&
+         PARAMETER  :: Prog='BlokAInv'
+!-----------------------------------------------------------------------------------------------------------
+    ! Start up macro
+    CALL StartUp(Args,Prog)
+    ! Get basis set and geometry
+    CALL Get(BS,Tag_O=CurBase)
+    CALL Get(GM,Tag_O=CurGeom)
+    TEST_AINV=.TRUE.
+#ifdef SPATIAL_THRESHOLDING
+    IF(GM%Ordrd==SFC_HILBERT.OR.GM%Ordrd==SFC_PEANO)THEN
+       CALL OpenASCII(InpFile,Inp)
+       IF(OptDblQ(Inp,Prog,AInvDistanceThresh))THEN
+          WRITE(*,*)'From input, AInvDistanceThresh = ',AInvDistanceThresh
+       ELSE
+          AInvDistanceThresh=1.D3
+          WRITE(*,*)' Using default AInvDistanceThreshold=',AInvDistanceThresh
+       ENDIF
+       IF(OptKeyQ(Inp,Prog,'NoTest'))THEN
+          TEST_AINV=.FALSE.
+       ENDIF
+       CLOSE(UNIT=Inp)
+    ELSE
+       AInvDistanceThresh=1.D24
+       TEST_AINV=.FALSE.
+       WRITE(*,*)' Using O(N^2) AInv due to non-local ordering. Try HOrder or ZOrder '
+    ENDIF
+#endif
+    ! Allocations 
+    CALL New(A)
+    CALL Get(A,TrixFile('S',Args))
+    ! Blocked AINV
+    CALL BlockedAInv(A,Thresholds%Trix,GM,AInvDistanceThresh,Z,Zt,PerfMon)
+
+    IF(PrintFlags%Key>=DEBUG_MEDIUM)THEN
+       CALL PPrint(PerfMon,Prog)  
+       CALL PPrint(PerfMon,Prog,Unit_O=6)  
+    ENDIF
+    ! Consistency check
+    IF(TEST_AINV)THEN
+       CALL New(T1)
+       CALL New(T2)
+       CALL Multiply(Zt,A,T1)
+       CALL Multiply(T1,Z,T2)
+       !
+       CALL SetToI(T1)
+       CALL Multiply(T1,-One)
+       CALL Add(T1,T2,A)
+       Mx0=Max(A)
+       !
+       IF(AInvDistanceThresh/=Zero)THEN     
+          Mssg='Max(Z^t.A.Z-I)='//TRIM(DblToShrtChar(Mx0))                 &
+               //', DistThrsh='//TRIM(DblToShrtChar(AInvDistanceThresh))  &
+               //', TrixThrsh='//TRIM(DblToShrtChar(Thresholds%Trix))
+       ELSE
+          Mssg='Max(Z^t.A.Z-I)='//TRIM(DblToShrtChar(Mx0))                 &
+               //', TrixThrsh='//TRIM(DblToShrtChar(Thresholds%Trix))
+       ENDIF
+       IF(Mx0>1.D2*Thresholds%Trix)THEN
+          CALL Warn('In BlokAInv, failed test: '//TRIM(Mssg))
+       ENDIF
+       IF(PrintFlags%Key==DEBUG_MAXIMUM)THEN
+          Mssg=ProcessName(Prog)//TRIM(Mssg)
+          WRITE(*,*)TRIM(Mssg)
+          CALL OpenASCII(OutFile,Out)
+          CALL PrintProtectL(Out)
+          WRITE(Out,*)TRIM(Mssg)
+          CALL PrintProtectR(Out)
+          CLOSE(UNIT=Out,STATUS='KEEP')
+       ENDIF
+    ENDIF
+    !  Put Z and ZT to disk
+    CALL Put(Z,TrixFile('Z',Args))
+    CALL Put(Zt,TrixFile('ZT',Args))
+    !  Debug
+    CALL PChkSum(Z,'Z',Proc_O=Prog)
+    CALL PPrint(Z,'Z')
+    CALL Plot(Z,'Z')
+    !  Tidy up
+    CALL Delete(Z)
+    CALL Delete(ZT)
+    CALL ShutDown(Prog) 
+  END PROGRAM BlokAInv
+
+
+
+
+
+
+
---
+++ SCFeqs/LowdinO.F90
@@ -0,0 +1,188 @@
+PROGRAM LowdinO
+  USE DerivedTypes
+  USE GlobalScalars
+  USE GlobalCharacters
+  USE InOut
+  USE PrettyPrint
+  USE MemMan
+  USE Parse
+  USE Macros
+  USE SetXYZ
+  USE LinAlg
+  IMPLICIT NONE
+#ifdef DSYEVD  
+  INTERFACE DSYEVD
+     SUBROUTINE DSYEVD(JOBZ,UPLO,N,A,LDA,W,WORK,LWORK,IWORK,LIWORK,INFO)
+         USE GlobalScalars
+         CHARACTER(LEN=1), INTENT(IN)    :: JOBZ, UPLO
+         INTEGER,          INTENT(IN)    :: LDA, LIWORK, LWORK, N
+         INTEGER,          INTENT(OUT)   :: INFO
+         INTEGER,          INTENT(OUT)   :: IWORK(*)
+         REAL(DOUBLE),     INTENT(INOUT) :: A(LDA,*)
+         REAL(DOUBLE),     INTENT(OUT)   :: W(*)
+         REAL(DOUBLE),     INTENT(OUT)   :: WORK(*)
+     END SUBROUTINE DSYEVD
+  END INTERFACE
+#else
+  INTERFACE DSYEV
+     SUBROUTINE DSYEV(JOBZ,UPLO,N,A,LDA,W,WORK,LWORK,INFO)
+         USE GlobalScalars
+         CHARACTER(LEN=1), INTENT(IN)    :: JOBZ, UPLO
+         INTEGER,          INTENT(IN)    :: LDA,  LWORK, N
+         INTEGER,          INTENT(OUT)   :: INFO
+         REAL(DOUBLE),     INTENT(INOUT) :: A(LDA,*)
+         REAL(DOUBLE),     INTENT(OUT)   :: W(*)
+         REAL(DOUBLE),     INTENT(OUT)   :: WORK(*)
+     END SUBROUTINE DSYEV
+  END INTERFACE
+#endif
+  TYPE(BCSR)                     :: S,X
+  TYPE(DBL_RNK2)                 :: Vectors,Tmp1,Tmp2
+  TYPE(DBL_VECT)                 :: Values,Work
+  TYPE(INT_VECT)                 :: IWork
+  TYPE(ARGMT)                    :: Args
+  INTEGER                        :: I,J,K,LgN,LWORK,LIWORK,Info,Status
+  CHARACTER(LEN=7),PARAMETER     :: Prog='LowdinO'
+  REAL(DOUBLE)                   :: Chk,CondS
+!--------------------------------------------------------------------
+  CALL StartUp(Args,Prog)
+  CALL New(Values,NBasF)
+  CALL New(Vectors,(/NBasF,NBasF/))
+  CALL Get(S,TrixFile('S',Args))
+  CALL SetEq(Vectors,S)
+  CALL Delete(S)
+!--------------------------------------------------------------------
+!
+!
+#ifdef DSYEVD
+  DO K=4,10000
+     IF(2**K>=NBasF)THEN
+        LgN=K
+        EXIT
+     ENDIF   
+  ENDDO
+  LWORK=2*(1+5*NBasF+2*NBasF*LgN+3*NBasF**2)
+  LIWORK=2*(2+5*NBasF)
+  CALL New(Work,LWork)
+  CALL New(IWork,LIWork)
+  CALL DSYEVD('V','U',NBasF,Vectors%D,NBasF,Values%D, &
+              Work%D,LWORK,IWork%I,LIWORK,Info)
+  IF(Info/=SUCCEED) &
+  CALL Halt('DSYEVD flaked in LowdinO. INFO='//TRIM(IntToChar(Info)))
+  CALL Delete(Work)
+  CALL Delete(IWork)
+#else
+  LWORK=MAX(1,3*NBasF)
+  CALL New(Work,LWork)
+  CALL DSYEV('V','U',NBasF,Vectors%D,NBasF,Values%D,Work%D,LWORK,Info)
+  IF(Info/=SUCCEED) &
+  CALL Halt('DSYEV flaked in LowdinO. INFO='//TRIM(IntToChar(Info)))
+  CALL Delete(Work)
+#endif
+!--------------------------------------------------------------------
+  IF(Values%D(1)<Zero)CALL Halt(' S matrix is not pos def')
+  CondS=Values%D(NBasF)/Values%D(1)
+  IF(CondS>1D4)  &
+     CALL Warn('Illconditioning detected in MakeS: Cond(S)='//TRIM(DblToShrtChar(CondS)))
+!--------------------------------------------------------------------
+  CALL New(Tmp2,(/NBasF,NBasF/))
+  CALL New(Tmp1,(/NBasF,NBasF/))
+!--------------------------------------------------------------------
+  Tmp1%D=Zero
+  DO I=1,NBasF
+     IF(Values%D(I)>1.D-12)THEN
+        Tmp1%D(I,I)=One/SQRT(Values%D(I))
+     ELSE
+        Tmp1%D(I,I)=Zero
+     ENDIF
+  ENDDO     
+  CALL DGEMM('N','N',NBasF,NBasF,NBasF,One,Vectors%D, &
+             NBasF,Tmp1%D,NBasF,Zero,Tmp2%D,NBasF)
+  CALL DGEMM('N','T',NBasF,NBasF,NBasF,One,Tmp2%D,    &
+             NBasF,Vectors%D,NBasF,Zero,Tmp1%D,NBasF)
+!--------------------------------------------------------------------
+  CALL Delete(Values)
+  CALL Delete(Vectors)
+  CALL Delete(Tmp2)
+!--------------------------------------------------------------------
+  CALL SetEq(S,Tmp1)    
+  CALL Delete(Tmp1)
+  CALL Filter(X,S)      
+!--------------------------------------------------------------------
+  CALL Put(X,TrixFile('X',Args)) 
+  CALL PChkSum(X,'X',Prog)
+  CALL PPrint(X,'X')
+  CALL Plot(X,'X')
+!--------------------------------------------------------------------
+  CALL Delete(S)
+  CALL Delete(X)
+  CALL ShutDown(Prog)
+!
+END PROGRAM LowdinO
+
+
+#ifdef BROKEN_CODE
+
+!
+!!$  DO I=1,NBasF
+!!$     DO J=1,NBasF
+!!$        SUM = Zero
+!!$        DO K=1,NBasF
+!!$           IF(ABS(Values%D(K)) .GT. 1.D-10) THEN
+!!$              SUM = SUM + Vectors%D(I,K)*Vectors%D(J,K)/SQRT(Values%D(K))
+!!$           ENDIF
+!!$        ENDDO
+!!$        Tmp1%D(I,J) = SUM
+!!$     ENDDO
+!!$  ENDDO
+!------------------------------------------------------------------------
+!
+!
+  DO I=1,NBasF
+     DO J=1,NBasF
+        IF(ABS(Values%D(J)) .GT. 1.D-8) THEN
+           Tmp1%D(I,J) = Vectors%D(I,J)/SQRT(Values%D(J))
+           Tmp2%D(I,J) = Vectors%D(J,I)
+        ENDIF
+     ENDDO
+  ENDDO
+  CALL DGEMM('N','N',NBasF,NBasF,NBasF,One,Tmp1%D,NBasF,Tmp2%D,NBasF,Zero,Vectors%D,NBasF)
+!
+!
+! Test for Inverse
+!    
+  CALL Get(S,TrixFile('S',Args))
+  CALL SetEq(Tmp1,S)
+  CALL Delete(S)
+!
+  DO I=1,NBasF
+     DO J=1,NBasF
+        SUM = Zero
+        DO K=1,NBasF
+           SUM = SUM + Vectors%D(I,K)*Vectors%D(K,J)
+        ENDDO
+        Tmp2%D(I,J) = SUM
+     ENDDO
+  ENDDO
+!           
+  Error = Zero
+  DO I=1,NBasF
+     DO J=1,NBasF
+        SUM = Zero
+        DO K=1,NBasF
+           SUM = SUM + Tmp1%D(I,K)*Tmp2%D(K,J)
+        ENDDO
+        IF(I==J) THEN
+           Error = Error + ABS((One-SUM))
+        ELSE
+           Error = Error + ABS(SUM)
+        ENDIF
+     ENDDO
+  ENDDO
+  WRITE(*,*) 'Num of Basis Fun   = ',NBasF
+  WRITE(*,*) 'Lowest  Eigenvalue = ',Values%D(1)
+  WRITE(*,*) 'Highest Eigenvalue = ',Values%D(NBasF)
+  WRITE(*,*) 'Condition Number   = ',Values%D(NBasF)/Values%D(1)
+  WRITE(*,*) 'Error in Inverse   = ',Error
+
+#endif
