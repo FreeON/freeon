5d1095ebb0be6cb939241d132d80ebd62e13d9b6
tymczak <tymczak>
2002-11-04 22:51:26 +0000

    FFELL has been moved here

--- QCTC/MondoPoles.F90
+++ QCTC/MondoPoles.F90
@@ -46,11 +46,16 @@ MODULE MondoPoles
 !====================================================================================================
 !  Global Array intermediates for multipole computations
 !====================================================================================================
-   REAL(DOUBLE), DIMENSION(0:2*FFEll2)          :: Factorial
-   REAL(DOUBLE), DIMENSION(0:FFEll2)            :: FactOlm0,FactMlm0,Sine,Cosine,CoFact
-   REAL(DOUBLE), DIMENSION(0:FFLen2)            :: FactOlm2,FactMlm2,ALegendreP,Spq,Cpq
-   REAL(DOUBLE), DIMENSION(0:SPEll+1,0:FFELL)   :: FudgeFactorial
-   CONTAINS
+  INTEGER,PARAMETER                            :: FFELL=64
+  INTEGER,PARAMETER                            :: FFELL2=2*FFELL
+  INTEGER,PARAMETER                            :: FFLen=FFEll*(FFEll+3)/2 
+  INTEGER,PARAMETER                            :: FFLen2=FFEll2*(FFEll2+3)/2
+!
+  REAL(DOUBLE), DIMENSION(0:2*FFEll2)          :: Factorial
+  REAL(DOUBLE), DIMENSION(0:FFEll2)            :: FactOlm0,FactMlm0,Sine,Cosine,CoFact
+  REAL(DOUBLE), DIMENSION(0:FFLen2)            :: FactOlm2,FactMlm2,ALegendreP,Spq,Cpq
+  REAL(DOUBLE), DIMENSION(0:SPEll+1,0:FFELL)   :: FudgeFactorial
+CONTAINS
 !====================================================================================
 !     Q->P
 !====================================================================================
