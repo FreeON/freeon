bdab17d948ccb7371138a0e33b7bf53c0198cca8
hugh <hugh>
2002-11-27 18:56:59 +0000

    Extra MD files

---
+++ MondoSCF/MD.F90
@@ -0,0 +1,350 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Routines to do Molecular Dyanamics                    !
+! Author: Hugh Nymeyer                                  !
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+!--------------------------------------------------------
+!
+MODULE MD
+!
+  USE GlobalCharacters      ! DEFAULT_CHR_LEN
+  USE DerivedTypes          ! DBL_VECTOR, etc, etc
+  USE GlobalScalars         ! conversion constants
+  USE SCFLocals             ! SCFControls, CRDS
+  USE MDLocals              ! MDState
+  USE MDIO                  ! I/O of special files
+  USE MDUtil                ! removeCM, AssignBoltzmannVel....
+  USE InOut                 ! Get and Put
+  USE AtomPairs             ! WrapAtoms
+  USE DrvFrcs               ! Force routines
+!
+  IMPLICIT NONE
+  CONTAINS
+!
+!--------------------------------------------------------
+!********************************************************
+!--------------------------------------------------------
+!
+  SUBROUTINE CALC_MD(Ctrl)
+!
+! Author: Hugh Nymeyer
+! Does: MD setup and MD calls
+!
+    IMPLICIT NONE
+!
+    TYPE(SCFControls)               :: Ctrl
+    TYPE(CRDS)                      :: GM
+    TYPE(MDState)                   :: MDS
+    INTEGER                         :: i
+!
+    IF(Ctrl%MDC%ALGORITHM.EQ.1) THEN   
+       Call Velocity_Verlet_Setup(Ctrl,MDS)
+       Call Velocity_Verlet(Ctrl,MDS)
+    ELSE
+       Call HALT('Unknown MD algorithm option')
+    END IF
+!
+  END SUBROUTINE CALC_MD
+!
+!--------------------------------------------------------
+!********************************************************
+!--------------------------------------------------------
+!
+  SUBROUTINE Velocity_Verlet_Setup(Ctrl,MDS)
+!
+! Author: Hugh Nymeyer
+!
+    IMPLICIT NONE
+!
+    TYPE(SCFControls)               :: Ctrl
+    TYPE(MDState)                   :: MDS
+    TYPE(CRDS)                      :: GM  
+!
+!------------------------------------
+! Miscellaneous setup
+!------------------------------------
+!
+    CtrlVect=SetCtrlVect(Ctrl,'MD')  ! what does this do?
+    Call RANDOM_SEED
+!
+!------------------------------------
+! MDS Defaults
+!------------------------------------
+!
+    if (.NOT. Ctrl%MDC%RESTRT) then
+       MDS%STEP=1
+       MDS%TIME=0.0D0
+    end if
+!
+!------------------------------------
+! First set up geometry
+!------------------------------------
+!
+    Call Get(GM,Tag_O=IntToChar(Ctrl%Current(3)))
+#ifdef PERIODIC
+    Call WrapAtoms(GM)
+#endif
+!
+!------------------------------------
+! HACK -- Why aren't AbCarts set?
+!------------------------------------
+!
+    GM%AbCarts%D = GM%Carts%D
+!
+!------------------------------------
+! then velocities
+!------------------------------------
+!
+!    if (.NOT. Ctrl%MDC%RESTRT) then	
+!       if (Ctrl%MDC%TEMP0 .GT. 0.0D0) then
+!          Call AssignBoltzmannVelocity(GM,Ctrl%MDC%TEMP0)
+!       end if
+!    end if
+!
+!------------------------------------
+! zero CM, VCM, and Rotation 
+!------------------------------------
+!
+    if (Ctrl%MDC%REM_TRANS .AND. .NOT. Ctrl%MDC%RESTRT) then 
+       Call removeCM(GM)
+       Call removeVCM(GM)
+    end if
+!
+    if (Ctrl%MDC%REM_ROTAT) then
+       Call removeAngMoment(GM)
+    end if
+!
+!------------------------------------
+! read restart if necessary
+!------------------------------------
+!
+    if (Ctrl%MDC%RESTRT) then
+       Call OpenRESIN(Ctrl%MDC%RESTRT_IN,MDS%TITLE)
+       Call ReadRESIN(Ctrl,MDS,GM)
+       Call CloseRESIN
+    end if
+!
+!------------------------------------
+! save geometry to current
+!------------------------------------
+!
+    Call Put(GM,Tag_O=IntToChar(Ctrl%Current(3))) 
+!
+!------------------------------------
+! more MDS variables which depend on GM
+!------------------------------------
+!
+    Call OneSCF(Ctrl)
+    Call Get(MDS%E_POT,'Etot',StatsToChar(Ctrl%Current))
+    MDS%E_KIN = computeKE(GM)
+    MDS%E_TOT = MDS%E_POT + MDS%E_KIN
+    if (.NOT. Ctrl%MDC%RESTRT) &
+         MDS%E_TOT_START = MDS%E_TOT
+!
+!------------------------------------
+  END SUBROUTINE Velocity_Verlet_Setup
+!
+!--------------------------------------------------------
+!********************************************************
+!--------------------------------------------------------
+!
+  SUBROUTINE Velocity_Verlet(Ctrl,MDS)
+!
+! Author: Hugh Nymeyer 10-24-02
+! Purpose: Does N steps of verlet integration assuming that
+!          all the setup has already been done.
+!
+! Note: I'm trying to do everything here in units of:
+!       energy   : Hartrees
+!       distance : Bohrs
+!       mass     : Atomic Mass Units
+!   --> time     : sqrt(mass*dist**2/energy) 
+!
+    IMPLICIT NONE
+!
+    TYPE(SCFControls)             :: Ctrl
+    TYPE(MDState)                 :: MDS
+    TYPE(CRDS)                    :: GM,GMnew
+    INTEGER                       :: ICyc,IBas,IGeo
+    INTEGER                       :: Atom       ! atom counter
+    TYPE(DBL_VECT)                :: F          ! force array
+    INTEGER                       :: i
+    INTEGER                       :: STEP
+    REAL(DOUBLE)                  :: DT         ! alias for convenience
+    REAL(DOUBLE),PARAMETER        :: HALF=0.5D0 ! ditto
+!
+!--------------------------------------------------------
+!   Set aliases
+!--------------------------------------------------------
+!
+    DT=Ctrl%MDC%DT
+!
+!--------------------------------------------------------
+!   Setup
+!--------------------------------------------------------
+!
+    write(6,*)'Velocity Verlet setup'            ! debug
+    ICyc=Ctrl%Current(1)
+    IBas=Ctrl%Current(2)
+    IGeo=Ctrl%Current(3)
+    Call Get(GM    ,Tag_O=IntToChar(IGeo)) 
+    Call Get(GMnew ,Tag_O=IntToChar(IGeo))   ! to copy all the non-MD variables
+    Call New(F,3*GM%NAtms)
+!
+!--------------------------------------------------------
+!   Open Files
+!--------------------------------------------------------
+!
+    Call OpenRESOUT(Ctrl%MDC%RESTRT_OUT,MDS%TITLE,Ctrl%MDC%CLOBBER)
+    Call OpenCRD(Ctrl%MDC%CRD_NAME,MDS%TITLE,Ctrl%MDC%CLOBBER)
+    Call OpenVEL(Ctrl%MDC%VEL_NAME,MDS%TITLE,Ctrl%MDC%CLOBBER)
+    Call OpenENE(Ctrl%MDC%ENE_NAME,MDS%TITLE,Ctrl%MDC%CLOBBER)
+!
+!--------------------------------------------------------
+!   compute forces
+!--------------------------------------------------------
+!
+    write(6,*)'Computing force once at start'    ! debug
+    Call OneSCF(Ctrl)
+    Call Forces(Ctrl)
+    Call Get(F,'GradE',Tag_O=IntToChar(IGeo))
+    F%D = -F%D    ! Hartrees per Bohr
+!
+!--------------------------------------------------------
+!   enter main dynamics loop
+!--------------------------------------------------------
+!
+    write(6,*)'Main dynamics loop'               ! debug
+    do STEP = MDS%STEP,Ctrl%MDC%MAX_STEPS        ! main dynamics loop
+!
+!      position step: t->t+dt
+!
+       write(6,*)'Updating positions'            ! debug
+#ifdef PERIODIC
+       do Atom=1,GM%NAtms
+          do i=1,3
+             GMnew%Carts%D(i,Atom)      = &
+                  GM%Carts%D(i,Atom)    + &
+                  DT * GM%Vects%D(i,Atom) + &
+                  HALF * DT**2 * F%D(3*(Atom-1)+i) / GM%AtMss%D(Atom)
+          end do
+       end do
+!       Call WrapAtoms(GMnew)
+#else
+       do Atom=1,GM%NAtms
+          do i=1,3
+             GMnew%Carts%D(i,Atom)        = &
+                  GM%Carts%D(i,Atom)      + &
+                  DT * GM%Vects%D(i,Atom) + &
+                  HALF * DT**2 * F%D(3*(Atom-1)+i) / GM%AtMss%D(Atom)
+          end do
+       end do
+#endif
+!
+!      velocity half step: t->t+dt/2
+!
+       write(6,*)'Updating velocities to t+dt/2'            ! debug
+       do Atom=1,GM%NAtms
+          do i=1,3
+             GMnew%Vects%D(i,Atom)        = &
+                  GM%Vects%D(i,Atom)      + &
+                  HALF * DT *  F%D(3*(Atom-1)+i) / GM%AtMss%D(Atom)
+          end do
+       end do
+!
+!      save coordinates at t+dt and velocities at t+dt/2
+!
+       write(6,*)'Saving intermediate geometry/velocities'  ! debug
+       IGeo = Igeo + 1       
+       Ctrl%Current(3)=IGeo
+       Call Put(GMnew,Tag_O=IntToChar(IGeo))  
+       Call SetGlobalCtrlIndecies(Ctrl)
+!
+!      compute force at t+dt
+!
+       write(6,*)'Computing forces at t+dt'                 ! debug
+       Call OneSCF(Ctrl)
+       Call Forces(Ctrl)
+       Call Get(F,'GradE',Tag_O=IntToChar(IGeo))
+       F%D = -F%D
+!
+!      velocity half step: t+dt/2->t+dt     
+!
+       write(6,*)'Updating velocities to t+dt'              ! debug
+       do Atom=1,GM%NAtms
+          do i=1,3
+             GMnew%Vects%D(i,Atom)        = &
+                  GMnew%Vects%D(i,Atom)   + &
+                  HALF * DT *  F%D(3*(Atom-1)+i) / GM%AtMss%D(Atom)
+          end do
+       end do
+!
+!      remove translation/rotation if needed
+!
+       if (Ctrl%MDC%REM_TRANS) then
+          if (MOD(STEP,Ctrl%MDC%TRANSfreq) .EQ. 0) then
+             Call removeCM(GM)
+             Call removeVCM(GM)
+          end if
+       end if
+!
+       if (Ctrl%MDC%REM_ROTAT) then
+          if (MOD(STEP,Ctrl%MDC%ROTATfreq) .EQ. 0) then
+             Call removeAngMoment(GM)
+          end if
+       end if
+!
+!      save geometry again
+!
+       write(6,*)'Saving final geometry at time t+dt'       ! debug
+       Call Put(GMnew,Tag_O=IntToChar(IGeo))  
+!
+!      update MDS
+!
+       MDS%STEP = STEP
+       MDS%TIME = MDS%TIME + DT 
+       MDS%E_KIN = computeKE(GMnew)
+       call Get(MDS%E_POT,'Etot',StatsToChar(Ctrl%Current))
+       MDS%E_TOT = MDS%E_POT + MDS%E_KIN
+       MDS%TEMPERATURE = computeTinstant(Ctrl,GM)
+       MDS%DE_TOT = MDS%E_TOT - MDS%E_TOT_START
+!
+!      output
+!
+       if ( MOD(STEP,Ctrl%MDC%CRDfreq) .EQ. 0) &
+            Call WriteCRD(GM,Ctrl)
+       if ( MOD(STEP,Ctrl%MDC%VELfreq) .EQ. 0) &
+            Call WriteVEL(GM)
+       if ( MOD(STEP,Ctrl%MDC%ENEfreq) .EQ. 0) &
+            Call WriteENE(GM,Ctrl,MDS)
+       if ( MOD(STEP,Ctrl%MDC%RESfreq) .EQ. 0) &
+            Call WriteRESOUT(Ctrl,MDS,GM)
+!
+!      update GM structures
+!
+       Call Get(GM,Tag_O=IntToChar(IGeo)) 
+!
+!--------------------------------------------------------
+!   end main dynamics loop
+!--------------------------------------------------------
+!
+    end do
+!
+!--------------------------------------------------------
+!   cleanup
+!--------------------------------------------------------
+!
+    Call CloseCRD
+    Call CloseVEL
+    Call CloseENE
+    Call CloseRESOUT
+    Call Delete(F)
+!
+!--------------------------------------------------------
+!
+  END SUBROUTINE VELOCITY_VERLET
+!--------------------------------------------------------
+!********************************************************
+!--------------------------------------------------------
+!
+  END MODULE
---
+++ MondoSCF/MDIO.F90
@@ -0,0 +1,302 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! This module handles i/o for special MD files: CRD,   !
+! VEL, ENE, and RESTART files
+! Author: Hugh Nymeyer                                 !
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+MODULE MDIO
+!
+  USE GlobalCharacters        ! DEFAULT_CHR_LEN
+  USE GlobalScalars           ! Unit numbers
+  USE SCFLocals               ! SCFControls, MDControls
+  USE MDLocals                ! MDState
+!
+  IMPLICIT NONE
+  CONTAINS
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE OpenCRD(FILENAME,TITLE,CLOBBER)
+!
+    CHARACTER(LEN=DEFAULT_CHR_LEN)       :: FILENAME
+    CHARACTER(LEN=DEFAULT_CHR_LEN)       :: TITLE
+    LOGICAL                              :: CLOBBER
+    INTEGER                              :: OPEN_ERROR
+!
+    IF (CLOBBER) THEN
+       OPEN(UNIT   = CRD_UNIT,    &
+            FILE   = FILENAME,    &
+            STATUS = "REPLACE",   &
+            FORM   = "FORMATTED", &
+            IOSTAT = OPEN_ERROR)
+    ELSE
+       OPEN(UNIT   = CRD_UNIT,    &
+            FILE   = FILENAME,    &
+            STATUS = "NEW",       &
+            FORM   = "FORMATTED", &
+            IOSTAT = OPEN_ERROR)
+    ENDIF
+!
+    IF (OPEN_ERROR .NE. 0) THEN
+       CALL HALT('! OpenCRD failed')
+    END IF
+!
+    WRITE(UNIT=CRD_UNIT,FMT='(A80)')TITLE
+!
+  END SUBROUTINE OpenCRD
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE OpenVEL(FILENAME,TITLE,CLOBBER)
+!
+    CHARACTER(LEN=DEFAULT_CHR_LEN)       :: FILENAME
+    CHARACTER(LEN=DEFAULT_CHR_LEN)       :: TITLE
+    LOGICAL                              :: CLOBBER
+    INTEGER                              :: OPEN_ERROR
+!
+    IF (CLOBBER) THEN
+       OPEN(UNIT   = VEL_UNIT,    &
+            FILE   = FILENAME,    &
+            STATUS = "REPLACE",   &
+            FORM   = "FORMATTED", &
+            IOSTAT = OPEN_ERROR)
+    ELSE
+       OPEN(UNIT   = VEL_UNIT,    &
+            FILE   = FILENAME,    &
+            STATUS = "NEW",       &
+            FORM   = "FORMATTED", &
+            IOSTAT = OPEN_ERROR)
+    ENDIF
+!
+    IF (OPEN_ERROR .NE. 0) THEN
+       CALL HALT('! OpenVEL failed')
+    END IF
+!
+    WRITE(UNIT=VEL_UNIT,FMT='(A80)')TITLE
+!    
+!
+  END SUBROUTINE OpenVEL
+
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE OpenENE(FILENAME,TITLE,CLOBBER)
+!
+    CHARACTER(LEN=DEFAULT_CHR_LEN)       :: FILENAME
+    CHARACTER(LEN=DEFAULT_CHR_LEN)       :: TITLE
+    LOGICAL                              :: CLOBBER
+    INTEGER                              :: OPEN_ERROR
+!
+    IF (CLOBBER) THEN
+       OPEN(UNIT   = ENE_UNIT,    &
+            FILE   = FILENAME,    &
+            STATUS = "REPLACE",   &
+            FORM   = "FORMATTED", &
+            IOSTAT = OPEN_ERROR)
+    ELSE
+       OPEN(UNIT   = ENE_UNIT,    &
+            FILE   = FILENAME,    &
+            STATUS = "NEW",       &
+            FORM   = "FORMATTED", &
+            IOSTAT = OPEN_ERROR)
+    ENDIF
+!
+    IF (OPEN_ERROR .NE. 0) THEN
+       CALL HALT('! OpenENE failed')
+    END IF
+!
+    WRITE(UNIT=ENE_UNIT,FMT='(A80)')TITLE
+    WRITE(UNIT=ENE_UNIT,FMT='(A80)') &
+	"--------------------------------------------------------------------------------"
+    WRITE(UNIT=ENE_UNIT,FMT='(" 0 ",6(A14X))') &
+         "STEP_NUMBER","TIME","E_POT","E_KIN","E_TOT","DE_TOT"
+    WRITE(UNIT=ENE_UNIT,FMT='(" 1 ",4(A14X))') &
+         "BOX_X","BOX_Y","BOX_Z","VOLUME"
+    WRITE(UNIT=ENE_UNIT,FMT='(" 2 ",5(A14X))') &
+         "VIRIAL_X","VIRIAL_Y","VIRIAL_Z","SET_PRESSURE","PRESSURE"
+    WRITE(UNIT=ENE_UNIT,FMT='(" 3 ",2(A14X))') &
+         "SET_TEMP","TEMPERATURE"
+    WRITE(UNIT=ENE_UNIT,FMT='(" 4 ",4(A14X))') &
+         "VCM_X","VCM_Y","VCM_Z","CM_KIN"
+    WRITE(UNIT=ENE_UNIT,FMT='(" 5 ",4(A14X))') &
+         "OMEGA_X","OMEGA_Y","OMEGA_Z","OMEGA_KIN"
+    WRITE(UNIT=ENE_UNIT,FMT='(A80)') &
+	"--------------------------------------------------------------------------------"
+!
+  END SUBROUTINE OpenENE
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE CloseCRD
+    CLOSE(CRD_UNIT)
+  END SUBROUTINE CloseCRD
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE CloseVEL
+    CLOSE(VEL_UNIT)
+  END SUBROUTINE CloseVEL
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE CloseENE
+    CLOSE(ENE_UNIT)
+  END SUBROUTINE CloseENE
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE WriteCRD(GM,CTRL)
+!
+    TYPE(CRDS)          :: GM
+    TYPE(SCFControls)   :: CTRL
+    INTEGER             :: i
+!
+!  For now, no box info is written to crd file
+!  Need to convert to external units (Angstroms)
+!
+#ifdef PERIODIC
+    IF (CTRL%MDC%AtomWrap) THEN
+       WRITE(CRD_UNIT,FMT='(10F8.3)') &
+            (GM%Carts%D(1,i) * BohrsToAngstroms, &
+             GM%Carts%D(2,i) * BohrsToAngstroms, &
+             GM%Carts%D(3,i) * BohrsToAngstroms, &
+             i=1,GM%NAtms)
+    ELSE
+       WRITE(CRD_UNIT,FMT='(10F8.3)') &
+            (GM%Carts%D(1,i) * BohrsToAngstroms, &
+             GM%Carts%D(2,i) * BohrsToAngstroms, &
+             GM%Carts%D(3,i) * BohrsToAngstroms, &
+             i=1,GM%NAtms)
+    END IF
+#else
+    WRITE(CRD_UNIT,FMT='(10F8.3)') &
+         (GM%Carts%D(1,i) * BohrsToAngstroms,&
+          GM%Carts%D(2,i) * BohrsToAngstroms,&
+          GM%Carts%D(3,i) * BohrsToAngstroms,&
+          i=1,GM%NAtms)
+#endif
+!
+  END SUBROUTINE WriteCRD
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE WriteVEL(GM)
+!
+    TYPE(CRDS)          :: GM
+    INTEGER             :: i
+!
+!  For now, no box info is written to vel file
+!
+    WRITE(VEL_UNIT,FMT='(10F8.3)') &
+         (GM%Vects%D(1,i), &
+          GM%Vects%D(2,i), &
+          GM%Vects%D(3,i), &
+          i=1,GM%NAtms)
+!
+  END SUBROUTINE WriteVEL
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE WriteENE(GM,CTRL,MDS)
+!
+    TYPE(CRDS)          :: GM
+    TYPE(SCFControls)   :: CTRL
+    TYPE(MDState)       :: MDS
+    INTEGER             :: i
+!
+! Need to convert to "outside" units before printing
+!
+    WRITE(UNIT=ENE_UNIT,FMT='(" 0 ",I14X,5(E14.6E2X))') &
+         MDS%STEP                               , &
+	 MDS%TIME * InternalTimeToSeconds       , &
+	 MDS%E_POT                              , &
+         MDS%E_KIN                              , &
+         MDS%E_TOT                              , &
+         MDS%DE_TOT
+!
+#ifdef PERIODIC
+    WRITE(UNIT=ENE_UNIT,FMT='(" 1 ",4(E14.6E2X))') &
+         (GM%PBC%TransVec(i),i=1,3),GM%PBC%CellVolume
+#else
+    WRITE(UNIT=ENE_UNIT,FMT='(" 1 ",4(E14.6E2X))') &
+         0.0D0,0.0D0,0.0.0D0,0.0D0
+#endif
+!
+    WRITE(UNIT=ENE_UNIT,FMT='(" 2 ",5(E14.6E2X))') &
+         (MDS%VIRIAL(i),i=1,3),CTRL%MDC%PRES,MDS%PRESSURE
+!
+    WRITE(UNIT=ENE_UNIT,FMT='(" 3 ",2(E14.6E2X))') &
+         CTRL%MDC%TEMP,MDS%TEMPERATURE
+!
+    WRITE(UNIT=ENE_UNIT,FMT='(" 4 ",4(E14.6E2X)') &
+         (MDS%VCM(i),i=1,3),MDS%CM_KIN
+!
+    WRITE(UNIT=ENE_UNIT,FMT='(" 5 ",4(E14.6E2X))') &
+         (MDS%OMEGA(i),i=1,3),MDS%OMEGA_KIN
+!
+    WRITE(UNIT=ENE_UNIT,FMT='(A80)') &
+	"--------------------------------------------------------------------------------"
+!
+  END SUBROUTINE WriteENE
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE OpenRESIN(FILENAME,TITLE)
+!
+    CHARACTER(LEN=DEFAULT_CHR_LEN)       :: FILENAME
+    CHARACTER(LEN=DEFAULT_CHR_LEN)       :: TITLE
+    INTEGER                              :: OPEN_ERROR
+!
+!   Do Nothing for now
+!
+  END SUBROUTINE OpenRESIN
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE OpenRESOUT(FILENAME,TITLE,CLOBBER)
+!
+    CHARACTER(LEN=DEFAULT_CHR_LEN)       :: FILENAME
+    CHARACTER(LEN=DEFAULT_CHR_LEN)       :: TITLE
+    LOGICAL                              :: CLOBBER
+    INTEGER                              :: OPEN_ERROR
+!
+!   Do Nothing for now
+!
+  END SUBROUTINE OpenRESOUT
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE ReadRESIN(Ctrl,MDS,GM)
+!
+    TYPE(SCFControls)  :: Ctrl
+    TYPE(MDState)      :: MDS
+    TYPE(CRDS)         :: GM
+!
+!   Do Nothing for now
+!    
+  END SUBROUTINE ReadRESIN
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE WriteRESOUT(Ctrl,MDS,GM)
+!
+    TYPE(SCFControls)  :: Ctrl
+    TYPE(MDState)      :: MDS
+    TYPE(CRDS)         :: GM
+!
+!   Do Nothing for now
+!    
+  END SUBROUTINE WriteRESOUT
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE CloseRESIN
+  END SUBROUTINE CloseRESIN
+!
+!
+!--------------------------------------------------------
+!
+  SUBROUTINE CloseRESOUT
+  END SUBROUTINE CloseRESOUT
+!
+END MODULE MDIO
