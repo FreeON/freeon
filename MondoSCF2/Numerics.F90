444e14cb6f0ec9bf0c1a022ee5936057ff853d59
mchalla <mchalla>
2003-01-16 17:03:45 +0000

    Changed Thresholds to Numerics

--- MondoSCF2/Numerics.F90
+++ MondoSCF2/Numerics.F90
@@ -1,4 +1,4 @@
-MODULE ThresholdsMod
+MODULE Numerics
   USE DerivedTypes
   IMPLICIT NONE
   !
@@ -24,4 +24,4 @@ MODULE ThresholdsMod
   REAL(DOUBLE),DIMENSION(4) :: GTol       =(/ 5.D-2, 5.D-3, 5.D-4,  5.D-5  /)
   ! Max Cartesian displacement
   REAL(DOUBLE),DIMENSION(4) :: XTol       =(/ 1.D-1, 1.D-2, 1.D-3,  1.D-4  /)
-END MODULE ThresholdsMod
+END MODULE Numerics
