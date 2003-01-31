219bb61328f5f765a72e5cdc53488335b8889303
tymczak <tymczak>
2003-01-31 17:52:42 +0000

    Filter(S) is done at a lower threshold

--- OneE/MakeS.F90
+++ OneE/MakeS.F90
@@ -19,48 +19,52 @@ PROGRAM MakeS
 #endif
   IMPLICIT NONE
 #ifdef PARALLEL
-  TYPE(DBCSR)         :: S,T1
+  TYPE(DBCSR)                :: S,T1
 #else
-  TYPE(BCSR)          :: S,T1
+  TYPE(BCSR)                 :: S,T1
 #endif
 #ifdef PERIODIC 
-  INTEGER                   :: NC
-  REAL(DOUBLE),DIMENSION(3) :: B
+  INTEGER                    :: NC
+  REAL(DOUBLE),DIMENSION(3)  :: B
 #endif
-  TYPE(AtomPair)      :: Pair
-
-  TYPE(BSET)          :: BS
-  TYPE(CRDS)          :: GM
-  TYPE(DBL_RNK4)      :: MD
-
-  TYPE(ARGMT)         :: Args
-  INTEGER             :: P,R,AtA,AtB,NN                         
-  character(len=dcl) :: mssg
+  TYPE(AtomPair)             :: Pair
+  TYPE(BSET)                 :: BS
+  TYPE(CRDS)                 :: GM
+  TYPE(DBL_RNK4)             :: MD
+  TYPE(ARGMT)                :: Args
+  INTEGER                    :: P,R,AtA,AtB,NN                         
   CHARACTER(LEN=5),PARAMETER :: Prog='MakeS'
-  !--------------------------------------- 
-  ! Start up macro
+!--------------------------------------- 
+! Start up macro
+
   CALL StartUp(Args,Prog,Serial_O=.FALSE.)
-#ifdef PERIODIC 
-#ifdef PARALLEL_CLONES
-#else
-  CALL Get(CS_OUT,'CS_OUT',Tag_O=CurBase)
-#endif
-#endif
-  !----------------------------------------------
-  ! Get basis set and geometry
+!----------------------------------------------
+! Get basis set and geometry
+
   CALL Get(BS,Tag_O=CurBase)
   CALL Get(GM,Tag_O=CurGeom)
-  !---------------------------------------------- 
-  ! Allocations 
+!---------------------------------------------- 
+! Allocations 
+
   CALL NewBraBlok(BS)
   CALL New(S)
-  !-----------------------------------------------
-  ! Initialize the matrix and associated indecies
+#ifdef PERIODIC
+#ifdef PARALLEL_CLONES
+#else
+!-----------------------------------------------
+! Calculate the Number of Cells
+  CALL Get(CS_OUT,'CS_OUT',Tag_O=CurBase)
+  CALL PPrint(CS_OUT,'outer sum',Prog)
+#endif
+#endif
+!-----------------------------------------------
+! Initialize the matrix and associated indecies
 
   P=1; R=1; S%RowPt%I(1)=1
   CALL SetEq(S%MTrix,Zero)
-  !-----------------------------------------------
-  ! Main loops
+!-----------------------------------------------
+! Main loops
+
 #ifdef PARALLEL
   S%NAtms=0
   DO AtA=Beg%I(MyId),End%I(MyId)
@@ -77,8 +81,8 @@ PROGRAM MakeS
            DO NC = 1,CS_OUT%NCells
               Pair%B = B+CS_OUT%CellCarts%D(:,NC)
               Pair%AB2  = (Pair%A(1)-Pair%B(1))**2 &
-                   + (Pair%A(2)-Pair%B(2))**2 &
-                   + (Pair%A(3)-Pair%B(3))**2
+                        + (Pair%A(2)-Pair%B(2))**2 &
+                        + (Pair%A(3)-Pair%B(3))**2
               IF(TestAtomPair(Pair)) THEN
                  S%MTrix%D(R:R+NN-1)=S%MTrix%D(R:R+NN-1)+SBlok(BS,Pair)
               ENDIF
@@ -86,7 +90,7 @@ PROGRAM MakeS
 #else
            S%MTrix%D(R:R+NN-1)=SBlok(BS,Pair)
 #endif
-           
+
            S%ColPt%I(P)=AtB
            S%BlkPt%I(P)=R
            R=R+NN 
@@ -108,28 +112,30 @@ PROGRAM MakeS
   ENDDO
   S%NBlks=P-1
   S%NNon0=R-1
-  !------------------------------------------------------------
-  ! Put S to disk
+!------------------------------------------------------------
+! Put S to disk
+  Thresholds%Trix = Thresholds%Trix*1.D-2
   CALL Filter(T1,S)
-  !! the following line gives a problem!!
-  ! CALL PPrint(T1,'S',Unit_O=6)
+  Thresholds%Trix = Thresholds%Trix*1.D2
+!! the following line gives a problem!!
+! CALL PPrint(T1,'S',Unit_O=6)
   CALL Put(T1,TrixFile('S',Args))
-  !-----------------------------------------------------------
-  ! Printing
-  
+!-----------------------------------------------------------
+! Printing
+
   CALL PChkSum(T1,'S',Prog)
   CALL PPrint( T1,'S')
   CALL Plot(   T1,'S')
-  !------------------------------------------------------------
-  ! Tidy up
-  
+!------------------------------------------------------------
+! Tidy up
+
   CALL Delete(S)
   CALL Delete(T1)
   CALL Delete(BS)
   CALL Delete(GM)
   CALL DeleteBraBlok()
-  ! didn't count flops, any accumulation is residual
-  ! from matrix routines
+! didn't count flops, any accumulation is residual
+! from matrix routines
   PerfMon%FLOP=Zero 
   CALL ShutDown(Prog)
 END PROGRAM MakeS
