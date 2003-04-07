fa7a020a790e4e5d604064c3712a8fda96990a39
nemeth <nemeth>
2003-04-07 17:43:17 +0000

    Some more optimization keys added.

---
+++ MondoSCF2/GeomOptKeys.F90
@@ -0,0 +1,44 @@
+MODULE GeomOptKeys
+   USE GlobalScalars
+   USE GlobalCharacters
+   IMPLICIT NONE
+!------------------------------------------------  
+!
+   CHARACTER(LEN=3),  PARAMETER :: OPTIMIZATION     ='Opt'
+   CHARACTER(LEN=5),  PARAMETER :: OPT_QUNEW        ='QuNew'
+   CHARACTER(LEN=7),  PARAMETER :: OPT_StpDesc      ='StpDesc'
+   CHARACTER(LEN=8),  PARAMETER :: OPT_DiagHess     ='DiagHess'
+   CHARACTER(LEN=8),  PARAMETER :: OPT_CartDIIS     ='CartDIIS'
+   CHARACTER(LEN=7),  PARAMETER :: OPT_IntDIIS      ='IntDIIS'
+   CHARACTER(LEN=7),  PARAMETER :: OPT_NoGDIIS      ='NoGDIIS'
+   CHARACTER(LEN=8),  PARAMETER :: OPT_NoRotOff     ='NoRotOff'
+   CHARACTER(LEN=11), PARAMETER :: OPT_NoTranslOff  ='NoTranslOff'
+!  Perform quasi-newton geometry optimization for each basis set in turn
+   INTEGER, PARAMETER           :: GRAD_QNEW_OPT    = 3489343 
+!  Optimizer type is set to Steepest Descent
+   INTEGER, PARAMETER           :: GRAD_STPDESC_OPT = 3876123 
+!  Optimizer type is set to Diagonal Hessian 
+   INTEGER, PARAMETER           :: GRAD_DIAGHESS_OPT = 8942901 
+!--------------------------------------------------------------------------------------
+!
+    CHARACTER(LEN=9),  PARAMETER :: COORDTYPE='CoordType'
+    CHARACTER(LEN=7),  PARAMETER :: CoordType_PrimInt='PrimInt'
+    CHARACTER(LEN=9),  PARAMETER :: CoordType_Cartesian='Cartesian'
+    CHARACTER(LEN=7),  PARAMETER :: CoordType_FullTrf='FullTrf'
+    CHARACTER(LEN=9),  PARAMETER :: CoordType_DoNewChol='DoNewChol'
+    CHARACTER(LEN=9),  PARAMETER :: CoordType_DoClssTrf='DoClssTrf'
+    CHARACTER(LEN=7),  PARAMETER :: CoordType_DoFixMM='DoFixMM'
+!-------------------------------------------------
+!  Parsing keys for <Options.StpDescInvH=>
+!
+    CHARACTER(LEN=11),  PARAMETER :: STPDESCINVH='StpDescInvH'
+!-------------------------------------------------
+!  Parsing keys for <Options.VDWFACT=>
+!
+    CHARACTER(LEN=7),  PARAMETER :: VDWFACT='VDWFact'
+!-------------------------------------------------
+!  Parsing keys for <Options.Refresh=>
+!
+    CHARACTER(LEN=11),  PARAMETER :: INTCREFRESH='IntCRefresh'
+!---------------------------------------------------------
+END MODULE GeomOptKeys
