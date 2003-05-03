9fc9f0498064adeff612446ef173c96b7e651c69
mchalla <mchalla>
2003-05-03 16:54:18 +0000

    Added in levelshift capability to SP2.  Needs to be called twice ...

--- SCFeqs/SP2.F90
+++ SCFeqs/SP2.F90
@@ -27,7 +27,7 @@ PROGRAM DMP_SP2 ! Density matrix purification, SP2 variation
 ! Trace Setting SP2
 !-------------------------------------------------------------------------------
   TYPE(ARGMT)                    :: Args
-  REAL(DOUBLE)                   :: Ne
+  REAL(DOUBLE)                   :: Ne,Lambda
   INTEGER                        :: I,MM
   LOGICAL                        :: Present
   CHARACTER(LEN=DEFAULT_CHR_LEN) :: Mssg,FFile
@@ -69,6 +69,31 @@ PROGRAM DMP_SP2 ! Density matrix purification, SP2 variation
      CALL SP2(P,Tmp1,Tmp2,Ne,MM)
      IF(CnvrgChck(Prog,I,Ne,MM,F,P,POld,Tmp1,Tmp2))EXIT
   ENDDO
+  CALL OpenASCII(InpFile,Inp)         
+  ! If we are called without the DIIS Fockian, consider a levelshift
+  IF(.NOT.PRESENT.AND.OptDblQ(Inp,'LevelShift',Lambda))THEN
+     ! Get the Fock matrix back ... 
+     CALL Get(F,TrixFile('OrthoF',Args,0))   
+     ! Construct the virtual projector, Q=I-P
+     CALL SetEq(POld,P)
+     CALL Multiply(POld,-One)   
+     CALL Add(POld,One)     
+     ! The shifted Fockian is F[Lambda] = P.F + (1+Lambda)*Q.F
+     Lambda=One+ABS(Lambda)
+     CALL Multiply(P,F,Tmp1)
+     CALL Multiply(POld,F,Tmp2)
+     CALL Multiply(Tmp2,Lambda)
+     CALL Add(Tmp1,Tmp2,POld)
+     ! Put the Fock matrix back all tidy like
+     CALL Put(POld,TrixFile('OrthoF',Args,0))   
+     Lambda=ABS(Lambda)-One
+     Mssg=TRIM(ProcessName(Prog))//' LevlShift = '  &
+          //TRIM(DblToMedmChar(Lambda))
+     CALL OpenASCII(OutFile,Out)         
+     WRITE(Out,*)TRIM(Mssg)
+     CLOSE(Out)
+  ENDIF
+  CLOSE(Inp)
   ! Orthogonal put and xform to AO rep and put
   CALL PutXForm(Prog,Args,P,POld,Tmp1)
   ! Tidy up
