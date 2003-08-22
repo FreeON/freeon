66a87c0aa4a7b4d787cefbe899b5a2e80c6dfe04
vweber <vweber>
2003-08-22 05:37:20 +0000

    Added Geometry check for too close atom pairs.

--- MondoSCF2/ConflictDetect.F90
+++ MondoSCF2/ConflictDetect.F90
@@ -1,5 +1,7 @@
 MODULE Conflicted
   USE DerivedTypes
+  USE Parse
+  USE ProcessControl
   USE ControlStructures
   IMPLICIT NONE
 CONTAINS 
@@ -47,5 +49,82 @@ CONTAINS
        CALL MondoHalt(PRSE_ERROR,' Basis sets in sequence is short.')
   END SUBROUTINE ConflictCheck2
 #endif
+
+
+
+
+
+
+  SUBROUTINE GeoConflictCheck(G)
+!H---------------------------------------------------------------------------------
+!H SUBROUTINE GeoConflictCheck(G)
+!H  Main driver for geometry conflict.
+!H---------------------------------------------------------------------------------
+    TYPE(Geometries) :: G
+    !-------------------------------------------------------------------
+    !
+    CALL GeoConflictCheck1(G)
+    !CALL GeoConflictCheck2()
+    !CALL GeoConflictCheck3()
+    !CALL GeoConflictCheck4()
+    !
+  END SUBROUTINE GeoConflictCheck
+  !
+  !
+  SUBROUTINE GeoConflictCheck1(G)
+!H---------------------------------------------------------------------------------
+!H SUBROUTINE GeoConflictCheck1(G)
+!H  This routine checks if a pair of atoms are too close from each other.
+!H---------------------------------------------------------------------------------
+    TYPE(Geometries)               :: G
+    !-------------------------------------------------------------------
+    INTEGER                        :: i,iClone,AtA,AtB,MaxClone
+    INTEGER, DIMENSION(2)          :: NClone
+    REAL(DOUBLE)                   :: Ax,Ay,Az,Bx,By,Bz,Dist,t1,t2
+    CHARACTER(LEN=DEFAULT_CHR_LEN) :: Text
+    !-------------------------------------------------------------------
+    REAL(DOUBLE), PARAMETER        :: MinDist = 0.5D+00
+    !-------------------------------------------------------------------
+    !
+    !Check for clones.
+    IF(G%Clones.EQ.1) THEN
+       ! We have only clone.
+       MaxClone=1
+       NClone(1)=1
+       NClone(2)=1
+    ELSE
+       ! We have several clones, check pairs 
+       ! only for reactant and product.
+       MaxClone=2
+       NClone(1)=0             ! reactant.
+       NClone(2)=G%Clones+1    ! product.
+    ENDIF
+    !
+    DO i=1,MaxClone
+       iClone=NClone(i)
+       DO AtA=2,G%Clone(iClone)%NAtms
+          Ax=G%Clone(iClone)%AbCarts%D(1,AtA)
+          Ay=G%Clone(iClone)%AbCarts%D(2,AtA)
+          Az=G%Clone(iClone)%AbCarts%D(3,AtA)
+          DO AtB=1,AtA-1
+             Bx=G%Clone(iClone)%AbCarts%D(1,AtB)
+             By=G%Clone(iClone)%AbCarts%D(2,AtB)
+             Bz=G%Clone(iClone)%AbCarts%D(3,AtB)
+             Dist=SQRT((Ax-Bx)**2+(Ay-By)**2+(Az-Bz)**2)
+             !write(*,*) 'Dist',Dist,Ax,Ay,Az,Bx,By,Bz
+             IF(Dist.LE.MinDist) THEN
+                Text=' The distance between the atoms '// &
+                     & TRIM(IntToChar(AtB))//' '//TRIM(G%Clone(iClone)%AtNam%C(AtB))//' and '// &
+                     & TRIM(IntToChar(AtA))//' '//TRIM(G%Clone(iClone)%AtNam%C(AtA))// &
+                     & ' in the clone '//TRIM(IntToChar(iClone)) //' is '// &
+                     & TRIM(DblToShrtChar(Dist))//' !'
+                CALL MondoHalt(PRSE_ERROR,Text)
+             ENDIF
+          ENDDO
+       ENDDO
+    ENDDO
+    !
+  END SUBROUTINE GeoConflictCheck1
+
 
 END MODULE Conflicted
