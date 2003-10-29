c965d105e03ddf2079c7f4520f5292e854e2f1ed
vweber <vweber>
2003-10-29 17:33:25 +0000

    Added a struct.

--- TwoE/ShellPairStruct.F90
+++ TwoE/ShellPairStruct.F90
@@ -6,4 +6,13 @@ MODULE ShellPairStruct
      INTEGER :: L
      REAL(DOUBLE), DIMENSION(PairLngth,MaxSPairs) :: SP
   END TYPE ShellPair
+  !
+  TYPE AtomInfo
+     INTEGER      :: K1,K2,NCell
+     REAL(DOUBLE) :: Atm1X,Atm1Y,Atm1Z
+     REAL(DOUBLE) :: Atm2X,Atm2Y,Atm2Z
+     REAL(DOUBLE) :: R12
+     REAL(DOUBLE) :: Atm12X,Atm12Y,Atm12Z
+  END TYPE AtomInfo
+  !
 END MODULE ShellPairStruct
