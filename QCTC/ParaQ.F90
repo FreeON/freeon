37cf48b8ce212cdd9b18e6fc783bb14634357d13
ckgan <ckgan>
2004-03-18 23:21:49 +0000

    Module for parallel QCTC included.

---
+++ QCTC/ParaQ.F90
@@ -0,0 +1,15 @@
+MODULE ParallelQCTC
+  USE Globals
+  USE MondoMPI
+  USE DerivedTypes
+  USE GlobalScalars
+  USE GlobalObjects
+  USE ERIGlobals
+  USE InOut
+  USE PoleTree
+  IMPLICIT NONE
+
+#ifdef PARALLEL
+
+#endif
+END MODULE ParallelQCTC
