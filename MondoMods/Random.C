a5fa32b667d48e0ed25b74fa1df97169f03ac9bc
tymczak <tymczak>
2004-06-16 19:01:11 +0000

    Me and CK fixed this
    CJ

--- MondoMods/Random.C
+++ MondoMods/Random.C
@@ -1,7 +1,11 @@
 #include <stdlib.h>
-/*
-double random_(void){return rand(); }
-*/
-double random_(void){ 
-srand( (unsigned int) time( NULL ));
-return drand48(); }
+
+double random_(void) { 
+  static long int i=0;
+  if(i == 0) {
+    i = (long int) time( NULL );
+    srand48(i); 
+  }
+  return drand48();
+}
+
