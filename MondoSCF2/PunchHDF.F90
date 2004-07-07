3619b0183861a04d5fea9626bda691c87cc1b43c
tymczak <tymczak>
2004-07-07 23:07:19 +0000

    Fixed a problem with the Wrapping

--- MondoSCF2/PunchHDF.F90
+++ MondoSCF2/PunchHDF.F90
@@ -156,7 +156,7 @@ CONTAINS
     TYPE(BasisSets)    :: B
     TYPE(Geometries)   :: G    
     TYPE(CellSet)      :: CS_IN,CS_OUT
-    INTEGER            :: cBAS,cGEO,iCLONE,HDFFileID
+    INTEGER            :: cBAS,cGEO,iCLONE,HDFFileID,I
     CHARACTER(LEN=DCL) :: chGEO
 !---------------------------------------------------------------------------!
     chGEO=IntToChar(cGEO)
@@ -167,9 +167,10 @@ CONTAINS
        CALL SetLatticeVectors(G%Clone(iCLONE),B%AtomPairThresh(iCLONE,cBAS),CS_IN,CS_OUT)
 !
 !      Make sure everything is wrapped correctly
-       G%Clone(iCLONE)%Carts%D = G%Clone(iCLONE)%AbCarts%D
+       G%Clone(iCLONE)%Carts%D = G%Clone(iCLONE)%AbCarts%D      
        CALL CalFracCarts(G%Clone(iCLONE))
        CALL WrapAtoms(G%Clone(iCLONE))
+       G%Clone(iCLONE)%AbCarts%D = G%Clone(iCLONE)%Carts%D  
 !
        HDF_CurrentID=OpenHDFGroup(HDFFileID,"Clone #"//TRIM(IntToChar(iCLONE)))
 !      If we have ECPs, temporarily reset this geometries nuclear charges
