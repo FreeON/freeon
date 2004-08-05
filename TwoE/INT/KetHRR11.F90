a8dd5007d40392def9e0dd0c62cf5220e3fe1880
mchalla <mchalla>
2004-08-05 21:18:18 +0000

    *** empty log message ***

--- TwoE/INT/KetHRR11.F90
+++
@@ -1,9 +0,0 @@
-   SUBROUTINE KetHRR11(LB,HRR) 
-      USE DerivedTypes
-      USE VScratchB
-      USE GlobalScalars
-      IMPLICIT REAL(DOUBLE) (W)
-      INTEGER :: LB
-      REAL(DOUBLE) :: HRR(1:LB,1,1)
-      !=|1,1)
-END SUBROUTINE KetHRR11
