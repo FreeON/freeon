34aae5bba559e2a1642c83f3315233f7d01a238f
tymczak <tymczak>
2004-08-19 18:32:35 +0000

    Makes the Derivative Tensors Numerically, still a work in progress

--- QCTC/MakePFFT.F90
+++ QCTC/MakePFFT.F90
@@ -16,9 +16,12 @@ PROGRAM MakePFFT
   TYPE(TIME)                     :: TimePFFT
   TYPE(CRDS)                     :: GM
   TYPE(DBL_VECT)                 :: TenC,TenS
+  TYPE(DBL_RNK2)                 :: BoxShape
+  TYPE(DBL_RNK3)                 :: dTenC,dTenS
   TYPE(CellSet)                  :: CS
   TYPE(ARGMT)                    :: Args
-  INTEGER                        :: MaxEll
+  INTEGER                        :: MaxEll,I,J,K
+  REAL(DOUBLE)                   :: DDelta 
   CHARACTER(LEN=DEFAULT_CHR_LEN) :: Mssg
   CHARACTER(LEN=8),PARAMETER     :: Prog='MakePFFT'
 !-------------------------------------------------------------------------------- 
@@ -40,12 +43,60 @@ PROGRAM MakePFFT
 !    Put them to HDF
      CALL Put(TenC,'PFFTensorC')
      CALL Put(TenS,'PFFTensorS')
-!    Delete
-     CALL Delete(GM)
-     CALL Delete(Args)
-     CALL Delete(TenC)
-     CALL Delete(TenS)
+!
+!    Do the Derivaltive Arrays
+!
+!    Allocate the derivative tensors
+     MaxEll=GM%PBC%PFFMaxEll
+     CALL New(dTenC,(/LSP(2*MaxEll),3,3/),(/0,1,1/))
+     CALL New(dTenS,(/LSP(2*MaxEll),3,3/),(/0,1,1/))
+     CALL New(BoxShape,(/3,3/))
+     CALL New(CS,CS_IN%NCells)
+     DDelta = 1.D-8
+!    Initialize CS
+     BoxShape%D=GM%PBC%BoxShape%D
+     dTenC%D=Zero
+     dTenS%D=Zero
+     DO K=1,CS_IN%NCells
+        CS_IN%CellCarts%D(:,K) = AtomToFrac(GM,CS_IN%CellCarts%D(:,K))
+     ENDDO
+!    Calculate the tensors numerically
+     DO I=1,3
+        DO J=1,3
+           IF(GM%PBC%AutoW%I(I)==1 .AND. GM%PBC%AutoW%I(J)==1) THEN
+              GM%PBC%BoxShape%D(I,J) = BoxShape%D(I,J) + DDelta
+              GM%PBC%InvBoxSh%D      = InverseMatrix(GM%PBC%BoxShape%D)
+              DO K=1,CS_IN%NCells
+                 CS%CellCarts%D(:,K) = FracToAtom(GM,CS_IN%CellCarts%D(:,K))
+              ENDDO
+!
+              CALL CalculatePFFT(MaxEll,GM,Args,CS,TenC,TenS)
+              dTenC%D(:,I,J) = TenC%D(:)
+              dTenS%D(:,I,J) = TenS%D(:)
+!
+              GM%PBC%BoxShape%D(I,J) = BoxShape%D(I,J) - DDelta  
+              GM%PBC%InvBoxSh%D      = InverseMatrix(GM%PBC%BoxShape%D)
+              DO K=1,CS_IN%NCells
+                 CS%CellCarts%D(:,K) = FracToAtom(GM,CS_IN%CellCarts%D(:,K))
+              ENDDO
+!
+              CALL CalculatePFFT(MaxEll,GM,Args,CS,TenC,TenS)
+              dTenC%D(:,I,J) = (TenC%D(:)-dTenC%D(:,I,J))/(Two*DDelta)
+              dTenS%D(:,I,J) = (TenS%D(:)-dTenS%D(:,I,J))/(Two*DDelta)
+           ENDIF
+        ENDDO
+     ENDDO
+!    Put them to HDF
+     CALL Put(dTenC,'dPFFTensorC')
+     CALL Put(dTenS,'dPFFTensorS')
   ENDIF
+! Delete
+  CALL Delete(GM)
+  CALL Delete(Args)
+  CALL Delete(TenC)
+  CALL Delete(TenS)  
+  CALL Delete(dTenC)
+  CALL Delete(dTenS)
 ! didn't count flops, any accumulation is residual
 ! from matrix routines
   PerfMon%FLOP=Zero 
