682b6388f60c76e5db5a836954617eac89cd78c0
tymczak <tymczak>
2004-08-31 21:00:52 +0000

    Put in Numerical Lattice Forces for QCTC and ONX have not tested them
    Should be Slow, Can be turned on with the
    compiler Derective NLATTFORCE
    
    CJ

--- MondoSCF2/SCFs.F90
+++ MondoSCF2/SCFs.F90
@@ -869,13 +869,23 @@ CONTAINS
     ! Build density with last DM
     CALL Invoke('MakeRho',N,S,M)
     ! Coulomb part
+#ifdef NLATTFORCE
+    CALL Invoke('JForce',N,S,M)
+    CALL NLATTFORCE_J()
+    ! Exact Hartree-Fock exchange component
+    IF(HasHF(O%Models(cBas)))THEN
+       CALL Invoke('GONX',N,S,M)
+       CALL NLATTFORCE_X()
+    ENDIF
+#else
     CALL Invoke('JForce',N,S,M)
     ! Exact Hartree-Fock exchange component
     IF(HasHF(O%Models(cBas)))THEN
-    !   CALL NXForce(cBAS,cGEO,N,G,B,S,M)
+    !  CALL NXForce(cBAS,cGEO,N,G,B,S,M)
     !  CALL Invoke('XForce',N,S,M)
        CALL Invoke('GONX',N,S,M)
     ENDIF
+#endif
     ! DFT exchange corrleation term
     IF(HasDFT(O%Models(cBas))) THEN
        CALL Invoke('XCForce',N,S,M)
@@ -943,9 +953,212 @@ CONTAINS
     CALL CloseHDF(HDFFileID)
     CALL Delete(S%Action)
   END SUBROUTINE Force
-  !===============================================================================
-  ! Numerically compute gradients of the exact HF exchange
-  !===============================================================================
+!===============================================================================
+! Numerically compute Lattice Forces for J
+!===============================================================================
+  SUBROUTINE NLATTFORCE_J(cBAS,cGEO,N,G,B,S,M)
+    TYPE(FileNames)    :: N
+    TYPE(Options)      :: O
+    TYPE(State)        :: S
+    TYPE(Geometries)   :: G
+    TYPE(BasisSets)    :: B
+    TYPE(Parallel)     :: M
+    INTEGER            :: cBAS,cGEO,iCLONE,I,J,II
+    CHARACTER(LEN=DCL) :: chGEO,chBAS,chSCF
+    REAL(DOUBLE)       :: DDelta,Lat00,E1,E2
+    CHARACTER(LEN=DCL) :: TrixName
+#ifdef PARRALEL
+    TYPE(DBCSR)        :: P,J1,J2,J3
+#else
+    TYPE(BCSR)         :: P,J1,J2,J3
+#endif
+!   
+    DDelta = 1.D-3
+    DO iCLONE=1,G%Clones
+       chGEO=IntToChar(cGEO)
+       chBAS=IntToChar(cBAS)
+       chSCF=IntToChar(S%Current%I(1)+1)
+       CALL New(BSiz,G%Clone(iCLONE)%NAtms)
+       CALL New(OffS,G%Clone(iCLONE)%NAtms)
+!
+       HDFFileID=OpenHDF(N%HFile)
+       HDF_CurrentID=OpenHDFGroup(HDFFileID,"Clone #"//TRIM(IntToChar(iCLONE)))
+       CALL Get(G%Clone(iCLONE)%PBC%LatFrc,'latfrc',Tag_O=chGEO)
+       CALL CloseHDFGroup(HDF_CurrentID)
+       CALL CloseHDF(HDFFileID)
+!
+       NAToms=G%Clone(iCLONE)%NAtms
+       MaxAtms=B%MxAts(cBAS)
+       MaxBlks=B%MxN0s(cBAS)
+       MaxNon0=B%MxBlk(cBAS)
+       NBasF=B%BSets(iCLONE,cBAS)%NBasF
+       BSiz%I=B%BSiz(iCLONE,cBAS)%I
+       OffS%I=B%OffS(iCLONE,cBAS)%I
+       MaxBlkSize=0
+       DO II=1,G%Clone(iCLONE)%NAtms; MaxBlkSize=MAX(MaxBlkSize,BSiz%I(II)); ENDDO
+!
+       CALL New(P)
+       CALL New(J1)
+       CALL New(J2)
+       CALL New(J3)
+!
+       TrixName=TRIM(N%M_SCRATCH)//TRIM(N%SCF_NAME)//'_Geom#'//TRIM(chGEO)//'_Base#'//TRIM(chBAS) &
+                //'_Cycl#'//TRIM(chSCF)//'_Clone#'//TRIM(IntToChar(iCLONE))//'.D'
+       CALL Get(P,TrixName)
+       chSCF=IntToChar(S%Current%I(1))
+!
+       DO I=1,3
+          DO J=1,3
+             Lat00 = G%Clone(iCLONE)%PBC%BoxShape%D(I,J)
+!
+             G%Clone(iCLONE)%PBC%BoxShape%D(I,J) =  Lat00+DDelta
+             CALL GeomArchive(cBAS,cGEO,N,B,G)
+             CALL Invoke('MakePFFT',N,S,M)
+             CALL Invoke('MakeRho' ,N,S,M)
+             CALL Invoke('QCTC'    ,N,S,M)  
+             TrixName=TRIM(N%M_SCRATCH)//TRIM(N%SCF_NAME)//'_Geom#'//TRIM(chGEO)//'_Base#'//TRIM(chBAS) &
+                //'_Cycl#'//TRIM(chSCF)//'_Clone#'//TRIM(IntToChar(iCLONE))//'.J'
+             CALL Get(J1,TrixName)
+             CALL Get(E1,'E_NuclearTotal')
+!
+             G%Clone(iCLONE)%PBC%BoxShape%D(I,J) =  Lat00-DDelta
+             CALL GeomArchive(cBAS,cGEO,N,B,G)
+             CALL Invoke('MakePFFT',N,S,M)
+             CALL Invoke('MakeRho' ,N,S,M)
+             CALL Invoke('QCTC',N,S,M)  
+             TrixName=TRIM(N%M_SCRATCH)//TRIM(N%SCF_NAME)//'_Geom#'//TRIM(chGEO)//'_Base#'//TRIM(chBAS) &
+                //'_Cycl#'//TRIM(chSCF)//'_Clone#'//TRIM(IntToChar(iCLONE))//'.J'
+             CALL Get(J2,TrixName)
+             CALL Get(E2,'E_NuclearTotal')
+!
+             CALL Multiply(J2,-One)
+             CALL Add(J1,J2,J3)
+             CALL Multiply(P,J3,J1)
+             G%Clone(iCLONE)%PBC%LatFrc%D(I,J) = G%Clone(iCLONE)%PBC%LatFrc%D(I,J) &
+                                                 + Trace(J1)/(Two*DDelta) + (E1-E2)/(Two*DDelta) 
+!
+             G%Clone(iCLONE)%PBC%BoxShape%D(I,J) =  Lat00
+!  
+          ENDDO
+       ENDDO
+!
+       HDFFileID=OpenHDF(N%HFile)
+       HDF_CurrentID=OpenHDFGroup(HDFFileID,"Clone #"//TRIM(IntToChar(iCLONE)))
+       CALL Put(G%Clone(iCLONE)%PBC%LatFrc,'latfrc',Tag_O=chGEO)
+       CALL CloseHDFGroup(HDF_CurrentID)
+       CALL CloseHDF(HDFFileID)
+!
+       CALL Delete(BSiz)
+       CALL Delete(OffS)
+       CALL Delete(P)
+       CALL Delete(J1)
+       CALL Delete(J2)
+       CALL Delete(J3)
+!
+    ENDDO
+!
+  END SUBROUTINE NLATTFORCE_J
+!===============================================================================
+! Numerically compute Lattice Forces for J
+!===============================================================================
+  SUBROUTINE NLATTFORCE_X(cBAS,cGEO,N,G,B,S,M)
+    TYPE(FileNames)    :: N
+    TYPE(Options)      :: O
+    TYPE(State)        :: S
+    TYPE(Geometries)   :: G
+    TYPE(BasisSets)    :: B
+    TYPE(Parallel)     :: M
+    INTEGER            :: cBAS,cGEO,iCLONE,I,J,II
+    CHARACTER(LEN=DCL) :: chGEO,chBAS,chSCF
+    REAL(DOUBLE)       :: DDelta,Lat00
+    CHARACTER(LEN=DCL) :: TrixName
+#ifdef PARRALEL
+    TYPE(DBCSR)        :: P,K1,K2,K3
+#else
+    TYPE(BCSR)         :: P,K1,K2,K3
+#endif
+!   
+    DDelta = 1.D-3
+    DO iCLONE=1,G%Clones
+       chGEO=IntToChar(cGEO)
+       chBAS=IntToChar(cBAS)
+       chSCF=IntToChar(S%Current%I(1)+1)
+       CALL New(BSiz,G%Clone(iCLONE)%NAtms)
+       CALL New(OffS,G%Clone(iCLONE)%NAtms)
+!
+       HDFFileID=OpenHDF(N%HFile)
+       HDF_CurrentID=OpenHDFGroup(HDFFileID,"Clone #"//TRIM(IntToChar(iCLONE)))
+       CALL Get(G%Clone(iCLONE)%PBC%LatFrc,'latfrc',Tag_O=chGEO)
+       CALL CloseHDFGroup(HDF_CurrentID)
+       CALL CloseHDF(HDFFileID)
+!
+       NAToms=G%Clone(iCLONE)%NAtms
+       MaxAtms=B%MxAts(cBAS)
+       MaxBlks=B%MxN0s(cBAS)
+       MaxNon0=B%MxBlk(cBAS)
+       NBasF=B%BSets(iCLONE,cBAS)%NBasF
+       BSiz%I=B%BSiz(iCLONE,cBAS)%I
+       OffS%I=B%OffS(iCLONE,cBAS)%I
+       MaxBlkSize=0
+       DO II=1,G%Clone(iCLONE)%NAtms; MaxBlkSize=MAX(MaxBlkSize,BSiz%I(II)); ENDDO
+!
+       CALL New(P)
+       CALL New(K1)
+       CALL New(K2)
+       CALL New(K3)
+!
+       TrixName=TRIM(N%M_SCRATCH)//TRIM(N%SCF_NAME)//'_Geom#'//TRIM(chGEO)//'_Base#'//TRIM(chBAS) &
+                //'_Cycl#'//TRIM(chSCF)//'_Clone#'//TRIM(IntToChar(iCLONE))//'.D'
+       CALL Get(P,TrixName)
+       chSCF=IntToChar(S%Current%I(1))
+!
+       DO I=1,3
+          DO J=1,3
+             Lat00 = G%Clone(iCLONE)%PBC%BoxShape%D(I,J)
+!
+             G%Clone(iCLONE)%PBC%BoxShape%D(I,J) =  Lat00+DDelta
+             CALL GeomArchive(cBAS,cGEO,N,B,G)
+             CALL Invoke('ONX'    ,N,S,M)  
+             TrixName=TRIM(N%M_SCRATCH)//TRIM(N%SCF_NAME)//'_Geom#'//TRIM(chGEO)//'_Base#'//TRIM(chBAS) &
+                //'_Cycl#'//TRIM(chSCF)//'_Clone#'//TRIM(IntToChar(iCLONE))//'.K'
+             CALL Get(K1,TrixName)
+!
+             G%Clone(iCLONE)%PBC%BoxShape%D(I,J) =  Lat00-DDelta
+             CALL GeomArchive(cBAS,cGEO,N,B,G)
+             CALL Invoke('ONX',N,S,M)  
+             TrixName=TRIM(N%M_SCRATCH)//TRIM(N%SCF_NAME)//'_Geom#'//TRIM(chGEO)//'_Base#'//TRIM(chBAS) &
+                //'_Cycl#'//TRIM(chSCF)//'_Clone#'//TRIM(IntToChar(iCLONE))//'.K'
+             CALL Get(K2,TrixName)
+!
+             CALL Multiply(K2,-One)
+             CALL Add(K1,K2,K3)
+             CALL Multiply(P,K3,K1)
+             G%Clone(iCLONE)%PBC%LatFrc%D(I,J) = G%Clone(iCLONE)%PBC%LatFrc%D(I,J) +  Trace(K1)/(Two*DDelta)
+!
+             G%Clone(iCLONE)%PBC%BoxShape%D(I,J) =  Lat00
+!  
+          ENDDO
+       ENDDO
+!
+       HDFFileID=OpenHDF(N%HFile)
+       HDF_CurrentID=OpenHDFGroup(HDFFileID,"Clone #"//TRIM(IntToChar(iCLONE)))
+       CALL Put(G%Clone(iCLONE)%PBC%LatFrc,'latfrc',Tag_O=chGEO)
+       CALL CloseHDFGroup(HDF_CurrentID)
+       CALL CloseHDF(HDFFileID)
+!
+       CALL Delete(BSiz)
+       CALL Delete(OffS)
+       CALL Delete(P)
+       CALL Delete(K1)
+       CALL Delete(K2)
+       CALL Delete(K3)
+!
+    ENDDO
+!
+  END SUBROUTINE NLATTFORCE_X
+!===============================================================================
+! Numerically compute gradients of the exact HF exchange
+!===============================================================================
   SUBROUTINE NXForce(cBAS,cGEO,N,G,B,S,M)
     TYPE(FileNames)  :: N
     TYPE(Options)    :: O
