273dfbb0f794088a9fc45f3e819b660cb64454f8
nemeth <nemeth>
2004-09-01 00:50:35 +0000

    Extra printing set back.

--- MondoSCF2/InCoords.F90
+++ MondoSCF2/InCoords.F90
@@ -1676,10 +1676,10 @@ CONTAINS
        RMSD=1.D+9
        !
        DO IStep=1,GBackTrf%MaxIt_CooTrf
-         IF(PRESENT(iGEO_O)) THEN
-           CALL PrtBackTrf(AtNum,ActCarts%D,PBCDim,PWDPath, &
-                           IRep,IStep,iGEO_O)
-         ENDIF
+        !IF(PRESENT(iGEO_O)) THEN
+        !  CALL PrtBackTrf(AtNum,ActCarts%D,PBCDim,PWDPath, &
+        !                  IRep,IStep,iGEO_O)
+        !ENDIF
          !
          ! Get B and refresh values of internal coords
          !
