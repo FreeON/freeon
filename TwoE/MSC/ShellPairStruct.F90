540d401372cd8b8dfe2baf8d7c89640ce3f05022
vweber <vweber>
2004-09-27 13:30:15 +0000

    Added bra-ket symmetry in the interface. Added a switch in the shellpairs.

--- TwoE/MSC/ShellPairStruct.F90
+++ TwoE/MSC/ShellPairStruct.F90
@@ -8,19 +8,10 @@ MODULE ShellPairStruct
      REAL(DOUBLE) :: Atm2X,Atm2Y,Atm2Z
   END TYPE SmallAtomInfo
   !
-!!$  TYPE ShellPair
-!!$     INTEGER :: IntType
-!!$     INTEGER :: L
-!!$     TYPE(SmallAtomInfo) :: AtmInfo
-!!$     REAL(DOUBLE), DIMENSION(5,3000) :: Cst
-!!$     !REAL(DOUBLE), DIMENSION(:,:), POINTER :: Cst
-!!$     !REAL(DOUBLE), DIMENSION(:,:), ALLOCATABLE :: Cst
-!!$     !REAL(DOUBLE), DIMENSION(...) :: Cst
-!!$  END TYPE ShellPair
-  !
   TYPE ShellPairG
      INTEGER :: IntType
      INTEGER :: L
+     LOGICAL :: Switch
      TYPE(SmallAtomInfo) :: AtmInfo
      REAL(DOUBLE), DIMENSION(10,MaxSPairs) :: Cst
   END TYPE ShellPairG
@@ -36,6 +27,7 @@ MODULE ShellPairStruct
   TYPE ShellPair
      INTEGER :: IntType
      INTEGER :: L
+     LOGICAL :: Switch
      TYPE(SmallAtomInfo) :: AtmInfo
      REAL(DOUBLE), DIMENSION(10,100) :: Cst
   END TYPE ShellPair
