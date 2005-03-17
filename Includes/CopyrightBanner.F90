b73827dcd7aff5de12185c726fbc387975cbe029
mchalla <mchalla>
2005-03-17 22:52:49 +0000

    The NEW GNU COPYLEFT!!

--- Includes/CopyrightBanner.F90
+++ Includes/CopyrightBanner.F90
@@ -1,27 +1,25 @@
 !------------------------------------------------------------------------------
-!--  This code is part of the MondoSCF suite of programs for linear scaling 
+!    This code is part of the MondoSCF suite of programs for linear scaling 
 !    electronic structure theory and ab initio molecular dynamics.
 !
-!--  Copyright (c) 2001, the Regents of the University of California.  
-!    This SOFTWARE has been authored by an employee or employees of the 
-!    University of California, operator of the Los Alamos National Laboratory 
-!    under Contract No. W-7405-ENG-36 with the U.S. Department of Energy.  
-!    The U.S. Government has rights to use, reproduce, and distribute this 
-!    SOFTWARE.  The public may copy, distribute, prepare derivative works 
-!    and publicly display this SOFTWARE without charge, provided that this 
-!    Notice and any statement of authorship are reproduced on all copies.  
-!    Neither the Government nor the University makes any warranty, express 
-!    or implied, or assumes any liability or responsibility for the use of 
-!    this SOFTWARE.  If SOFTWARE is modified to produce derivative works, 
-!    such modified SOFTWARE should be clearly marked, so as not to confuse 
-!    it with the version available from LANL.  The return of derivative works
-!    to the primary author for integration and general release is encouraged. 
-!    The first publication realized with the use of MondoSCF shall be
-!    considered a joint work.  Publication of the results will appear
-!    under the joint authorship of the researchers nominated by their
-!    respective institutions. In future publications of work performed
-!    with MondoSCF, the use of the software shall be properly acknowledged,
-!    e.g. in the form <<These calculations have been performed using MondoSCF, 
-!    a suite of programs for linear scaling electronic structure theory and
-!    ab initio molecular dynamics>>, and given appropriate citation.  
+!    Copyright (2004). The Regents of the University of California. This 
+!    material was produced under U.S. Government contract W-7405-ENG-36 
+!    for Los Alamos National Laboratory, which is operated by the University 
+!    of California for the U.S. Department of Energy. The U.S. Government has 
+!    rights to use, reproduce, and distribute this software.  NEITHER THE 
+!    GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, 
+!    OR ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  
+!
+!    This program is free software; you can redistribute it and/or modify 
+!    it under the terms of the GNU General Public License as published by the 
+!    Free Software Foundation; either version 2 of the License, or (at your 
+!    option) any later version. Accordingly, this program is distributed in 
+!    the hope that it will be useful, but WITHOUT ANY WARRANTY; without even 
+!    the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+!    PURPOSE. See the GNU General Public License at www.gnu.org for details.
+!
+!    While you may do as you like with this software, the GNU license requires
+!    that you clearly mark derivative software.  In addition, you are encouraged 
+!    to return derivative works to the MondoSCF group for review, and possible 
+!    disemination in future releases.
 !------------------------------------------------------------------------------
