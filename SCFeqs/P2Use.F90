e320b8daecef874a37097619478db24cdf09ebb4
mchalla <mchalla>
2005-04-22 23:14:58 +0000

    Not quite stable yet.  BUT, looks very close.

--- SCFeqs/P2Use.F90
+++ SCFeqs/P2Use.F90
@@ -38,7 +38,7 @@ PROGRAM P2Use
   INTEGER                       :: I,J,JP,AtA,Q,R,T,KA,NBFA,NPur,PcntPNon0,Qstep, & 
                                    OldFileID,ICart,N,NStep,iGEO,DMPOrder
   CHARACTER(LEN=2)              :: Cycl
-  LOGICAL                       :: Present,DoingMD,ConvergeAOSP,ConvergeAll,AOSPExit
+  LOGICAL                       :: Present,DoingMD
   CHARACTER(LEN=DEFAULT_CHR_LEN):: Mssg,BName
   CHARACTER(LEN=5),PARAMETER    :: Prog='P2Use'
   !------------------------------------------------------------------------------- 
@@ -293,29 +293,22 @@ PROGRAM P2Use
 #ifdef PARALLEL
      CALL Multiply(P0,S0,Tmp1)
      TError0 = ABS(Trace(Tmp1)-Half*DBLE(NEl))
-     IF(MyID.EQ.ROOT) THEN 
-        IF(PrintFlags%Key==DEBUG_MAXIMUM) WRITE(*,*) "Trace Error: Tr[P0,S0] = ",TError0
-     ENDIF
+!     IF(MyID.EQ.ROOT)WRITE(*,*) "Trace Error: Tr[P0,S0] = ",TError0
      CALL Multiply(P0,S1,Tmp1)
      TError0 = ABS(Trace(Tmp1)-Half*DBLE(NEl))
-     IF(MyID.EQ.ROOT)THEN 
-        IF(PrintFlags%Key==DEBUG_MAXIMUM) WRITE(*,*) "Trace Error: Tr[P0,S1] = ",TError0 
-     ENDIF
+!     IF(MyID.EQ.ROOT)WRITE(*,*) "Trace Error: Tr[P0,S1] = ",TError0 
 #else
      TError0 = ABS(Trace(P0,S1)-Half*DBLE(NEl))
-     IF(PrintFlags%Key==DEBUG_MAXIMUM) THEN
-        WRITE(*,*) "Trace Error: Tr[P0,S0] = ",ABS(Trace(P0,S0)-Half*DBLE(NEl))
-        WRITE(*,*) "Trace Error: Tr[P0,S1] = ",ABS(Trace(P0,S1)-Half*DBLE(NEl))
-     ENDIF
+!     WRITE(*,*) "Trace Error: Tr[P0,S0] = ",ABS(Trace(P0,S0)-Half*DBLE(NEl))
+!     WRITE(*,*) "Trace Error: Tr[P0,S1] = ",ABS(Trace(P0,S1)-Half*DBLE(NEl))
 #endif
 !    Initialize
-     NStep       = 0
-     Lam         = Zero
-     DLam        = One
-     ConvergeAll = .FALSE.
+     NStep = 0
+     Lam   = Zero
+     DLam  = One
 !    Purify 
      DO 
-        NStep = NStep + 1 
+        NStep = NStep + 1
         Lam  = Lam + DLam
 !       Set Up S and P        
         CALL SetEq(Tmp1,S0)
@@ -333,10 +326,8 @@ PROGRAM P2Use
                 //' Lambda = '//TRIM(DblToMedmChar(Lam))      
            CALL OpenASCII(OutFile,Out)
            CALL PrintProtectL(Out)
-           IF(PrintFlags%Key==DEBUG_MAXIMUM) THEN
-              WRITE(*,*)TRIM(Mssg)
-              WRITE(Out,*)TRIM(Mssg)
-           ENDIF
+!           WRITE(*,*)TRIM(Mssg)
+!           WRITE(Out,*)TRIM(Mssg)
            CALL PrintProtectR(Out)
            CLOSE(UNIT=Out,STATUS='KEEP')
 #ifdef PARALLEL          
@@ -352,9 +343,7 @@ PROGRAM P2Use
         Norm_Error = TrP-Half*DBLE(NEl)
         Ipot_Error = One
 !
-        Qstep        = 0
-        ConvergeAOSP = .FALSE.
-        AOSPExit     = .FALSE.
+        Qstep=0
         DO I=1,20
            IF((ABS(Ipot_Error) > 0.5D0 .OR. ABS(Norm_Error) > 0.5D0) .AND. I > 1) THEN
               IF(Norm_Error > Zero) THEN
@@ -378,40 +367,31 @@ PROGRAM P2Use
            IF(MyId==ROOT)THEN
 #endif
               PNon0s=100.D0*DBLE(P%NNon0)/DBLE(NBasF*NBasF)
-              Mssg=ProcessName(Prog,'AO-DMX '//TRIM(IntToChar(NStep))//":"//TRIM(IntToChar(I))) &
+              Mssg=ProcessName(Prog,'AO-DMX '//TRIM(IntToChar(I))) &
                    //' dN='//TRIM(DblToShrtChar(Norm_Error))       &
                    //', %Non0='//TRIM(DblToShrtChar(PNon0s))              
               CALL OpenASCII(OutFile,Out)
               CALL PrintProtectL(Out)
-              IF(PrintFlags%Key==DEBUG_MAXIMUM) THEN
-                 WRITE(*,*)TRIM(Mssg)
-                 WRITE(Out,*)TRIM(Mssg)
-              ENDIF
+!              WRITE(*,*)TRIM(Mssg)
+!              WRITE(Out,*)TRIM(Mssg)
               CALL PrintProtectR(Out)
               CLOSE(UNIT=Out,STATUS='KEEP')
 #ifdef PARALLEL          
            ENDIF
 #endif
-!          Logic
-           IF(ABS(Ipot_Error) < 1.0D-10 .AND. ABS(Norm_Error) < 1.0D-10) THEN
-              AOSPExit     = .TRUE.
-              ConvergeAOSP = .TRUE.
-              IF(Lam > (One-1.D-14)) ConvergeAll = .TRUE.
-           ELSEIF(Qstep > 5) THEN
-              AOSPExit     = .TRUE.
-              IF(ABS(Ipot_Error) < 1.0D-4 .AND. ABS(Norm_Error) < 1.0D-4) ConvergeAOSP = .TRUE.
-              IF(Lam > (One-1.D-14)) ConvergeAll = .TRUE.
-           ELSEIF(ABS(Norm_Error) > 100.D0*TError0) THEN
-              AOSPExit     = .TRUE.
-           ENDIF
-           IF(AOSPExit) EXIT
+           IF(ABS(Ipot_Error) < 1.0D-10 .AND. ABS(Norm_Error) < 1.0D-10) EXIT
+           IF(ABS(Norm_Error) > 100.D0*TError0) EXIT
+           IF(Qstep > 4) EXIT
         ENDDO
-!       Logic
-        IF(ConvergeAll) THEN
-           CALL SetEq(P0,P)
-           EXIT
+!
+        IF(ABS(Norm_Error) < 0.01D0) THEN
+           IF(Lam > (One-1.D-14)) THEN
+              EXIT
+           ELSE
+              CALL SetEq(P0,P)
+           ENDIF
         ELSE
-           IF(DLam < 1.D-2 .OR. NStep > 50) THEN
+           IF(NStep > 50) THEN
               CALL Halt('P2Use:Density Matrix Extrapolater Failed to converge in'//TRIM(IntToChar(NStep))//' steps')
            ENDIF
            Lam  = Lam - DLam
