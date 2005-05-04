33b8d72c4e12564064d50dd786f176f7c25adac0
tymczak <tymczak>
2005-05-04 17:25:48 +0000

    removed the NewPAC compiler derective do not need it here
    CJ

--- QCTC/LibQCTC/PoleNodeType.F90
+++ QCTC/LibQCTC/PoleNodeType.F90
@@ -11,17 +11,14 @@ MODULE PoleNodeType
      INTEGER                               :: Edex     ! ENDign index of ORB list for this node
      INTEGER                               :: NQ       ! Number of centers
      INTEGER                               :: Ell      ! Ell type
-     INTEGER                               :: MaxDELL  ! Maximium Ell of the Distributuions in the box
+     INTEGER                               :: EllCD    ! Maximium Ell of the Distributuions in the box
      REAL(DOUBLE)                          :: Zeta     ! Minimum exponent in this node
      REAL(DOUBLE)                          :: Strength ! Strength of the Pole
      REAL(DOUBLE)                          :: DMax2    ! (Max distance)^2 from node center to dist
+     REAL(DOUBLE)                          :: WCoef    ! Weight for the Gaussian in the Box 
      TYPE(BBox)                            :: Box      ! Bounding Box of distribution (for PAC)
      TYPE(PoleNode),POINTER                :: Descend  ! Next node in tree descent
      TYPE(PoleNode),POINTER                :: Travrse  ! Next node in tree traversal
-!
-     INTEGER                               :: EllCD    ! Maximium Ell of the Distributuions in the box
-     REAL(DOUBLE)                          :: WCoef    ! Weight for the Gaussian in the Box 
-!
 #ifdef POINTERS_IN_DERIVED_TYPES
      REAL(DOUBLE),DIMENSION(:),POINTER     :: S        ! Im component of the multipole tensor
      REAL(DOUBLE),DIMENSION(:),POINTER     :: C        ! Re component of the multipole tensor
