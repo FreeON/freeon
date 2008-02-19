16a12ffe94e18182c4cb2f7be15c5f556a27e4d7
Nicolas Bock <nbock@lanl.gov>
2008-02-19 11:16:19 -0700

    Changed the default string length from 128 to 256 characters.
    If Mondo is run with longer path names, it can happen that the full path of
    files does not fit into 128 characters.

--- MondoSCF/ParseCommands.f90
+++ MondoSCF/ParseCommands.f90
@@ -52,39 +52,48 @@ CONTAINS
     ! Get environmental variables
     CALL GetEnv('PWD',N%M_PWD)
     CALL GetEnv('MONDO_HOME',N%M_HOME)
-    if(len(trim(N%M_HOME)) == 0) then
+    IF(len(trim(N%M_HOME)) == 0) THEN
       N%M_HOME = HAVE_MONDO_HOME
-      write(*,*) 'env variable $(MONDO_HOME) not set. Using '//trim(N%M_HOME)
-    endif
+      WRITE(*,"(A)") 'env variable $(MONDO_HOME) not set. Using '//trim(N%M_HOME)
+    ENDIF
     CALL GetEnv('MONDO_EXEC',N%M_EXEC)
-    if(LEN(TRIM(N%M_EXEC)) == 0) then
+    IF(LEN(TRIM(N%M_EXEC)) == 0) THEN
       N%M_EXEC = HAVE_MONDO_HOME//"/bin"
-      write(*,*) 'env variable $(MONDO_EXEC) not set. Using '//trim(N%M_EXEC)
-    endif
+      WRITE(*,"(A)") 'env variable $(MONDO_EXEC) not set. Using '//trim(N%M_EXEC)
+    ENDIF
     CALL GetEnv('MONDO_SCRATCH',N%M_SCRATCH)
-    IF(LEN(TRIM(N%M_SCRATCH)) == 0) then
-      write(*,*) 'env variable $(MONDO_SCRATCH) not set. Using /tmp'
+    IF(LEN(TRIM(N%M_SCRATCH)) == 0) THEN
+      WRITE(*,"(A)") 'env variable $(MONDO_SCRATCH) not set. Using /tmp'
       N%M_SCRATCH = "/tmp/"
       MONDO_SCRATCH = "/tmp/"
-    endif
+    ENDIF
+
     ! Set path names etc
     N%M_PWD=TRIM(N%M_PWD)//'/'
     N%M_HOME=TRIM(N%M_HOME)//'/'
     N%M_SCRATCH=TRIM(N%M_SCRATCH)//'/'
+
     ! Here is the scf name, tagged with the PID
     PROCESS_ID=IntToChar(GetPID())
     DotDex=INDEX(Args%C%C(1),'.')
-    IF(DotDex==0) &
-         CALL MondoHalt(PRSE_ERROR,' No "." in inut file name = <'//TRIM(N%IFile)//'>')
+    IF(DotDex==0) THEN
+      WRITE(*,"(A)") 'Parse error: no "." in input file name = <'//TRIM(N%IFile)//'>'
+      STOP "Termination of MondoSCF"
+    ENDIF
     N%SCF_NAME=Args%C%C(1)(1:DotDex-1)//'_'//TRIM(PROCESS_ID)
     PWDName=TRIM(N%M_PWD)//TRIM(N%SCF_NAME)
     SCRName=TRIM(N%M_SCRATCH)//TRIM(N%SCF_NAME)
+
     ! Input file with full path
     N%IFile=TRIM(N%M_PWD)//TRIM(Args%C%C(1))
+
     ! Check to see that the input file exists
     INQUIRE(FILE=N%IFile,EXIST=Exists)
-    IF(.NOT.Exists) &
-         CALL MondoHalt(PRSE_ERROR,' Input file: '//TRIM(N%IFile)//' does not exist! ')
+    IF(.NOT.Exists) THEN
+      WRITE(*,"(A)") 'Parse error: Input file "'//TRIM(N%IFile)//'" does not exist!'
+      STOP "Termination of MondoSCF"
+    ENDIF
+
     ! Determine if input file has a '.' in it.  If so, create SCF name from string up to the '.'
     DotDex=INDEX(Args%C%C(1),'.')
     IF(DotDex==0)THEN
@@ -92,6 +101,7 @@ CONTAINS
     ELSE
       N%SCF_NAME=Args%C%C(1)(1:DotDex-1)//'_'//TRIM(PROCESS_ID)
     ENDIF
+
     ! Out file with full path (demand an output file)
     ! Check to see that the output file does not exist (no overwrites allowed)
     !    INQUIRE(FILE=N%OFile,EXIST=Exists)
@@ -116,9 +126,10 @@ CONTAINS
       N%GFile=TRIM(N%M_PWD)//TRIM(Args%C%C(4))
     ENDIF
     N%HFile=TRIM(SCRName)//InfF
+
     ! If restart file set, it is set in ParseOptions
     N%RFile=""
-    !
+
 #if FULL_ON_FRONT_END_DEBUG
     WRITE(*,*)' N%IFile = '//TRIM(N%IFile)
     WRITE(*,*)' N%OFile = '//TRIM(N%OFile)
@@ -127,9 +138,9 @@ CONTAINS
     WRITE(*,*)' N%RFile = '//TRIM(N%RFile)
     WRITE(*,*)' N%GFile = '//TRIM(N%GFile)
 #else
-    WRITE(*,*)' InputFile  = '//TRIM(N%IFile)
-    WRITE(*,*)' OutputFile = '//TRIM(N%OFile)
-    WRITE(*,*)' LogFile    = '//TRIM(N%LFile)
+    WRITE(*,"(A)") 'InputFile  = '//TRIM(N%IFile)
+    WRITE(*,"(A)") 'OutputFile = '//TRIM(N%OFile)
+    WRITE(*,"(A)") 'LogFile    = '//TRIM(N%LFile)
 #endif
   END SUBROUTINE LoadCommands
 END MODULE ParseCommands
