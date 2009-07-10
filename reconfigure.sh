1e73aff9b5ce68a54f3b015e3a4c3ced90aa9973
Nicolas Bock <nicolasbock@gmail.com>
2009-07-10 10:45:36 -0600

    Cleaning up hdf5-1.8.3 sources so we can configure them.
    To make the dist target work in hdf5-1.8.3 I modified the sources a little bit
    so we can configure them ourselves and create proper Makefiles.

--- reconfigure.sh
+++ reconfigure.sh
@@ -2,4 +2,4 @@
 #
 # Use this script to reconfigure the sources after checking them out from git.
 
-autoreconf --no-recursive --install --symlink . LinearAlgebraLibrary
+autoreconf --no-recursive --install --symlink . LinearAlgebraLibrary MondoMods/hdf5-1.8.3
