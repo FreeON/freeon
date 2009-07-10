9c3b6012598ae22a8e8f3f025016894e87f9fd2d
Nicolas Bock <nicolasbock@gmail.com>
2009-07-09 21:17:52 -0600

    Another small fix.

--- reconfigure.sh
+++ reconfigure.sh
@@ -2,4 +2,4 @@
 #
 # Use this script to reconfigure the sources after checking them out from git.
 
-autoreconf --no-recursive --install --symlink .
+autoreconf --no-recursive --install --symlink . LinearAlgebraLibrary
