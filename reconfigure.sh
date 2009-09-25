8e13b0b34c0d09bbbfb3f130d451057227f716c6
Nicolas Bock <nicolasbock@gmail.com>
2009-09-25 09:10:02 -0600

    Some minor fixes.

--- reconfigure.sh
+++ reconfigure.sh
@@ -2,4 +2,4 @@
 #
 # Use this script to reconfigure the sources after checking them out from git.
 
-autoreconf --no-recursive --install --symlink . LinearAlgebraLibrary MondoMods/hdf5-1.8.3
+autoreconf --no-recursive --install --symlink --warnings=no-portability . LinearAlgebraLibrary MondoMods/hdf5-1.8.3
