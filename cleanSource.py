ebcb12824f72e5ff7664b8ba1c746454d8ae17e3
Nicolas Bock <nicolasbock@gmail.com>
2009-09-25 09:10:02 -0600

    Some minor fixes.

--- cleanSource.py
+++ cleanSource.py
@@ -4,13 +4,23 @@
 
 import optparse, sys, re, tempfile
 
-parser = optparse.OptionParser()
+parser = optparse.OptionParser(description = """This script cleans a source
+file. It does (1) remove trailing white space characters, and (2) replace a
+TAB character with space characters in cases where the TAB character follows a
+leading space character.""")
 
 parser.add_option("--pretend",
     help = "Just check, but not change any files",
     action = "store_true",
     default = False,
     dest = "pretend")
+
+parser.add_option("--tab",
+    help = "Replace a TAB character with N spaces (default N = 2)",
+    default = 2,
+    type = "int",
+    dest = "tab",
+    metavar = "N")
 
 options, arguments = parser.parse_args()
 
