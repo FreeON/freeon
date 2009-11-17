e4e58b2306b49915027f845f20c0ccf317e4a1c8
Nicolas Bock <nicolasbock@gmail.com>
2009-11-17 07:41:30 -0700

    Regression test template.
    This version of the regression template shows how one could "make check" in
    the builddir without having to "make install".

--- buildbot/regression/master/master.c
+++ buildbot/regression/master/master.c
@@ -7,22 +7,40 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define MAX_LENGTH 10000
+
 int
 invoke (char *program, char *path, char **argv)
 {
   pid_t pid;
   int status;
+  char program_path[MAX_LENGTH];
 
   printf("[master:invoke] starting %s in %s\n", program, path);
+
+  /* Construct absolute path to program. */
+  snprintf(program_path, MAX_LENGTH, "%s/%s", path, program);
 
   pid = fork();
   if (pid == 0)
   {
-    /* This is the child process. */
-    if ((status = execvp(program, argv)) < 0)
+    /* This is the child process.
+     *
+     * First try to use the hardcoded path, i.e. the install path of the
+     * binaries. If that fails, fall back to using simply the binary name,
+     * relying on the PATH environment variable and the shell.
+     */
+    printf("[master:invoke] invoking %s\n", program_path);
+    if ((status = execvp(program_path, argv)) < 0)
     {
       printf("[master:invoke] error with execvp: %s\n", strerror(errno));
-      return 1;
+
+      printf("[master:invoke] invoking %s\n", program);
+      if ((status = execvp(program, argv)) < 0)
+      {
+        printf("[master:invoke] error with execvp: %s\n", strerror(errno));
+        return 1;
+      }
     }
   }
   printf("[master:invoke] this is the parent process\n");
