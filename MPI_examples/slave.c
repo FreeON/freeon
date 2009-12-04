870377c751c4bc4197829109899a2be1d450df50
Nicolas Bock <nicolasbock@gmail.com>
2009-12-04 13:02:04 -0700

    Fixed spawning problem.
    This example now polls the children only in 2 second intervals, which causes
    negligable load on the master process.

--- MPI_examples/slave.c
+++ MPI_examples/slave.c
@@ -10,24 +10,36 @@ main (int argc, char **argv)
   int size;
   int i, j;
   double temp;
+  int workload;
+  char buffer;
   MPI_Comm spawn;
+  FILE *random_dev;
+  int random_seed;
 
   MPI_Init(&argc, &argv);
 
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  printf("[slave %i] working\n", rank);
-  for (i = 0; i < 10000; ++i) {
-    for (j = 0; j < 500000; ++j)
+  /* Create some random workload. */
+  random_dev = fopen("/dev/urandom", "r");
+  fgets((char*) &random_seed, sizeof(int), random_dev);
+  srand(random_seed);
+  fclose(random_dev);
+
+  workload = 20000+(rand()/(double) RAND_MAX-0.5)*3000;
+  printf("[slave %i] working with workload %i\n", rank, workload);
+
+  for (i = 0; i < workload; ++i) {
+    for (j = 0; j < 100000; ++j)
     {
       temp = rand();
     }
   }
 
-  printf("[slave %i] waiting at barrier\n", rank);
+  printf("[slave %i] sending message to master\n", rank);
   MPI_Comm_get_parent(&spawn);
-  MPI_Barrier(spawn);
+  MPI_Send(&buffer, 1, MPI_CHAR, 0, 0, spawn);
 
   MPI_Finalize();
 }
