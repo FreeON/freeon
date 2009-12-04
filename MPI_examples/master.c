bc918f0a1f456b8bf55f86438826710e6cde87c4
Nicolas Bock <nicolasbock@gmail.com>
2009-12-04 13:02:04 -0700

    Fixed spawning problem.
    This example now polls the children only in 2 second intervals, which causes
    negligable load on the master process.

--- MPI_examples/master.c
+++ MPI_examples/master.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <unistd.h>
 
 int
 main (int argc, char **argv)
@@ -10,6 +11,13 @@ main (int argc, char **argv)
   int *error_codes;
   int spawn_counter = 0;
   char *slave_argv[] = { "arg1", "arg2", 0 };
+  char *buffer;
+  MPI_Request *request;
+  MPI_Status *status;
+  int *flag;
+  int buffer_index;
+  int number_children;
+  int i;
   MPI_Comm spawn;
 
   MPI_Init(&argc, &argv);
@@ -26,8 +34,44 @@ main (int argc, char **argv)
       printf("[master] (%i) forking processes\n", spawn_counter++);
       error_codes = (int*) malloc(sizeof(int)*size);
       MPI_Comm_spawn("./slave", slave_argv, size, MPI_INFO_NULL, 0, MPI_COMM_SELF, &spawn, error_codes);
-      printf("[master] waiting at barrier\n");
-      MPI_Barrier(spawn);
+      MPI_Comm_remote_size(spawn, &number_children);
+      printf("[master] waiting for messages from %i children\n", number_children);
+
+      /* Prepare receive buffers. */
+      buffer = (char*) malloc(sizeof(char)*number_children);
+      request = (MPI_Request*) malloc(sizeof(MPI_Request)*number_children);
+      flag = (int*) malloc(sizeof(int)*number_children);
+      status = (MPI_Status*) malloc(sizeof(MPI_Status)*number_children);
+      for (buffer_index = 0; buffer_index < number_children; ++buffer_index)
+      {
+        MPI_Irecv(buffer+buffer_index, 1, MPI_CHAR, MPI_ANY_SOURCE, 0, spawn, request+buffer_index);
+        *(flag+buffer_index) = 0;
+      }
+
+      buffer_index = 0;
+      while (buffer_index < number_children)
+      {
+        for (i = 0; i < number_children; ++i)
+        {
+          if (! *(flag+i))
+          {
+            MPI_Test(request+i, flag+i, status+i);
+            if (*(flag+i))
+            {
+              printf("[master] child %i finished\n", i+1);
+              buffer_index++;
+            }
+          }
+        }
+
+        sleep(2);
+      }
+
+      printf("[master] everybody finished, freeing memory\n");
+      free(status);
+      free(flag);
+      free(request);
+      free(buffer);
       free(error_codes);
     }
   }
