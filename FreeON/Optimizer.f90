be9b2e71671639d3d6742de5c5f084f9b348ccec
Nicolas Bock <nicolasbock@gmail.com>
2010-01-21 23:16:27 -0700

    Fixed typo in log statement.

--- FreeON/Optimizer.f90
+++ FreeON/Optimizer.f90
@@ -139,7 +139,7 @@ CONTAINS
     ! Hardcode stepsize.
     CALL MondoLog(DEBUG_NONE, "ConjugateGradient", "initiating conjugate gradient optimization")
     CALL MondoLog(DEBUG_NONE, "ConjugateGradient", "convergence reached when maxForce and RMSForce are below " &
-      //TRIM(DblToShrtChar(GTol(C%Opts%AccuracyLevels(iBAS))*au2eV/AUToAngstroms))//" eV/A)")
+      //TRIM(DblToShrtChar(GTol(C%Opts%AccuracyLevels(iBAS))*au2eV/AUToAngstroms))//" eV/A")
 
     ! Calculate the gradient.
     CALL MondoLog(DEBUG_NONE, "ConjugateGradient", "calculating initial force")
