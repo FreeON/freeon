5c80630a16d4d0fa78f100e5666b59b4797c2466
Nicolas Bock <nicolasbock@gmail.com>
2010-02-03 08:47:11 -0700

    Again, another fix for NFS and QCTC.
    The performance of the last patch was really bad. I hacked QCTC to write
    E_NuclearTotal to binary files, one for each clone, and then have clone 1
    collect the values written into hdf. This obviously serializes writes to hdf
    and should have minimal impact on performance.

--- tests/lock.f90
+++ tests/lock.f90
@@ -22,27 +22,28 @@ PROGRAM locktest
   ! Get some information on world communicator.
   CALL MPI_COMM_RANK(MPI_COMM_WORLD, rank, IErr)
   pid = GetPIDWrapper()
-  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "starting up...", "rank "//TRIM(IntToChar(MyID)))
+  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "starting up...", "rank "//TRIM(IntToChar(rank)))
 
   ! Allocate lock.
   lock%Alloc = ALLOCATED_FALSE
   CALL AllocateLock(lock, MPI_COMM_WORLD)
 
-  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "trying to get lock", "rank "//TRIM(IntToChar(MyID)))
-  CALL AcquireLock(lock, FreeONLockExclusive)
-  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "sleeping", "rank "//TRIM(IntToChar(MyID)))
-  CALL FreeONSleep(5)
-  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "releasing lock", "rank "//TRIM(IntToChar(MyID)))
+  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "trying to get lock", "rank "//TRIM(IntToChar(rank)))
+  !CALL AcquireLock(lock, FreeONLockExclusive)
+  CALL AcquireLock(lock, FreeONLockShared)
+  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "sleeping", "rank "//TRIM(IntToChar(rank)))
+  CALL FreeONSleep(10)
+  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "releasing lock", "rank "//TRIM(IntToChar(rank)))
   CALL ReleaseLock(lock)
 
   ! Wait for other ranks.
-  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "waiting for other ranks to finish", "rank "//TRIM(IntToChar(MyID)))
+  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "waiting for other ranks to finish", "rank "//TRIM(IntToChar(rank)))
   CALL MPI_Barrier(MPI_COMM_WORLD, IErr)
-  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "everybody finished", "rank "//TRIM(IntToChar(MyID)))
+  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "everybody finished", "rank "//TRIM(IntToChar(rank)))
 
   ! Free lock.
-  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "freeing lock", "rank "//TRIM(IntToChar(MyID)))
-  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "communicator = "//TRIM(IntToChar(lock%communicator)), "rank "//TRIM(IntToChar(MyID)))
+  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "freeing lock", "rank "//TRIM(IntToChar(rank)))
+  CALL MondoLog(DEBUG_NONE, "lock "//TRIM(IntToChar(pid)), "communicator = "//TRIM(IntToChar(lock%communicator)), "rank "//TRIM(IntToChar(rank)))
   CALL FreeLock(lock)
 
   CALL MPI_FINALIZE(IErr)
