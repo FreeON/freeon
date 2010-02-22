FreeON/freeon

commit bfc618d658224083a73968e7a3a19eaffc45b98b

First attempt at porting the spawning problem to the front-end
I ported the message sending and receiving to the front-end. However, the CPU
load on the front-end is still there.

Modules/Macros.f90
@@ -142,15 +142,15 @@ CONTAINS
     ! Print time stamp
     IF(PrintFlags%Key>DEBUG_MEDIUM)THEN
 #if defined(PARALLEL) || defined(PARALLEL_CLONES)
-      IF(MyId==ROOT) THEN
+      IF(MyId == ROOT) THEN
         CALL TimeStamp('Entering '//TRIM(Prog),Enter_O=.TRUE.)
       ENDIF
 #else
       CALL TimeStamp('Entering '//TRIM(Prog))
 #endif
     ENDIF
-    !
-    !   CALL ShutDown(Prog)
+
+    ! CALL ShutDown(Prog)
   END SUBROUTINE StartUp
 
 #if defined(PARALLEL) || defined(PARALLEL_CLONES)
@@ -272,17 +272,18 @@ CONTAINS
     ! ... and close the HDF file ...
     CALL CloseHDF(HDFFileID)
     ! ... shutdown MPI and print a time stamp
-#if defined(PARALLEL) || defined(PARALLEL_CLONES)
-    IF(PrintFlags%Key > DEBUG_MEDIUM.AND.MyId==ROOT) THEN
-      CALL TimeStamp('Exiting '//TRIM(Prog),Enter_O=.FALSE.)
-    ENDIF
-    ! Shutdown MPI
-    CALL FiniMPI()
-#else
     IF(PrintFlags%Key>DEBUG_MEDIUM) THEN
+#if defined(PARALLEL) || defined(PARALLEL_CLONES)
+      IF(MyId == ROOT) THEN
+        CALL TimeStamp('Exiting '//TRIM(Prog),Enter_O=.FALSE.)
+      ENDIF
+
+      ! Shutdown MPI
+      CALL FiniMPI()
+#else
       CALL TimeStamp('Exiting '//TRIM(Prog),Enter_O=.FALSE.)
-    ENDIF
 #endif
+    ENDIF
     STOP
   END SUBROUTINE ShutDown
 

Modules/MondoMPI.f90
@@ -113,14 +113,23 @@ MODULE MondoMPI
         INTEGER                    :: IErr
         CHARACTER(LEN=*),PARAMETER :: Sub='FiniMPI'
 #ifdef MPI2
-        INTEGER                    :: PARENT, ALL
-!
+        INTEGER   :: PARENT, INTRA_PARENT
+        CHARACTER :: message_buffer
+
         ! Get the parent communicator
-        CALL MPI_COMM_GET_PARENT(PARENT,IErr)
+        CALL MPI_COMM_GET_PARENT(PARENT, IErr)
+
         ! Merge the parent and current local communicators
-        CALL MPI_INTERCOMM_MERGE(PARENT,.TRUE.,ALL,IErr)
-        ! Sychronize parent and child
-        CALL MPI_BARRIER(ALL,IErr)
+        CALL MPI_INTERCOMM_MERGE(PARENT, .TRUE., INTRA_PARENT, IErr)
+
+        ! Sychronize parent and child. We do that by sending a message to the
+        ! parent.
+        !CALL MondoLog(DEBUG_NONE, "FiniMPI", "sending message to rank 0")
+        CALL MPI_SEND(message_buffer, 1, MPI_CHARACTER, 0, 0, PARENT, IErr)
+
+        ! Free the communicator.
+        CALL MPI_COMM_FREE(PARENT, IErr)
+        CALL MPI_COMM_FREE(INTRA_PARENT, IErr)
 #endif
         CALL MPI_FINALIZE(IErr)
       END SUBROUTINE FiniMPI

FreeON/Overlay.f90
@@ -70,8 +70,13 @@ CONTAINS
     TYPE(INT_VECT),SAVE  :: IChr
     CHARACTER(LEN=2*DCL) :: CmndLine
     INTEGER              :: beginClump, endClump
+
 #if (defined(PARALLEL) || defined(PARALLEL_CLONES)) && defined(MPI2)
-    INTEGER              :: SPAWN, ALL
+    INTEGER                              :: SPAWN, INTRA_SPAWN, buffer_index
+    CHARACTER, ALLOCATABLE, DIMENSION(:) :: message_buffer
+    INTEGER, ALLOCATABLE, DIMENSION(:)   :: message_request
+    INTEGER, ALLOCATABLE, DIMENSION(:,:) :: message_status
+    LOGICAL, ALLOCATABLE, DIMENSION(:)   :: message_flag
 #else
     INTERFACE
       FUNCTION Spawn(NC, MaxLen, IChr)
@@ -127,14 +132,58 @@ CONTAINS
       ENDIF
 
       ! Merge the spawned and current local communicators
-      CALL MPI_INTERCOMM_MERGE(SPAWN,.TRUE.,ALL,IErr)
+      CALL MPI_INTERCOMM_MERGE(SPAWN, .TRUE., INTRA_SPAWN, IErr)
 
-      ! Wait for the kiddies to be done
-      CALL MPI_BARRIER(ALL,IErr)
+      ! Wait for the kiddies to be done. We do that by waiting for them to send
+      ! us a nice message instead of waiting at a barrier. This is to avoid
+      ! spending CPU time in the front-end polling the barrier. Waiting for
+      ! messages, we can do non-blocking receives and control the polling
+      ! frequency ourselves.
+      !
+      ! First set up the receive buffers.
+      ALLOCATE(message_buffer(M%Clump%I(3, iCLUMP)))
+      ALLOCATE(message_request(M%Clump%I(3, iCLUMP)))
+      ALLOCATE(message_flag(M%Clump%I(3, iCLUMP)))
+      ALLOCATE(message_status(M%Clump%I(3, iCLUMP), MPI_STATUS_SIZE))
+
+      DO I = 1, M%Clump%I(3, iCLUMP)
+        !CALL MondoLog(DEBUG_NONE, "Invoke", "setting up receive buffers")
+        CALL MPI_IRECV(message_buffer(I), 1, MPI_CHARACTER, MPI_ANY_SOURCE, 0, SPAWN, message_request(I), IErr)
+        message_flag(I) = .FALSE.
+      ENDDO
+
+      ! Loop over children to check whether they are done and have sent a
+      ! message.
+      buffer_index = 0
+      DO WHILE(buffer_index < M%Clump%I(3, iCLUMP))
+        DO I = 1, M%Clump%I(3, iCLUMP)
+          IF(.NOT. message_flag(I)) THEN
+            !CALL MondoLog(DEBUG_NONE, "Invoke", "testing child "//TRIM(IntToChar(I)))
+            CALL MPI_TEST(message_request(I), message_flag(I), message_status(I,:), IErr)
+            IF(message_flag(I)) THEN
+              !CALL MondoLog(DEBUG_NONE, "Invoke", "child "//TRIM(IntToChar(buffer_index+1))//" has finished")
+              buffer_index = buffer_index+1
+            ENDIF
+          ENDIF
+        ENDDO
+
+        ! Sleep a little.
+        !CALL MondoLog(DEBUG_NONE, "Invoke", "sleeping")
+        CALL SLEEP(2)
+      ENDDO
+
+      ! All children are done.
+      !CALL MondoLog(DEBUG_NONE, "Invoke", "all children are done")
 
       ! Free the communicator.
       CALL MPI_COMM_FREE(SPAWN, IErr)
-      CALL MPI_COMM_FREE(ALL, IErr)
+      CALL MPI_COMM_FREE(INTRA_SPAWN, IErr)
+
+      ! Free the buffers.
+      DEALLOCATE(message_buffer)
+      DEALLOCATE(message_request)
+      DEALLOCATE(message_flag)
+      DEALLOCATE(message_status)
 #else
       ! Create ASCII integer array to beat F9x/C incompatibility
       CALL CVToIV(NArg,ArgV,MaxLen,IChr)

commit afe3eacaadb2616537dc9f266b19bca95fac93dd

Fixed really embarrassing bug in ShutDown().
FiniMPI() was called only for debugging levels >= DEBUG_MEDIUM, which leads to
crashes when Debug=MinDebug is used. This bug was introduced in

commit bfc618d658224083a73968e7a3a19eaffc45b98b

FreeON/FreeON.f90
@@ -47,8 +47,6 @@ PROGRAM FreeON
 
   TYPE(Controls) :: C
 
-  LOGICAL :: debugThis
-
   CALL Init(PerfMon)
   CALL Init(MemStats)
 
@@ -63,13 +61,6 @@ PROGRAM FreeON
 
     ! Initialize controls
     CALL InitGlobal(C)
-
-    ! Debug.
-    CALL MondoLog(DEBUG_NONE, "FreeON", "debug...")
-    debugThis = .FALSE.
-    DO WHILE(debugThis)
-    ENDDO
-    CALL MondoLog(DEBUG_NONE, "FreeON", "past debug...")
 
     ! Much ado about gradients
     SELECT CASE(C%Opts%Grad)

Modules/Macros.f90
@@ -119,21 +119,11 @@ CONTAINS
     ! Each ROOT in each MONDO_COMM opens the HDF file --->FOR READ ONLY<---
     HDFFileID=OpenHDF(H5File)
     HDF_CurrentID=HDFFileID
-    ! Operate at the top level of the archive
-    ! Open a group that the ROOT of each clone accesses by default
+    ! Operate at the top level of the archive.  Open a group that the ROOT of
+    ! each clone accesses by default
     H5GroupID=OpenHDFGroup(HDFFileID,"Clone #"//TRIM(IntToChar(MyClone)))
     ! Default is now the cloned group id rather than the HDF file id
     HDF_CurrentID=H5GroupID
-    !    WRITE(*,22)TRIM(Prog),MyId,MyClone
-    ! 22 format(A10,' IN MACROS, GETTING TAG',I3,", ",I3)
-    ! 23 format(A10,' IN MACROS, GOT     TAG',I3,", ",I3)
-    !    CALL GET(ITAG,"TAG")
-    !    WRITE(*,23)TRIM(Prog),MyId,MyClone
-    !    CALL GET(ETAG,'E_NuclearTotal',StatsToChar(Current))
-    !    CALL PUT(2*ITAG,"TAG")
-    !    CALL PUT(2D0*ETAG,'E_NuclearTotal',StatsToChar(Current))
-    !    IF(MyID==ROOT)WRITE(*,33),MyId,MyClone,iTAG,ETag,HDFFileID,HDF_CurrentID
-    !    33 FORMAT(' MyId = ',I2,', MyClone = ',I2,', iTAG = ',I6,' ETAG = ',D12.6,', FileID = ',I10,' CURRENTID = ',I10)
     ! Load variables global at the group (clone) level
     CALL LoadGroupGlobals(Args)
     ! Initialize memory statistics
@@ -221,10 +211,8 @@ CONTAINS
   SUBROUTINE ShutDown(Prog)
     CHARACTER(LEN=*),INTENT(IN) :: Prog
 
-    !    IF(MyID==ROOT)WRITE(*,33)Prog,MyId,MyClone,H5GroupID,HDFFileID
-    !    33 FORMAT(A10,'SHUTDOWN: MyId = ',I2,', MyClone = ',I2,', GroupID = ',I10,' FileID = ',I10)
-
-    ! Following is a bit of fancy HDF footwork, necessary for parallel-clones to work properly
+    ! Following is a bit of fancy HDF footwork, necessary for parallel-clones to
+    ! work properly
     !
     ! Close the clone directories
     CALL CloseHDFGroup(H5GroupID)
@@ -234,8 +222,6 @@ CONTAINS
     ! Revert back to global communicator, rank etc ...
     MONDO_COMM=MPI_COMM_WORLD
     MyID=MRank()
-    !CALL MondoLog(DEBUG_NONE, "ShutDown", "setting MyID = "//TRIM(IntToChar(MyID)) &
-    !  //", MONDO_COMM = "//TRIM(IntToChar(MONDO_COMM)))
     ! and reopen the upper level HDF directory for just the world root node
     HDFFileID=OpenHDF(H5File)
 #endif
@@ -266,26 +252,18 @@ CONTAINS
       ENDIF
     ENDIF
 
-    ! OMG,this is anoying!!
-    ! Check MemStats.
-!    IF(MemStats%Allocs /= MemStats%DeAllocs) THEN
-!      CALL MondoLog(DEBUG_NONE, "ShutDown ("//TRIM(Prog)//")", "Possible memory leak. Allocs = " &
-!        //TRIM(IntToChar(MemStats%Allocs))//", DeAllocs = " &
-!        //TRIM(IntToChar(MemStats%DeAllocs)))
-      !CALL Halt("[FIXME]")
-!    ENDIF
-
     IF(PrintFlags%Key==DEBUG_MAXIMUM) THEN
       CALL PPrint(MemStats,Prog)
     ENDIF
 
     ! Now mark success of this program ...
+    CALL MondoLog(DEBUG_NONE, "ShutDown", "[FIXME] should not put in parallel")
     CALL Put(.FALSE.,'ProgramFailed')
     ! ... and close the HDF file ...
     CALL CloseHDF(HDFFileID)
-    ! ... shutdown MPI and print a time stamp
+
+    ! Print a time stamp.
     IF(PrintFlags%Key>DEBUG_MEDIUM) THEN
-#if defined(PARALLEL) || defined(PARALLEL_CLONES)
 #if defined(PARALLEL)
       IF(MyId == ROOT) THEN
 #endif
@@ -293,13 +271,12 @@ CONTAINS
 #if defined(PARALLEL)
       ENDIF
 #endif
-
-      ! Shutdown MPI
-      CALL FiniMPI()
-#else
-      CALL TimeStamp('Exiting '//TRIM(Prog),Enter_O=.FALSE.)
-#endif
     ENDIF
+
+#if defined(PARALLEL) || defined(PARALLEL_CLONES)
+    ! Shutdown MPI
+    CALL FiniMPI()
+#endif
     STOP
   END SUBROUTINE ShutDown
 
@@ -308,8 +285,8 @@ CONTAINS
   !==============================================================
   SUBROUTINE LoadTopLevelGlobals(Args)
     TYPE(ARGMT) :: Args
-    INTEGER :: I,ChkNPrc
-    !-----------------------------------------------------------!
+    INTEGER     :: I,ChkNPrc
+
     IF(Args%NC>=2) THEN
       SCFActn=TRIM(Args%C%C(2))
     ENDIF
@@ -391,11 +368,11 @@ CONTAINS
 #if defined(PARALLEL) || defined(PARALLEL_CLONES)
         CALL New(OffSt,NPrc-1,0)
         CALL Get(OffSt,'dbcsroffsets',Tag_O=CurBase)
-        !             CALL Get(ChkNPrc,'chknprc')
-        !             IF(NPrc/=ChkNPrc) &
-        !                  CALL Halt('In StartUp() --- Inconsistency: NPrc = '  &
-        !                  //TRIM(IntToChar(NPrc))//' ChkNPrc = ' &
-        !                  //TRIM(IntToChar(ChkNPrc)))
+        !CALL Get(ChkNPrc,'chknprc')
+        !IF(NPrc/=ChkNPrc) &
+        !     CALL Halt('In StartUp() --- Inconsistency: NPrc = '  &
+        !     //TRIM(IntToChar(NPrc))//' ChkNPrc = ' &
+        !     //TRIM(IntToChar(ChkNPrc)))
         CALL New(Beg,NPrc-1,0)
         CALL New(End,NPrc-1,0)
         CALL Get(Beg,'beg',Tag_O=CurBase)
@@ -424,6 +401,7 @@ CONTAINS
     CALL Get(CS_OUT,'ovcells',Tag_O=CurGeom)
 
   END SUBROUTINE LoadGroupGlobals
+
   !=========================================================
   ! MARK FAILURE OF PROG
   !=========================================================
@@ -432,15 +410,14 @@ CONTAINS
     CALL Put(.TRUE.,'ProgramFailed')
     CALL Put(Prog  ,'FailedProgram')
   END SUBROUTINE MarkFailure
+
   !=========================================================
   ! MARK SUCCESS OF PROG
   !=========================================================
   SUBROUTINE MarkSuccess()
     CALL Put(.FALSE.,'ProgramFailed')
   END SUBROUTINE MarkSuccess
-  !-------------------------------------------------------------------------------
-  !
-  !-------------------------------------------------------------------------------
+
   SUBROUTINE Init_MEMS(A)
     TYPE(MEMS), INTENT(OUT) :: A
     A%Allocs=0
@@ -449,9 +426,7 @@ CONTAINS
     A%MaxMem=0
     A%MaxAlloc=0
   END SUBROUTINE Init_MEMS
-  !-------------------------------------------------------------------------------
-  !
-  !-------------------------------------------------------------------------------
+
   SUBROUTINE Init_TIME(A)
     TYPE(TIME),INTENT(OUT) :: A
 #if defined(PARALLEL)
@@ -463,9 +438,7 @@ CONTAINS
     A%CPUS=CPUSec()
     A%Wall=WallSec()
   END SUBROUTINE Init_TIME
-  !-------------------------------------------------------------------------------
 
-  !-------------------------------------------------------------------------------
   SUBROUTINE Init_DEBG(A,Prog)
     TYPE(DEBG), INTENT(OUT)      :: A
     CHARACTER(LEN=*), INTENT(IN) :: Prog
@@ -474,6 +447,7 @@ CONTAINS
     IF(MyId==ROOT)THEN
 #endif
       CALL OpenASCII(InpFile,Inp)
+
       IF(OptKeyQ(Inp,GLOBAL_DEBUG,DBG_PRT_INTS))THEN
         A%Int=DEBUG_INTEGRAL
       ELSE
@@ -529,7 +503,7 @@ CONTAINS
       ENDIF
 #if defined(PARALLEL)
     ENDIF
-    IF(InParallel)CALL BCast(A)
+    IF(InParallel) CALL BCast(A)
 #endif
     CLOSE(Inp)
   END SUBROUTINE Init_DEBG

Modules/MondoMPI.f90
@@ -153,26 +153,26 @@ CONTAINS
       CALL MPI_FINALIZE(IErr)
    END SUBROUTINE FiniMPI
 
-   FUNCTION MRank(communicator_O)
+   FUNCTION MRank(comm_O)
       INTEGER                     :: IErr,MRank
-      INTEGER, OPTIONAL           :: communicator_O
+      INTEGER, OPTIONAL           :: comm_O
       CHARACTER(LEN=*), PARAMETER :: Sub = 'MRank'
 
-      IF(PRESENT(communicator_O)) THEN
-         CALL MPI_COMM_RANK(communicator_O, MRank, IErr)
+      IF(PRESENT(comm_O)) THEN
+         CALL MPI_COMM_RANK(comm_O, MRank, IErr)
       ELSE
          CALL MPI_COMM_RANK(MONDO_COMM, MRank, IErr)
       ENDIF
       CALL ErrChk(IErr, Sub)
    END FUNCTION MRank
 
-   FUNCTION MSize(communicator_O)
+   FUNCTION MSize(comm_O)
       INTEGER                     :: IErr,MSize
-      INTEGER, OPTIONAL           :: communicator_O
+      INTEGER, OPTIONAL           :: comm_O
       CHARACTER(LEN=*), PARAMETER :: Sub = 'MSize'
 
-      IF(PRESENT(communicator_O)) THEN
-         CALL MPI_COMM_SIZE(communicator_O, MSize, IErr)
+      IF(PRESENT(comm_O)) THEN
+         CALL MPI_COMM_SIZE(comm_O, MSize, IErr)
       ELSE
          CALL MPI_COMM_SIZE(MONDO_COMM, MSize, IErr)
       ENDIF

Modules/PrettyPrint.f90
@@ -70,7 +70,7 @@ MODULE PrettyPrint
       MODULE PROCEDURE Print_CheckSum_DBCSR
 #endif
    END INTERFACE
-!
+
    CHARACTER(LEN=DEFAULT_CHR_LEN) :: String
    CHARACTER(LEN=*),PARAMETER     :: CheckEq = ' CheckSum = '
    CONTAINS
@@ -90,11 +90,11 @@ MODULE PrettyPrint
          DDay=DDate(5:6)//'/'//DDate(7:8)//'/'//DDate(3:4)
          HMS=TTime(1:2)//':'//TTime(3:4)//':'//TTime(5:6)
          IF(PrintFlags%Fmt==DEBUG_MMASTYLE)THEN
-            CALL MondoLog(DEBUG_NONE, "TimeStamp", '(*'//TRIM(Mssg)//' '//DDay//' @ '//HMS//'*)')
+            CALL MondoLog(DEBUG_NONE, "TimeStamp", '(*'//TRIM(Mssg)//' '//TRIM(DDay)//' @ '//TRIM(HMS)//'*)')
          ELSEIF(Enter)THEN
-            CALL MondoLog(DEBUG_NONE, "TimeStamp", '<<'//TRIM(Mssg)//' '//DDay//' @ '//HMS//'>>')
+            CALL MondoLog(DEBUG_NONE, "TimeStamp", '<<'//TRIM(Mssg)//' '//TRIM(DDay)//' @ '//TRIM(HMS)//'>>')
          ELSE
-            CALL MondoLog(DEBUG_NONE, "TimeStamp", '>-'//TRIM(Mssg)//' '//DDay//' @ '//HMS//'-<')
+            CALL MondoLog(DEBUG_NONE, "TimeStamp", '>-'//TRIM(Mssg)//' '//TRIM(DDay)//' @ '//TRIM(HMS)//'-<')
          ENDIF
 #ifdef PARALLEL
          ENDIF