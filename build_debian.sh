abb2737f053e0fa132fd45369410fd244619db2f
Nicolas Bock <nicolasbock@gmail.com>
2013-09-26 10:30:51 -0700

    Moved debian packaging commands into makefile.

--- build_debian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-make dist
-mv freeon-1.0.3.tar.bz2 freeon_1.0.3.orig.tar.bz2
-rm -rf freeon-1.0.3
-tar xf freeon_1.0.3.orig.tar.bz2
-rsync -av debian freeon-1.0.3/
-cd freeon-1.0.3
-debuild -us -uc
