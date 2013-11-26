301aeb34d97a30a82d21947e7ab66a751b508f3e
Nicolas Bock <nicolasbock@gmail.com>
2013-11-26 11:46:43 -0700

    Fixed convertBCSR

--- tools/bcsr.cc
+++ tools/bcsr.cc
@@ -495,6 +495,7 @@ void BCSR::toMM (char *filename)
           for(int i = 0; i < MBlock; i++) {
             for(int j = 0; j < NBlock; j++)
             {
+              double Aij = matrix[pointer+i+j*MBlock];
               fprintf(fstream, "%d %d % e\n", 1+i+i_block, 1+j+j_block, Aij);
             }
           }
